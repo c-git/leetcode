cb8509175e0e555555536d13681b4c0f806d256b
Che <43485962+c-git@users.noreply.github.com>
2022-05-02 00:35:33 -0400

    2. Add Two Numbers

---
+++ python3/_2_AddTwoNumbers.py
@@ -0,0 +1,43 @@
+from typing import Optional, Tuple
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.val == other.val and self.next == other.next
+
+    def __repr__(self):
+        return f'{self.val} -> {self.next}'
+
+
+class Solution:
+    def sum(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int) \
+            -> Tuple[ListNode, int]:
+        v1 = 0 if l1 is None else l1.val
+        v2 = 0 if l2 is None else l2.val
+        sum_ = v1 + v2 + carry
+        digit = sum_ % 10
+        return ListNode(digit), sum_ // 10
+
+    def addTwoNumbers(
+            self, l1: Optional[ListNode], l2: Optional[ListNode]) -> \
+            Optional[ListNode]:
+        head, carry = self.sum(l1, l2, 0)
+        last = head
+        l1 = l1.next
+        l2 = l2.next
+        while l1 is not None or l2 is not None or carry > 0:
+            next_, carry = self.sum(l1, l2, carry)
+            last.next = next_
+            last = next_
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+        return head
--- python3/main.py
+++ python3/main.py
@@ -1,15 +1,32 @@
-from python3._2164_SortEvenandOddIndicesIndependently import Solution
+from typing import List
+
+from python3._2_AddTwoNumbers import ListNode, Solution
+
+
+def convertToListNodes(l_in: List[int]) -> ListNode:
+    head = ListNode(l_in[0])
+    last = head
+    for i in range(1, len(l_in)):
+        next_ = ListNode(l_in[i])
+        last.next = next_
+        last = next_
+    return head
 
 
 def main():
     examples = [
-        ([4, 1, 2, 3], [2, 3, 4, 1]),
-        ([2, 1], [2, 1]),
+        (([2, 4, 3], [5, 6, 4]), [7, 0, 8]),
+        (([0], [0]), [0]),
+        (([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]), [8, 9, 9, 9, 0, 0, 0, 1]),
     ]
     solution = Solution()
     for example in examples:
         input_, exp = example
-        output_ = solution.sortEvenOdd(input_)
+        l1, l2 = input_
+        l1 = convertToListNodes(l1)
+        l2 = convertToListNodes(l2)
+        exp = convertToListNodes(exp)
+        output_ = solution.addTwoNumbers(l1, l2)
         assert exp == output_, \
             f'\n' \
             f'inp: {input_}\n' \
