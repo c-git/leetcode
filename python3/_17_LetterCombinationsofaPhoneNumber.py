824988282e1fe82753238d62ef0ec20ba8699a90
Che <43485962+c-git@users.noreply.github.com>
2022-05-09 12:16:10 -0400

    17. Letter Combinations of a Phone Number

---
+++ python3/_17_LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,60 @@
+from collections.abc import Iterable
+from typing import List, Tuple
+
+
+class Solution:
+    _mapping = None
+
+    @classmethod
+    @property
+    def mapping(cls):
+        if cls._mapping is None:
+            cls._mapping = {}
+            char_ord = 97  # a
+            for digit in range(2, 10):
+                chars = []
+                cls._mapping[digit] = chars
+                for i in range(3):
+                    chars.append(chr(char_ord))
+                    char_ord += 1
+            cls._mapping[9].append(chr(char_ord))  # z
+        return cls._mapping
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+        chars = [self.mapping[int(digit)] for digit in digits]
+        stack: List[Tuple[Iterable, str]] = [] if len(digits) == 0 else \
+            [((iter(chars[0])), '')]
+        while len(stack) > 0:
+            iterator, prefix = stack.pop()
+            if len(digits) - 1 == len(prefix):
+                # On last digit
+                for suffix in iterator:
+                    result.append(prefix + suffix)
+            else:
+                try:
+                    new_prefix = prefix + next(iterator)
+                    stack.append((iterator, prefix))
+                    stack.append((iter(chars[len(new_prefix)]), new_prefix))
+                except StopIteration:
+                    pass  # Do nothing this digit is completed
+        return result
+
+
+def tester():
+    print("17 start")
+    examples = [
+        ("23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
+        ("234",
+         ["adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg",
+          "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh",
+          "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi", ]),
+        ("", []),
+        ("2", ["a", "b", "c"]),
+    ]
+    solution = Solution()
+    for example in examples:
+        input_, exp = example
+        output_ = solution.letterCombinations(input_)
+        assert exp == output_, f'\ninp: {input_}\nexp: {exp}\nout: {output_}'
+    print("17 complete")
