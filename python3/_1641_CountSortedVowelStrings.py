8bc40b15905b859f9bdfae49c007d9c7becc5180
Che <43485962+c-git@users.noreply.github.com>
2022-05-11 10:52:59 -0400

    1641. Submission 3

--- python3/_1641_CountSortedVowelStrings.py
+++ python3/_1641_CountSortedVowelStrings.py
@@ -2,17 +2,17 @@ from python3.helper import Eg, tester_helper
 
 
 class Solution:
-    def solve(self, n, letter_count) -> int:
-        result = 0
-        if n == 1:
-            return letter_count
-        for i in range(letter_count):
-            result += self.solve(n - 1, letter_count - i)
-        return result
-
     def countVowelStrings(self, n: int) -> int:
-        vowels = 5
-        return self.solve(n, 5)
+        stack = [(n, 5)]
+        result = 0
+        while len(stack) > 0:
+            size, letter_count = stack.pop()
+            if size == 1:
+                result += letter_count
+            else:
+                for i in range(letter_count):
+                    stack.append((size - 1, letter_count - i))
+        return result
 
 
 def tester():
