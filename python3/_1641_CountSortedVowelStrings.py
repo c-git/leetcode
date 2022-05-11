27b7998d795d66b945bc70d67e48237ee237ba3a
Che <43485962+c-git@users.noreply.github.com>
2022-05-11 11:32:17 -0400

    1641. Submission 5

--- python3/_1641_CountSortedVowelStrings.py
+++ python3/_1641_CountSortedVowelStrings.py
@@ -1,24 +1,19 @@
-import numpy as np
-
 from python3.helper import Eg, tester_helper
 
 
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        table = np.zeros((n + 1, 6))
+        last_values = {}  # Store values needed to compute next set of values
 
         # Base Case
-        for i in range(6):
-            table[1, i] = i
+        for i in range(1, 6):
+            last_values[i] = i
 
         # General Case
         for size in range(2, n + 1):
-            for letter_count in range(1, 5 + 1):
-                first_char_fixed_as_curr = table[size - 1, letter_count]
-                without_curr_char = table[size, letter_count - 1]
-                table[size, letter_count] = (first_char_fixed_as_curr +
-                                             without_curr_char)
-        return int(table[n, 5])
+            for letter_count in range(2, 5 + 1):
+                last_values[letter_count] += last_values[letter_count - 1]
+        return last_values[5]
 
 
 def tester():
