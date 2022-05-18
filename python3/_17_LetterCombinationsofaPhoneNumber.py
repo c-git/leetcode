2e32004b9b4298e7e3c021a9d10ec31958e302f6
Che <43485962+c-git@users.noreply.github.com>
2022-05-18 11:10:14 -0400

    17. Add comments and remove redundant brackets

--- python3/_17_LetterCombinationsofaPhoneNumber.py
+++ python3/_17_LetterCombinationsofaPhoneNumber.py
@@ -25,19 +25,40 @@ class Solution:
 
     def letterCombinations(self, digits: str) -> List[str]:
         result = []
-        chars = [self.mapping[int(digit)] for digit in digits]
+
+        # Create list of all the characters needed for each digit of the input
+        # e.g. [['a', 'b', 'c'], ['d', 'e', 'f']]
+        chars: List[List[str]] = [self.mapping[int(digit)] for digit in digits]
+
+        # Use stack to track work remaining to be done
+        # Each entry in the stack will store:
+        #  iterator - what to iterate through to get next digit to do
+        #  prefix - what has been done before
         stack: List[Tuple[Iterable, str]] = [] if len(digits) == 0 else \
-            [((iter(chars[0])), '')]
+            [(iter(chars[0]), '')]
+
         while len(stack) > 0:
-            iterator, prefix = stack.pop()
+            iterator, prefix = stack.pop()  # Get next item to work on
+
             if len(digits) - 1 == len(prefix):
                 # On last digit
                 for suffix in iterator:
                     result.append(prefix + suffix)
             else:
                 try:
+                    # Append the next character from our iterable to our
+                    # prefix to create a new prefix for our next friend. If
+                    # there are no more things to be added from our iterator
+                    # when we call next we will get a StopIteration exception,
+                    # and we will not add anything back onto the stack
                     new_prefix = prefix + next(iterator)
+
+                    # Put this iterator back onto the stack for later use (
+                    # maybe empty now, but we will check when we get back to it)
                     stack.append((iterator, prefix))
+
+                    # Add the newly created prefix to the stack along with an
+                    # iterator for the next set of values to be appended to it
                     stack.append((iter(chars[len(new_prefix)]), new_prefix))
                 except StopIteration:
                     pass  # Do nothing this digit is completed
