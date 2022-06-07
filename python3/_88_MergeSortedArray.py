cb16c0f0e2b9e18c5324fea93c185383c612cf84
Che <43485962+c-git@users.noreply.github.com>
2022-06-07 10:00:07 -0400

    88. Sub 2

--- python3/_88_MergeSortedArray.py
+++ python3/_88_MergeSortedArray.py
@@ -1,3 +1,4 @@
+import math
 from typing import Any, List, Tuple
 
 from python3.helper import Eg, tester_helper
@@ -8,8 +9,28 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2
-        nums1.sort()
+        # Convert m and n into indices
+        m -= 1
+        n -= 1
+
+        next_pos = len(nums1) - 1
+        while n >= 0:
+            # LI:
+            # - All indices greater than m are already inserted into the
+            # sorted part of nums1
+            # - All indices greater than n in nums2 are already inserted into
+            # the sorted part of nums 1
+            # - All indices in nums1 greater than next_pos are already sorted
+            assert m < next_pos
+            a = nums1[m] if m >= 0 else -math.inf
+            b = nums2[n] if n >= 0 else -math.inf
+            if b >= a:
+                nums1[next_pos] = b
+                n -= 1
+            else:
+                nums1[next_pos] = a
+                m -= 1
+            next_pos -= 1
 
 
 def ev(in_: Tuple[Any], out_, exp) -> bool:
