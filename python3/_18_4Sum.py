1f656844351f3e0545a6a382a64496c1e1964fc5
Che <43485962+c-git@users.noreply.github.com>
2022-06-22 11:22:22 -0400

    18. 4Sum

---
+++ python3/_18_4Sum.py
@@ -0,0 +1,74 @@
+from math import inf
+from typing import List
+
+from python3.helper import Eg, evaluator_sort_to_compare_two_deep, tester_helper
+
+
+class Solution:
+    def binary_search(self, nums: List[int], target: int) -> bool:
+        """
+        Returns true if the target is found
+
+        src: https://www.geeksforgeeks.org/python-program-for-binary-search/
+        :param nums: The sorted numbers to search
+        :param target: The number to search for
+        :return: True if number found else False
+        """
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (high + low) // 2
+            if nums[mid] < target:
+                # If target is greater, ignore left half
+                low = mid + 1
+            elif nums[mid] > target:
+                # If target is smaller, ignore right half
+                high = mid - 1
+            else:
+                # means target is present at mid
+                return True
+        # If we reach here, then the element was not present
+        return False
+
+    def anySum(self, nums: List[int], target: int, levels: int) \
+            -> List[List[int]]:
+        """
+        Solves any sum level recursively
+
+        NB: nums must be sorted
+        :param nums: Sorted list of numbers to pick from
+        :param target: The value that the number should add up to
+        :param levels: The number of numbers to be included in the sum
+        :return: List of solutions
+        """
+        if len(nums) == 0:
+            return []
+        assert levels >= 1
+        if levels == 1:
+            return [] if not self.binary_search(nums, target) else [[target]]
+        result = []
+        num = -inf
+        for i in range(len(nums) - levels + 1):
+            if num == nums[i]:
+                continue
+            num = nums[i]
+            for x in self.anySum(nums[i + 1:], target - num, levels - 1):
+                result.append([num] + x)
+        return result
+
+    def fourSum(
+            self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        return self.anySum(nums, target, 4)
+
+
+def tester():
+    examples = [
+        Eg(([1, 0, -1, 0, -2, 2], 0),
+           [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]],
+           evaluator_sort_to_compare_two_deep),
+        Eg(([2, 2, 2, 2, 2], 8), [[2, 2, 2, 2]]),
+        Eg(([2, 2, 2, 2, 2, 2], 8), [[2, 2, 2, 2]]),
+        Eg(([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0), [[0, 0, 0, 0]]),
+    ]
+    tester_helper(18, examples, Solution().fourSum)
