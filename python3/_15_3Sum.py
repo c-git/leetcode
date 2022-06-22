e6538b00e5cc9c93ce2264c3b4bbfe121ec76595
Che <43485962+c-git@users.noreply.github.com>
2022-06-22 10:24:11 -0400

    15. Sub2

--- python3/_15_3Sum.py
+++ python3/_15_3Sum.py
@@ -5,14 +5,18 @@ from python3.helper import Eg, tester_helper
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return []
+        nums.sort()
         result = []
+        first_num = nums[0] - 1  # set to diff value
         for ind_outer in range(len(nums) - 2):
             seen = set()  # Stores the 2nd numbers we've seen
+            if first_num == nums[ind_outer]:
+                continue  # Skip if same value for starting number
             first_num = nums[ind_outer]
             target = -first_num
-            for ind_inner, num in enumerate(nums[ind_outer + 1:]):
-                if ind_inner == ind_outer:
-                    continue
+            for num in nums[ind_outer + 1:]:
                 diff = target - num
                 if diff in seen:
                     result.append([first_num, diff, num])
@@ -22,8 +26,9 @@ class Solution:
 
 def tester():
     examples = [
-        Eg([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+        # Eg([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
         Eg([], []),
-        Eg([0], [])
+        Eg([0], []),
+        Eg([0, 0, 0], [[0, 0, 0]])
     ]
     tester_helper(15, examples, Solution().threeSum)
