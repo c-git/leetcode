7c42892965a1f5380ccf8d8ef348fbaa7e364cf2
Che <43485962+c-git@users.noreply.github.com>
2022-06-22 10:30:20 -0400

    15. Sub3

--- python3/_15_3Sum.py
+++ python3/_15_3Sum.py
@@ -12,6 +12,7 @@ class Solution:
         first_num = nums[0] - 1  # set to diff value
         for ind_outer in range(len(nums) - 2):
             seen = set()  # Stores the 2nd numbers we've seen
+            third_num = set()
             if first_num == nums[ind_outer]:
                 continue  # Skip if same value for starting number
             first_num = nums[ind_outer]
@@ -19,16 +20,19 @@ class Solution:
             for num in nums[ind_outer + 1:]:
                 diff = target - num
                 if diff in seen:
-                    result.append([first_num, diff, num])
+                    if num not in third_num:
+                        result.append([first_num, diff, num])
+                        third_num.add(num)
                 seen.add(num)
         return result
 
 
 def tester():
     examples = [
-        # Eg([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+        Eg([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
         Eg([], []),
         Eg([0], []),
-        Eg([0, 0, 0], [[0, 0, 0]])
+        Eg([0, 0, 0], [[0, 0, 0]]),
+        Eg([0, 0, 0, 0], [[0, 0, 0]])
     ]
     tester_helper(15, examples, Solution().threeSum)
