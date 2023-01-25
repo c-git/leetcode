181b65ce7f2779dfd85512d50f67e210cb789719
Che <43485962+c-git@users.noreply.github.com>
2023-01-24 19:57:53 -0500

    Reset for next question

--- rust/src/_1_two_sum.rs
+++ rust/src/_1_two_sum.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 struct Solution;
 
 impl Solution {
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,3 @@
-mod _1_two_sum;
+#![allow(dead_code)]
+// Add new file as a module to make it run
+// Create Solution struct `struct Solution;` in new module
