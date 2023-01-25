49fe1ab44407f6960cb35d3a1a13f97d64f4af06
Che <43485962+c-git@users.noreply.github.com>
2023-01-24 23:05:04 -0500

    Add conversion impls

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
 // Add new file as a module to make it run
 // Create Solution struct `struct Solution;` in new module
+// For an example of linked lists see _2_add_two_numbers
 mod helper;
