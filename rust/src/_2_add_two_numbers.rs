2fae306cddba18eff5393f118963a550f1e6015d
Che <43485962+c-git@users.noreply.github.com>
2023-01-24 23:14:20 -0500

    Sub 1

--- rust/src/_2_add_two_numbers.rs
+++ rust/src/_2_add_two_numbers.rs
@@ -7,7 +7,31 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        todo!()
+        // Source: https://github.com/zwhitchcox/leetcode/blob/master/src/0002_add_two_numbers.rs
+        let mut result = None;
+
+        // Make inputs mutable to be able to walk down the list
+        let mut l1 = l1;
+        let mut l2 = l2;
+
+        let mut carry = 0;
+        let mut curr = &mut result;
+        while l1.is_some() || l2.is_some() || carry != 0 {
+            let mut sum = carry;
+            if let Some(node) = l1 {
+                sum += node.val;
+                l1 = node.next;
+            }
+            if let Some(node) = l2 {
+                sum += node.val;
+                l2 = node.next;
+            }
+            carry = sum / 10;
+            *curr = Some(Box::new(ListNode::new(sum % 10)));
+            curr = &mut curr.as_mut().unwrap().next;
+        }
+
+        result
     }
 }
 
