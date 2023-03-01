c3d67a399c353df2726c557b372fea4b87fed585
Che <43485962+c-git@users.noreply.github.com>
2023-03-01 01:04:22 -0500

    Sub 1

--- rust/src/_20_valid_parentheses.rs
+++ rust/src/_20_valid_parentheses.rs
@@ -2,13 +2,50 @@ struct Solution;
 
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        todo!()
+        let mut stack = vec![];
+        let open_brackets = ['(', '{', '['];
+        for c in s.chars() {
+            if open_brackets.contains(&c) {
+                stack.push(c);
+            } else {
+                match c {
+                    ')' => {
+                        if !Self::check_pop('(', &mut stack) {
+                            return false;
+                        }
+                    }
+                    '}' => {
+                        if !Self::check_pop('{', &mut stack) {
+                            return false;
+                        }
+                    }
+                    ']' => {
+                        if !Self::check_pop('[', &mut stack) {
+                            return false;
+                        }
+                    }
+
+                    _ => unreachable!("Assumed only brackets based on question"),
+                }
+            }
+        }
+        stack.is_empty() // If stack is empty then string is valid otherwise there are unclosed brackets
+    }
+
+    fn check_pop(open_bracket: char, stack: &mut Vec<char>) -> bool {
+        if !stack.is_empty() && stack[stack.len() - 1] == open_bracket {
+            stack.pop();
+            return true;
+        }
+
+        // Wrong char at top of stack or stack empty
+        false
     }
 }
 
 #[test]
 fn case1() {
-    let s = "()";
+    let s = "()".to_string();
     let expected = true;
 
     let actual = Solution::is_valid(s);
@@ -17,7 +54,7 @@ fn case1() {
 
 #[test]
 fn case2() {
-    let s = "()[]{}";
+    let s = "()[]{}".to_string();
     let expected = true;
 
     let actual = Solution::is_valid(s);
@@ -26,7 +63,7 @@ fn case2() {
 
 #[test]
 fn case3() {
-    let s = "(]";
+    let s = "(]".to_string();
     let expected = false;
 
     let actual = Solution::is_valid(s);
