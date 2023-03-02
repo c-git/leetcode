9f008aee170d76bf24468de648d004a3cf44f781
Che <43485962+c-git@users.noreply.github.com>
2023-03-02 00:06:57 -0500

    Sub 2

--- rust/src/_785_is_graph_bipartite.rs
+++ rust/src/_785_is_graph_bipartite.rs
@@ -8,14 +8,18 @@ impl Solution {
         let mut b = HashSet::<i32>::new();
         for (u, edges) in graph.iter().enumerate() {
             let u = u as i32;
-            let mut a_local: &mut HashSet<i32>;
-            let mut b_local: &mut HashSet<i32>;
-            if a.contains(&u) || !b.contains(&u) {
+            let a_local: &mut HashSet<i32>;
+            let b_local: &mut HashSet<i32>;
+            if a.contains(&u) {
                 a_local = &mut a;
                 b_local = &mut b;
-            } else {
+            } else if b.contains(&u) || Self::edge_in(&a, edges) {
                 a_local = &mut b;
                 b_local = &mut a;
+            } else {
+                // Edge might be in be but we know it's not in a so this edge can only go in a
+                a_local = &mut a;
+                b_local = &mut b;
             }
             a_local.insert(u);
             for edge in edges {
@@ -26,6 +30,15 @@ impl Solution {
             }
         }
         true
+    }
+
+    fn edge_in(a: &HashSet<i32>, edges: &[i32]) -> bool {
+        for edge in edges {
+            if a.contains(edge) {
+                return true;
+            }
+        }
+        false
     }
 }
 
@@ -41,6 +54,14 @@ fn case1() {
 #[test]
 fn case2() {
     let graph = vec![vec![1, 3], vec![0, 2], vec![1, 3], vec![0, 2]];
+    let expected = true;
+
+    let actual = Solution::is_bipartite(graph);
+    assert_eq!(actual, expected);
+}
+#[test]
+fn case3() {
+    let graph = vec![vec![1], vec![0, 3], vec![3], vec![1, 2]];
     let expected = true;
 
     let actual = Solution::is_bipartite(graph);
