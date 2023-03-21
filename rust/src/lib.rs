745725e4a46d63527e2cc57060df739364f531c8
Che <43485962+c-git@users.noreply.github.com>
2023-03-21 19:18:48 -0400

    Simplify Rust Setup

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,3 +4,19 @@
 // Create Solution struct `struct Solution;` in new module
 // For an example of linked lists see _2_add_two_numbers
 mod helper;
+
+// Stub for creating test cases
+
+// struct Solution;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn case1() {
+//         let input = ;
+//         let expected = ;
+//         let actual = Solution::(input);
+//         assert_eq!(expected, actual);
+//     }
+// }
