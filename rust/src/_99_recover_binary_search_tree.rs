ab7dde0abe85b50e4ae2e61d5f563797e4cadf32
Che <43485962+c-git@users.noreply.github.com>
2023-03-24 12:21:21 -0400

    Sub 3

--- rust/src/_99_recover_binary_search_tree.rs
+++ rust/src/_99_recover_binary_search_tree.rs
@@ -1,5 +1,5 @@
-use std::cell::RefCell;
 use std::rc::Rc;
+use std::{cell::RefCell, mem};
 
 // Definition for a binary tree node.
 // #[derive(Debug, PartialEq, Eq)]
@@ -20,93 +20,63 @@ use std::rc::Rc;
 //   }
 // }
 use crate::helper::TreeNode;
-
-#[derive(Eq, PartialEq, Debug)]
-enum SearchResult {
-    SwappedNode(Rc<RefCell<TreeNode>>),
-    NotFound,
-    Completed,
-}
-
 impl Solution {
-    fn recover_tree_helper(
-        root: Option<Rc<RefCell<TreeNode>>>,
-        min: Option<i32>,
-        max: Option<i32>,
-    ) -> SearchResult {
-        let root = if let Some(val) = root {
-            val
-        } else {
-            return SearchResult::NotFound;
-        };
-
-        let root_val = root.borrow().val;
-
-        if let Some(min) = min {
-            if root_val <= min {
-                return SearchResult::SwappedNode(root.clone());
-            }
-        }
-        if let Some(max) = max {
-            if root_val >= max {
-                return SearchResult::SwappedNode(root.clone());
-            }
-        }
-
-        fn swap(a: Rc<RefCell<TreeNode>>, b: Rc<RefCell<TreeNode>>) {
-            let temp = a.borrow().val;
-            a.borrow_mut().val = b.borrow().val;
-            b.borrow_mut().val = temp;
-        }
-
-        let left = root.borrow().left.clone();
-        let left_result = match Self::recover_tree_helper(left, min, Some(root_val)) {
-            SearchResult::SwappedNode(swapped_node) => {
-                if swapped_node.borrow().val > root_val {
-                    SearchResult::SwappedNode(swapped_node)
-                } else {
-                    return SearchResult::SwappedNode(swapped_node); // Swap needs to happen higher up
-                }
-            }
-            SearchResult::NotFound => SearchResult::NotFound,
-            SearchResult::Completed => return SearchResult::Completed,
-        };
-
-        let right = root.borrow().right.clone();
-        let right_result = match Self::recover_tree_helper(right, Some(root_val), max) {
-            SearchResult::SwappedNode(swapped_node) => {
-                if swapped_node.borrow().val < root_val {
-                    SearchResult::SwappedNode(swapped_node)
-                } else {
-                    assert_eq!(left_result, SearchResult::NotFound); // Based on question stating there can only be 1
-                    return SearchResult::SwappedNode(swapped_node); // Swap needs to happen higher up
-                }
-            }
-            SearchResult::NotFound => SearchResult::NotFound,
-            SearchResult::Completed => return SearchResult::Completed,
-        };
-
-        match (left_result, right_result) {
-            (SearchResult::SwappedNode(left_node), SearchResult::SwappedNode(right_node)) => {
-                // Swap the children nodes
-                swap(left_node, right_node);
-                SearchResult::Completed
-            }
-            (SearchResult::SwappedNode(left_node), SearchResult::NotFound) => {
-                swap(left_node, root);
-                SearchResult::Completed
-            }
-            (SearchResult::NotFound, SearchResult::SwappedNode(right_node)) => {
-                swap(right_node, root);
-                SearchResult::Completed
-            }
-            (SearchResult::NotFound, SearchResult::NotFound) => SearchResult::NotFound,
-            (_, _) => unreachable!("All SearchResult::Completed should already have been returned"),
-        }
-    }
-
     pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        Self::recover_tree_helper(root.clone(), None, None);
+        // Based on solution from https://leetcode.com/problems/recover-binary-search-tree/solutions/1964742/rust-implementation-with-and-without-additional-memory/?languageTags=rust
+
+        // TLDR: Find first out of place value in an in place traversal and find the place where it must go to not be wrong compared to the node after it
+        let mut current_node = root.clone();
+        let mut previous_node: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut left_swap_node: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut right_swap_node: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut stack = vec![];
+
+        while !stack.is_empty() || current_node.is_some() {
+            // Go to leftmost node if on a new node (like root or first time visiting a right child)
+            while current_node.is_some() {
+                stack.push(current_node.clone());
+                current_node = current_node.clone().unwrap().borrow().left.clone();
+            }
+            current_node = stack
+                .pop()
+                .expect("Either stack already had values or current_node at start was added to it");
+
+            // Perform action
+            if left_swap_node.is_none() {
+                // Looking for left side of swap
+                if previous_node.is_some()
+                    && previous_node.as_ref().unwrap().borrow().val
+                        > current_node.as_ref().unwrap().borrow().val
+                {
+                    left_swap_node = previous_node;
+                }
+            } else {
+                // Looking for where the left node needs to go to
+                if left_swap_node.as_ref().unwrap().borrow().val
+                    < current_node.as_ref().unwrap().borrow().val
+                {
+                    // Needs to go in previous position
+                    right_swap_node = previous_node.clone();
+                    break;
+                }
+            }
+
+            // Go to next node
+            previous_node = current_node;
+            current_node = previous_node.as_ref().unwrap().borrow().right.clone();
+        }
+
+        // Handle case where it goes at the end of the in order traversal
+        if right_swap_node.is_none() {
+            right_swap_node = previous_node;
+        }
+
+        match (left_swap_node, right_swap_node) {
+            (Some(a), Some(b)) => mem::swap(&mut a.borrow_mut().val, &mut b.borrow_mut().val),
+            (_, _) => unreachable!(
+                "Question asserts that there will always be a swap that needs to be made"
+            ),
+        }
     }
 }
 
@@ -141,6 +111,16 @@ mod tests {
         let input: TreeRoot = "[2,3,1]".into();
         let mut input: Option<Rc<RefCell<TreeNode>>> = input.into();
         let expected: TreeRoot = "[2,1,3]".into();
+        Solution::recover_tree(&mut input);
+        assert_eq!(expected, input.into());
+    }
+
+    #[test]
+    fn case4() {
+        // To test break statement
+        let input: TreeRoot = "[2,3,1,null,null,null,5]".into();
+        let mut input: Option<Rc<RefCell<TreeNode>>> = input.into();
+        let expected: TreeRoot = "[2,1,3,null,null,null,5]".into();
         Solution::recover_tree(&mut input);
         assert_eq!(expected, input.into());
     }
