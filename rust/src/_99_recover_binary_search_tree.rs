b8877eeda4fb79f308de0fb883835a36c78a4cb7
Che <43485962+c-git@users.noreply.github.com>
2023-03-23 23:14:48 -0400

    Sub 1

--- rust/src/_99_recover_binary_search_tree.rs
+++ rust/src/_99_recover_binary_search_tree.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -22,34 +21,71 @@ use std::rc::Rc;
 // }
 use crate::helper::TreeNode;
 
+enum SearchResult {
+    SwappedNode(Rc<RefCell<TreeNode>>),
+    NotFound,
+    Completed,
+}
+
 impl Solution {
-    fn is_valid_bst_in_range(
+    fn recover_tree_helper(
         root: Option<Rc<RefCell<TreeNode>>>,
         min: Option<i32>,
         max: Option<i32>,
-    ) -> bool {
-        // Based on Fasil's idea
-        if let Some(root) = root {
-            let root = root.borrow();
-            if let Some(min) = min {
-                if root.val <= min {
-                    return false;
-                }
-            }
-            if let Some(max) = max {
-                if root.val >= max {
-                    return false;
-                }
-            }
-            Self::is_valid_bst_in_range(root.left.clone(), min, Some(root.val))
-                && Self::is_valid_bst_in_range(root.right.clone(), Some(root.val), max)
+    ) -> SearchResult {
+        let root = if let Some(val) = root {
+            val
         } else {
-            true
+            return SearchResult::NotFound;
+        };
+
+        let root_val = root.borrow().val;
+
+        if let Some(min) = min {
+            if root_val <= min {
+                return SearchResult::SwappedNode(root.clone());
+            }
         }
+        if let Some(max) = max {
+            if root_val >= max {
+                return SearchResult::SwappedNode(root.clone());
+            }
+        }
+
+        let left = root.borrow().left.clone();
+        match Self::recover_tree_helper(left, min, Some(root_val)) {
+            SearchResult::SwappedNode(swapped_node) => {
+                return if swapped_node.borrow().val > root_val {
+                    root.borrow_mut().val = swapped_node.borrow().val;
+                    swapped_node.borrow_mut().val = root_val;
+                    SearchResult::Completed
+                } else {
+                    SearchResult::SwappedNode(swapped_node) // Swap needs to happen higher up
+                };
+            }
+            SearchResult::NotFound => { /* Do nothing check other child */ }
+            SearchResult::Completed => return SearchResult::Completed,
+        }
+
+        let right = root.borrow().right.clone();
+        let result = match Self::recover_tree_helper(right, Some(root_val), max) {
+            SearchResult::SwappedNode(swapped_node) => {
+                if swapped_node.borrow().val < root_val {
+                    root.borrow_mut().val = swapped_node.borrow().val;
+                    swapped_node.borrow_mut().val = root_val;
+                    SearchResult::Completed
+                } else {
+                    SearchResult::SwappedNode(swapped_node) // Swap needs to happen higher up
+                }
+            }
+            SearchResult::NotFound => SearchResult::NotFound,
+            SearchResult::Completed => SearchResult::Completed,
+        };
+        result
     }
 
     pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        Self::is_valid_bst_in_range(root.clone(), None, None);
+        Self::recover_tree_helper(root.clone(), None, None);
     }
 }
 
