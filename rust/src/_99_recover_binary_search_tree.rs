426ed670bb5919d0ce717fe4526fa3624dd12a7f
Che <43485962+c-git@users.noreply.github.com>
2023-03-23 23:58:17 -0400

    Sub 2

--- rust/src/_99_recover_binary_search_tree.rs
+++ rust/src/_99_recover_binary_search_tree.rs
@@ -21,6 +21,7 @@ use std::rc::Rc;
 // }
 use crate::helper::TreeNode;
 
+#[derive(Eq, PartialEq, Debug)]
 enum SearchResult {
     SwappedNode(Rc<RefCell<TreeNode>>),
     NotFound,
@@ -52,36 +53,56 @@ impl Solution {
             }
         }
 
-        let left = root.borrow().left.clone();
-        match Self::recover_tree_helper(left, min, Some(root_val)) {
-            SearchResult::SwappedNode(swapped_node) => {
-                return if swapped_node.borrow().val > root_val {
-                    root.borrow_mut().val = swapped_node.borrow().val;
-                    swapped_node.borrow_mut().val = root_val;
-                    SearchResult::Completed
-                } else {
-                    SearchResult::SwappedNode(swapped_node) // Swap needs to happen higher up
-                };
-            }
-            SearchResult::NotFound => { /* Do nothing check other child */ }
-            SearchResult::Completed => return SearchResult::Completed,
+        fn swap(a: Rc<RefCell<TreeNode>>, b: Rc<RefCell<TreeNode>>) {
+            let temp = a.borrow().val;
+            a.borrow_mut().val = b.borrow().val;
+            b.borrow_mut().val = temp;
         }
 
-        let right = root.borrow().right.clone();
-        let result = match Self::recover_tree_helper(right, Some(root_val), max) {
+        let left = root.borrow().left.clone();
+        let left_result = match Self::recover_tree_helper(left, min, Some(root_val)) {
             SearchResult::SwappedNode(swapped_node) => {
-                if swapped_node.borrow().val < root_val {
-                    root.borrow_mut().val = swapped_node.borrow().val;
-                    swapped_node.borrow_mut().val = root_val;
-                    SearchResult::Completed
+                if swapped_node.borrow().val > root_val {
+                    SearchResult::SwappedNode(swapped_node)
                 } else {
-                    SearchResult::SwappedNode(swapped_node) // Swap needs to happen higher up
+                    return SearchResult::SwappedNode(swapped_node); // Swap needs to happen higher up
                 }
             }
             SearchResult::NotFound => SearchResult::NotFound,
-            SearchResult::Completed => SearchResult::Completed,
+            SearchResult::Completed => return SearchResult::Completed,
         };
-        result
+
+        let right = root.borrow().right.clone();
+        let right_result = match Self::recover_tree_helper(right, Some(root_val), max) {
+            SearchResult::SwappedNode(swapped_node) => {
+                if swapped_node.borrow().val < root_val {
+                    SearchResult::SwappedNode(swapped_node)
+                } else {
+                    assert_eq!(left_result, SearchResult::NotFound); // Based on question stating there can only be 1
+                    return SearchResult::SwappedNode(swapped_node); // Swap needs to happen higher up
+                }
+            }
+            SearchResult::NotFound => SearchResult::NotFound,
+            SearchResult::Completed => return SearchResult::Completed,
+        };
+
+        match (left_result, right_result) {
+            (SearchResult::SwappedNode(left_node), SearchResult::SwappedNode(right_node)) => {
+                // Swap the children nodes
+                swap(left_node, right_node);
+                SearchResult::Completed
+            }
+            (SearchResult::SwappedNode(left_node), SearchResult::NotFound) => {
+                swap(left_node, root);
+                SearchResult::Completed
+            }
+            (SearchResult::NotFound, SearchResult::SwappedNode(right_node)) => {
+                swap(right_node, root);
+                SearchResult::Completed
+            }
+            (SearchResult::NotFound, SearchResult::NotFound) => SearchResult::NotFound,
+            (_, _) => unreachable!("All SearchResult::Completed should already have been returned"),
+        }
     }
 
     pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
@@ -111,6 +132,15 @@ mod tests {
         let input: TreeRoot = "[3,1,4,null,null,2]".into();
         let mut input: Option<Rc<RefCell<TreeNode>>> = input.into();
         let expected: TreeRoot = "[2,1,4,null,null,3]".into();
+        Solution::recover_tree(&mut input);
+        assert_eq!(expected, input.into());
+    }
+
+    #[test]
+    fn case3() {
+        let input: TreeRoot = "[2,3,1]".into();
+        let mut input: Option<Rc<RefCell<TreeNode>>> = input.into();
+        let expected: TreeRoot = "[2,1,3]".into();
         Solution::recover_tree(&mut input);
         assert_eq!(expected, input.into());
     }
