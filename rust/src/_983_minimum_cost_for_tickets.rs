ae0f136146134539d2143715d34c4e33bf1e1e2d
Che <43485962+c-git@users.noreply.github.com>
2023-03-28 11:14:00 -0400

    Sub 4

--- rust/src/_983_minimum_cost_for_tickets.rs
+++ rust/src/_983_minimum_cost_for_tickets.rs
@@ -1,42 +1,32 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn mincost_tickets(days: Vec<i32>, costs: Vec<i32>) -> i32 {
+        // Use DP and calculate best solution for each index starting from the latest day and going forward,
+        // then can use previously calculated solutions to calculate the one before it until we get to the start then we've solved it
         debug_assert!(costs.len() == 3, "As per constraint in question");
         let costs = vec![(1, costs[0]), (7, costs[1]), (30, costs[2])];
-        let mut memo = HashMap::new();
-        Self::mincost_tickets_helper(&days[..], &costs, &mut memo)
-    }
 
-    fn mincost_tickets_helper(
-        days: &[i32],
-        costs: &[(i32, i32)],
-        memo: &mut HashMap<i32, i32>,
-    ) -> i32 {
-        if days.is_empty() {
-            return 0;
-        }
-        let n = days.len() as i32;
-        if let Some(result) = memo.get(&n) {
-            return *result;
-        }
+        // Create table with best values and initialize all values to 0
+        let mut table = vec![i32::MAX; days.len() + 1]; // Plus one for the set with no days
+        table[days.len()] = 0; // Initialize last value to 0 as there are no days and thus no cost
 
-        let mut result = i32::MAX;
-        let first_day = &days[0];
-        for (duration, cost) in costs.iter() {
-            let mut covered_days = days.len(); // Assume all are covered then loop to see if that does not hold (if not found then it does hold)
-            for (i, day) in days.iter().enumerate() {
-                if day >= &(first_day + duration) {
-                    covered_days = i;
-                    break;
+        for day in (0..days.len()).rev() {
+            let first_date = &days[day];
+            for (duration, cost) in costs.iter() {
+                // Assume all are covered then loop to see if that does not hold (if not found then it does hold).
+                // days.len() is valid index because there is an extra slot with a 0 to be used as a base case
+                let mut next_uncovered_index = days.len();
+                for (i, date) in days[day..].iter().enumerate() {
+                    if date >= &(first_date + duration) {
+                        next_uncovered_index = i + day;
+                        break;
+                    }
                 }
+                let option_cost = *cost + table[next_uncovered_index];
+                table[day] = table[day].min(option_cost);
             }
-            let option_cost =
-                *cost + Self::mincost_tickets_helper(&days[covered_days..], costs, memo);
-            result = result.min(option_cost);
         }
-        memo.insert(n, result);
-        result
+
+        table[0] // Question constraint guarantees that there will be at least one day in the input
     }
 }
 
