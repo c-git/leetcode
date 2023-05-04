d3d8319915c0228ba81d9e784ab9b55cdd1a7aa9
Che <43485962+c-git@users.noreply.github.com>
2023-05-04 17:50:23 -0400

    Sub 2
    Attempt to speed up execution and reduce memory usage

--- rust/src/_1491_average_salary_excluding_the_minimum_and_maximum_salary.rs
+++ rust/src/_1491_average_salary_excluding_the_minimum_and_maximum_salary.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn average(salary: Vec<i32>) -> f64 {
         debug_assert!(salary.len() >= 3);
-        let mut sum = 0.0;
-        let mut min = f64::MAX;
-        let mut max = f64::MIN;
-        for sal in &salary {
-            let sal = *sal as f64;
+        // Ok to use i32 to sum because max total salaries is 10^8
+        let mut sum = 0;
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+        for &sal in &salary {
             sum += sal;
             if min > sal {
                 min = sal;
@@ -15,7 +15,7 @@ impl Solution {
             }
         }
         sum -= min + max;
-        sum / (salary.len() - 2) as f64
+        sum as f64 / (salary.len() - 2) as f64
     }
 }
 
@@ -36,6 +36,14 @@ mod tests {
     fn case2() {
         let input = vec![1000, 2000, 3000];
         let expected = 2000.0;
+        let actual = Solution::average(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case3() {
+        let input = vec![1_000_000; 100];
+        let expected = 1_000_000.0;
         let actual = Solution::average(input);
         assert_eq!(actual, expected);
     }
