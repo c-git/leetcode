6cdead544506c097161958f0d16c4360d4c6075e
Che <43485962+c-git@users.noreply.github.com>
2023-05-07 09:06:31 -0400

    Sub 2

--- rust/src/_1964_find_the_longest_valid_obstacle_course_at_each_position.rs
+++ rust/src/_1964_find_the_longest_valid_obstacle_course_at_each_position.rs
@@ -1,21 +1,15 @@
 impl Solution {
     pub fn longest_obstacle_course_at_each_position(obstacles: Vec<i32>) -> Vec<i32> {
         let mut result: Vec<i32> = Vec::with_capacity(obstacles.len());
-        for (i, curr_height) in obstacles.iter().enumerate() {
-            let len = 1 + obstacles
-                .iter()
-                .take(i)
-                .enumerate()
-                .rev()
-                .find_map(|(obstacle_index, obstacle_height)| {
-                    if obstacle_height <= curr_height {
-                        Some(result[obstacle_index])
-                    } else {
-                        None
-                    }
-                })
-                .unwrap_or_default();
-            result.push(len);
+        for (outer_index, outer_height) in obstacles.iter().enumerate() {
+            let mut max_prev_len = 0;
+            for (inner_index, inner_height) in obstacles.iter().take(outer_index).enumerate().rev()
+            {
+                if inner_height <= outer_height && max_prev_len < result[inner_index] {
+                    max_prev_len = result[inner_index]
+                }
+            }
+            result.push(max_prev_len + 1);
         }
         result
     }
@@ -46,6 +40,14 @@ mod tests {
     fn case3() {
         let input = vec![3, 1, 5, 6, 4, 2];
         let expected = vec![1, 1, 2, 3, 2, 2];
+        let actual = Solution::longest_obstacle_course_at_each_position(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case4() {
+        let input = vec![5, 1, 5, 5, 1, 3, 4, 5, 1, 4];
+        let expected = vec![1, 1, 2, 3, 2, 3, 4, 5, 3, 5];
         let actual = Solution::longest_obstacle_course_at_each_position(input);
         assert_eq!(actual, expected);
     }
