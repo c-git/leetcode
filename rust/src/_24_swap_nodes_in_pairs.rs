af25a4d416872cb39f2a901c25c21cade1cf9a47
Che <43485962+c-git@users.noreply.github.com>
2023-05-15 23:54:28 -0400

    24. Swap Nodes in Pairs

---
+++ rust/src/_24_swap_nodes_in_pairs.rs
@@ -0,0 +1,76 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn swap_pairs(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut left = &mut head;
+        while left.is_some() {
+            let mut right = left.as_mut().unwrap().next.take();
+            if right.is_some() {
+                left.as_mut().unwrap().next = right.as_mut().unwrap().next.take();
+                let temp = left.take();
+                *left = right;
+                left.as_mut().unwrap().next = temp;
+                left = &mut left.as_mut().unwrap().next.as_mut().unwrap().next;
+            } else {
+                break;
+            }
+        }
+        head
+    }
+}
+
+use crate::helper::ListNode;
+
+struct Solution;
+#[cfg(test)]
+mod tests {
+    use crate::helper::ListHead;
+
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let input: ListHead = vec![1, 2, 3, 4].into();
+        let expected: ListHead = vec![2, 1, 4, 3].into();
+        let actual = Solution::swap_pairs(input.into());
+        assert_eq!(actual, expected.into());
+    }
+
+    #[test]
+    fn case2() {
+        let input: ListHead = vec![].into();
+        let expected: ListHead = vec![].into();
+        let actual = Solution::swap_pairs(input.into());
+        assert_eq!(actual, expected.into());
+    }
+
+    #[test]
+    fn case3() {
+        let input: ListHead = vec![1].into();
+        let expected: ListHead = vec![1].into();
+        let actual = Solution::swap_pairs(input.into());
+        assert_eq!(actual, expected.into());
+    }
+
+    #[test]
+    fn case4() {
+        let input: ListHead = vec![1, 2, 3, 4, 5].into();
+        let expected: ListHead = vec![2, 1, 4, 3, 5].into();
+        let actual = Solution::swap_pairs(input.into());
+        assert_eq!(actual, expected.into());
+    }
+}
