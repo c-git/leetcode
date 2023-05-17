3bb8a1612072938697842e7cb658c38d6c71509a
Che <43485962+c-git@users.noreply.github.com>
2023-05-17 11:03:39 -0400

    2130. Maximum Twin Sum of a Linked List

--- rust/src/_1008_construct_binary_search_tree_from_preorder_traversal.rs
+++ rust/src/_1008_construct_binary_search_tree_from_preorder_traversal.rs
@@ -19,9 +19,49 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+// class Solution:
+//     def build_bst(
+//         self, min: int, max: int, preorder: list[int], index: int
+//     ) -> tuple[TreeNode | None, int]:
+//         if index == len(preorder):
+//             return None, index
+//         value = preorder[index]
+//         if value < min or value > max:
+//             return None, index
+//         root = TreeNode(value)
+//         index += 1
+//         root.left, index = self.build_bst(min, value - 1, preorder, index)
+//         root.right, index = self.build_bst(value + 1, max, preorder, index)
+//         return root, index
+
+//     def bstFromPreorder(self, preorder: list[int]) -> TreeNode | None:  # noqa: N802
+//         return self.build_bst(0, 10**8 + 1, preorder, 0)[0]
+
 impl Solution {
     pub fn bst_from_preorder(preorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        todo!()
+        // Source: nasser python solution conversion
+        Self::build_bst(0, 100_000_001, &preorder, 0).0
+    }
+
+    fn build_bst(
+        min: i32,
+        max: i32,
+        preorder: &[i32],
+        mut index: usize,
+    ) -> (Option<Rc<RefCell<TreeNode>>>, usize) {
+        if index == preorder.len() {
+            return (None, index);
+        }
+
+        let value = preorder[index];
+        if value < min || value > max {
+            return (None, index);
+        }
+        let mut root = TreeNode::new(value);
+        index += 1;
+        (root.left, index) = Self::build_bst(min, value - 1, preorder, index);
+        (root.right, index) = Self::build_bst(value + 1, max, preorder, index);
+        (Some(Rc::new(RefCell::new(root))), index)
     }
 }
 
