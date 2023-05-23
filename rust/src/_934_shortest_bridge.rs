4fca765362086a296a22c840eca34c80d21636f0
Che <43485962+c-git@users.noreply.github.com>
2023-05-23 19:07:15 -0400

    934. Sub 1

--- rust/src/_934_shortest_bridge.rs
+++ rust/src/_934_shortest_bridge.rs
@@ -1,5 +1,9 @@
+use std::collections::BinaryHeap;
+
 use CellType::*;
 use Direction::*;
+
+type Position = (i32, i32); //Uses i32 to be able to subtract below 0 to simplify code
 
 #[derive(Debug, PartialEq, Eq)]
 enum CellType {
@@ -8,8 +12,8 @@ enum CellType {
     Visited,
 }
 
-impl From<&i32> for CellType {
-    fn from(value: &i32) -> Self {
+impl From<i32> for CellType {
+    fn from(value: i32) -> Self {
         match value {
             0 => Water,
             1 => Land,
@@ -36,7 +40,7 @@ enum Direction {
     Right,
 }
 
-impl From<Direction> for (i32, i32) {
+impl From<Direction> for Position {
     fn from(value: Direction) -> Self {
         match value {
             Up => (-1, 0),
@@ -52,10 +56,10 @@ impl Direction {
         [Up, Down, Left, Right]
     }
 
-    fn neighbours(pos: (i32, i32), n: i32) -> Vec<(i32, i32)> {
+    fn neighbours(pos: Position, n: i32) -> Vec<Position> {
         let mut result = Vec::with_capacity(4);
         for direction in Self::all() {
-            let dir: (i32, i32) = direction.into();
+            let dir: Position = direction.into();
             let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
             if new_pos.0 >= 0 && new_pos.0 < n && new_pos.1 >= 0 && new_pos.1 < n {
                 result.push(new_pos);
@@ -65,7 +69,42 @@ impl Direction {
     }
 }
 
+struct CellReference {
+    pos: Position,
+    distance: i32,
+}
+
+impl CellReference {
+    fn new(pos: Position) -> Self {
+        Self { pos, distance: 1 }
+    }
+
+    fn new_with_distance(pos: Position, distance: i32) -> CellReference {
+        Self { pos, distance }
+    }
+}
+
+impl PartialEq for CellReference {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance == other.distance
+    }
+}
+impl Eq for CellReference {}
+
+impl PartialOrd for CellReference {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.distance.partial_cmp(&self.distance)
+    }
+}
+
+impl Ord for CellReference {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
 impl Solution {
+    // After reading editorial (There was no better way than what I could think of...)
     pub fn shortest_bridge(mut grid: Vec<Vec<i32>>) -> i32 {
         let n = grid.len() as i32;
 
@@ -80,7 +119,7 @@ impl Solution {
         let mut top_left_edge = (0, 0);
         'outer: for (row_index, row) in grid.iter().enumerate() {
             for (col_index, cell_value) in row.iter().enumerate() {
-                let cell_type: CellType = cell_value.into();
+                let cell_type: CellType = (*cell_value).into();
                 if cell_type == Land {
                     top_left_edge = (row_index as i32, col_index as i32);
                     break 'outer;
@@ -88,14 +127,24 @@ impl Solution {
             }
         }
 
-        // Mark first island all as visited and track all edges with water
+        // Mark first island all as visited and add all adjacent water to the BFS queue
+        let mut bfs_queue = BinaryHeap::new();
         grid[top_left_edge.0 as usize][top_left_edge.1 as usize] = Visited.into();
         let mut stack = vec![top_left_edge];
         while let Some(pos) = stack.pop() {
             for neighbour in Direction::neighbours(pos, n) {
-                if grid[neighbour.0 as usize][neighbour.1 as usize] == Land.into() {
-                    grid[neighbour.0 as usize][neighbour.1 as usize] = Visited.into();
-                    stack.push(neighbour);
+                let neighbour_value = &mut grid[neighbour.0 as usize][neighbour.1 as usize];
+                let neighbour_type: CellType = (*neighbour_value).into();
+                match neighbour_type {
+                    Land => {
+                        *neighbour_value = Visited.into();
+                        stack.push(neighbour); // Record this cell to check it's neighbours for more land
+                    }
+                    Water => {
+                        *neighbour_value = Visited.into();
+                        bfs_queue.push(CellReference::new(neighbour)); // Record water found for BFS later
+                    }
+                    _ => {}
                 }
             }
         }
@@ -104,9 +153,36 @@ impl Solution {
             for row in grid.iter() {
                 println!("{row:?}")
             }
+            println!()
         }
 
-        todo!()
+        // Walk out from edges of first island until we find the second island (and well the answer)
+        while let Some(CellReference { pos, distance }) = bfs_queue.pop() {
+            if grid[pos.0 as usize][pos.1 as usize] == Land.into() {
+                return distance;
+            }
+            for neighbour in Direction::neighbours(pos, n) {
+                let neighbour_value = &mut grid[neighbour.0 as usize][neighbour.1 as usize];
+                let neighbour_type: CellType = (*neighbour_value).into();
+                match neighbour_type {
+                    Land => {
+                        return distance;
+                    }
+                    Water => {
+                        *neighbour_value = Visited.into();
+                        bfs_queue.push(CellReference::new_with_distance(neighbour, distance + 1));
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if cfg!(debug_assertions) {
+            for row in grid.iter() {
+                println!("{row:?}")
+            }
+        }
+
+        unreachable!("Constraint that second island exists")
     }
 }
 
