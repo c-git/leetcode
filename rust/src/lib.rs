c83463be040e2d65c8412ecaaf1677450659b3bf
Che <43485962+c-git@users.noreply.github.com>
2023-05-24 15:21:40 -0400

    Add new stub for rstest

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,7 +5,7 @@
 // For an example of linked lists see _2_add_two_numbers
 mod helper;
 
-// Stub for creating test cases
+// Stub for creating test cases with no crates
 
 // struct Solution;
 // #[cfg(test)]
@@ -16,6 +16,22 @@ mod helper;
 //     fn case1() {
 //         let input = ;
 //         let expected = ;
+//         let actual = Solution::(input);
+//         assert_eq!(actual, expected);
+//     }
+// }
+
+// Stub if using rstest
+
+// struct Solution;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use rstest::rstest;
+
+//     #[rstest]
+//     #[case(,)]
+//     fn case(#[case] input: , #[case] expected: ) {
 //         let actual = Solution::(input);
 //         assert_eq!(actual, expected);
 //     }
