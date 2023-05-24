dc39fb0601dd02ab8af9b21bc34a12697aa89e93
Che <43485962+c-git@users.noreply.github.com>
2023-05-23 22:26:50 -0400

    347. Top K Frequent Elements

---
+++ rust/src/_347_top_k_frequent_elements.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let k = k as usize;
+        let mut freq = HashMap::new();
+        for num in nums {
+            let entry = freq.entry(num).or_insert(0);
+            *entry += 1;
+        }
+
+        let mut result = Vec::with_capacity(k + 1);
+
+        for (key, value) in freq {
+            result.push((value, key));
+            result.sort_unstable_by(|(a, _), (b, _)| b.cmp(a)); // This is fine because the solution is unique
+            if result.len() > k {
+                result.pop();
+            }
+        }
+
+        result.into_iter().map(|(_, k)| k).collect()
+    }
+}
+
+struct Solution;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn validate(mut actual: Vec<i32>, mut expected: Vec<i32>) {
+        actual.sort_unstable();
+        expected.sort_unstable();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case1() {
+        let nums = vec![1, 1, 1, 2, 2, 3];
+        let k = 2;
+        let expected = vec![1, 2];
+        let actual = Solution::top_k_frequent(nums, k);
+        validate(actual, expected);
+    }
+
+    #[test]
+    fn case2() {
+        let nums = vec![1];
+        let k = 1;
+        let expected = vec![1];
+        let actual = Solution::top_k_frequent(nums, k);
+        validate(actual, expected);
+    }
+}
