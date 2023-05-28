b574dbffbe979bcb73143d9083ad1ed41fdacabd
Che <43485962+c-git@users.noreply.github.com>
2023-05-27 20:58:05 -0400

    1140. Sub 3
    Change out hashmap to remove cost of hashing

--- rust/src/_1140_stone_game_ii.rs
+++ rust/src/_1140_stone_game_ii.rs
@@ -1,19 +1,18 @@
-use std::collections::HashMap;
 impl Solution {
     // Source: sak96
     pub fn stone_game_ii_helper(
         piles: &[i32],
         sum_piles: i32,
         m: usize,
-        memory: &mut HashMap<(usize, usize), i32>,
+        memo: &mut Vec<Vec<Option<i32>>>,
     ) -> i32 {
         debug_assert_eq!(sum_piles, piles.iter().sum::<i32>());
         let key = (piles.len(), m);
         let value = if piles.len() <= 2 * m {
             // take all you can and give nothing back
             sum_piles
-        } else if memory.contains_key(&key) {
-            return *memory.get(&key).expect("already checked");
+        } else if let Some(value) = memo[key.0][key.1] {
+            return value;
         } else {
             // pick move that maximizes final score
             // final score = current piles sum - opponents best move
@@ -22,18 +21,20 @@ impl Solution {
             for i in 1..=2 * m {
                 sum_remaining -= piles[i - 1];
                 let score = sum_piles
-                    - Self::stone_game_ii_helper(&piles[i..], sum_remaining, i.max(m), memory);
+                    - Self::stone_game_ii_helper(&piles[i..], sum_remaining, i.max(m), memo);
                 max_score = max_score.max(score);
             }
             max_score
         };
-        memory.insert(key, value);
+        memo[key.0][key.1] = Some(value);
         value
     }
 
     pub fn stone_game_ii(piles: Vec<i32>) -> i32 {
         let sum: i32 = piles.iter().sum();
-        Self::stone_game_ii_helper(&piles, sum, 1, &mut HashMap::new())
+        let n = piles.len();
+        let mut memo = vec![vec![None; n + 1]; n + 1];
+        Self::stone_game_ii_helper(&piles, sum, 1, &mut memo)
     }
 }
 
