73de0b06e2fab9ef2639dd078a654db5619e45c8
Che <43485962+c-git@users.noreply.github.com>
2023-05-29 20:42:56 -0400

    29. Sub 2
    Deal with negative overflows

--- rust/src/_29_divide_two_integers.rs
+++ rust/src/_29_divide_two_integers.rs
@@ -1,28 +1,16 @@
 impl Solution {
-    pub fn divide(mut dividend: i32, mut divisor: i32) -> i32 {
-        let is_negative = match (dividend, divisor) {
-            (a, b) if a >= 0 && b >= 0 => false,
-            (a, b) if a >= 0 && b < 0 => {
-                divisor = -divisor;
-                true
-            }
-            (a, b) if a < 0 && b >= 0 => {
-                dividend = -dividend;
-                true
-            }
-            (a, b) if a < 0 && b < 0 => {
-                dividend = -dividend;
-                divisor = -divisor;
-                false
-            }
+    pub fn divide(dividend: i32, divisor: i32) -> i32 {
+        let (is_negative, dividend, divisor) = match (dividend, divisor) {
+            (a, b) if a >= 0 && b >= 0 => (false, a as u32, b as u32),
+            (a, b) if a >= 0 && b < 0 => (true, a as u32, -(b as i64) as u32),
+            (a, b) if a < 0 && b >= 0 => (true, -(a as i64) as u32, b as u32),
+            (a, b) if a < 0 && b < 0 => (false, -(a as i64) as u32, -(b as i64) as u32),
             (_, _) => unreachable!(),
         };
-        debug_assert!(dividend >= 0 && divisor >= 0);
 
         let dividend = dividend.to_string();
         let mut result = String::new();
         let mut carry = 0;
-        let divisor = divisor as u32;
         for c in dividend.chars() {
             carry = Self::mul10(carry) + c.to_digit(10).unwrap();
             let (quotient, remainder) = Self::divide_with_remainder(carry, divisor);
@@ -30,11 +18,12 @@ impl Solution {
             result.push_str(&quotient.to_string());
         }
 
-        let result: i32 = result.parse().unwrap();
-        if is_negative {
-            -result
-        } else {
-            result
+        let result: u32 = result.parse().unwrap();
+        match (is_negative, result) {
+            (false, r) if r >= i32::MAX as u32 => i32::MAX,
+            (false, r) => r as i32,
+            (true, r) if r > (i32::MAX as u32) => i32::MIN,
+            (true, r) => -(r as i32),
         }
     }
 
@@ -48,11 +37,11 @@ impl Solution {
     }
 
     fn mul10(carry: u32) -> u32 {
-        dbg!(if carry == 0 {
+        if carry == 0 {
             0
         } else {
             (1..10).fold(carry, |acc, _| acc + carry)
-        })
+        }
     }
 }
 
@@ -70,6 +59,7 @@ mod tests {
     #[case(400, 10, 40)]
     #[case(210, 3, 70)]
     #[case(6009, 3, 2003)]
+    #[case(-2147483648, -1, 2147483647)]
     fn case(#[case] dividend: i32, #[case] divisor: i32, #[case] expected: i32) {
         let actual = Solution::divide(dividend, divisor);
         assert_eq!(actual, expected);
