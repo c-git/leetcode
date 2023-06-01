de6cf85160f278e7470e3cde7c8f06f6fb8bcb25
Che <43485962+c-git@users.noreply.github.com>
2023-06-01 17:11:04 -0400

    1091. Sub 2

--- rust/src/_1091_shortest_path_in_binary_matrix.rs
+++ rust/src/_1091_shortest_path_in_binary_matrix.rs
@@ -14,6 +14,7 @@ impl Solution {
         }
 
         let n = grid.len();
+        let target = n - 1; // Target cell position index
         let mut is_visited = vec![vec![false; n]; n];
         let n = n as i32; // Converted here to reduce the overall number of conversions required
         let mut queue = VecDeque::new();
@@ -32,12 +33,12 @@ impl Solution {
                     for col_offset in -1..=1 {
                         let new_col = col + col_offset;
                         if (0..n).contains(&new_col) {
-                            if new_row == n - 1 && new_col == n - 1 {
-                                return new_distance;
-                            }
                             let new_row = new_row as usize;
                             let new_col = new_col as usize;
                             if !is_visited[new_row][new_col] && grid[new_row][new_col] == 0 {
+                                if new_row == target && new_col == target {
+                                    return new_distance;
+                                }
                                 is_visited[new_row][new_col] = true;
                                 queue.push_back(CellData {
                                     distance: new_distance,
@@ -64,6 +65,7 @@ mod tests {
     #[case(vec![vec![0,1],vec![1,0]],2)]
     #[case(vec![vec![0,0,0],vec![1,1,0],vec![1,1,0]],4)]
     #[case(vec![vec![1,0,0],vec![1,1,0],vec![1,1,0]],-1)]
+    #[case(vec![vec![0,0,0],vec![1,1,0],vec![1,1,1]],-1)]
     fn case(#[case] input: Vec<Vec<i32>>, #[case] expected: i32) {
         let actual = Solution::shortest_path_binary_matrix(input);
         assert_eq!(actual, expected);
