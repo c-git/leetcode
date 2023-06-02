147e53c9229986fe6ca64c29840c986275feca69
Che <43485962+c-git@users.noreply.github.com>
2023-06-02 11:47:47 -0400

    2101. Sub 1

--- rust/src/_2101_detonate_the_maximum_bombs.rs
+++ rust/src/_2101_detonate_the_maximum_bombs.rs
@@ -1,29 +1,72 @@
-impl Solution {
-    pub fn maximum_detonation(mut bombs: Vec<Vec<i32>>) -> i32 {
-        // General idea is that if we sort the bombs in ascending order of their blast radius then we can solve
-        // the problem for prefixes of the list and know that we are not missing anything because anything it can reach
-        // that we haven't checked yet can also reach it so we'll just blow up that one instead
-        // and once we extend the maximum of the previous list then we must get the overall maximum
+use std::cmp::max;
 
-        let mut dp = vec![1; bombs.len()];
-        bombs.sort_unstable_by_key(|x| x[2]);
-        for (this_ind, this_bomb) in bombs.iter().enumerate().skip(1) {
+struct Graph {
+    /// Node numbers index into the list of their edges
+    edges: Vec<Vec<usize>>,
+}
+
+impl Graph {
+    fn new(node_count: usize) -> Self {
+        Self {
+            edges: vec![vec![]; node_count],
+        }
+    }
+
+    fn add_edge(&mut self, src: usize, dst: usize) {
+        debug_assert!(!self.edges[src].contains(&dst));
+        self.edges[src].push(dst);
+    }
+
+    fn explosion_chain(&self, start_node: usize) -> i32 {
+        self.explosion_chain_helper(start_node, &mut vec![false; self.len()])
+    }
+
+    fn explosion_chain_helper(&self, start_node: usize, is_visited: &mut [bool]) -> i32 {
+        if is_visited[start_node] {
+            return 0;
+        } else {
+            is_visited[start_node] = true
+        }
+
+        let mut result = 1;
+        for &neighbour in self.edges[start_node].iter() {
+            result += self.explosion_chain_helper(neighbour, is_visited);
+        }
+        result
+    }
+
+    fn len(&self) -> usize {
+        self.edges.len()
+    }
+}
+
+impl Solution {
+    pub fn maximum_detonation(bombs: Vec<Vec<i32>>) -> i32 {
+        // Build Graph
+        let mut graph = Graph::new(bombs.len());
+        for (this_ind, this_bomb) in bombs.iter().enumerate() {
             let (this_x, this_y, r) = (this_bomb[0], this_bomb[1], this_bomb[2]);
-            let blast_radius = r as f32;
-            for (other_ind, other_bomb) in bombs.iter().enumerate().take(this_ind).rev() {
-                let (other_x, other_y) = (other_bomb[0], other_bomb[1]);
+            let this_blast_radius = r as f32;
+            for (other_ind, other_bomb) in bombs.iter().enumerate().skip(this_ind + 1) {
+                let (other_x, other_y, r) = (other_bomb[0], other_bomb[1], other_bomb[2]);
+                let other_blast_radius = r as f32;
                 let distance =
                     (((this_x - other_x).pow(2) + (this_y - other_y).pow(2)) as f32).sqrt();
-                if distance <= blast_radius {
-                    let candidate_count = 1 + dp[other_ind];
-                    if dp[this_ind] < candidate_count {
-                        dp[this_ind] = candidate_count;
-                    }
+                if distance <= this_blast_radius {
+                    graph.add_edge(this_ind, other_ind);
+                }
+                if distance <= other_blast_radius {
+                    graph.add_edge(other_ind, this_ind);
                 }
             }
         }
 
-        *dp.iter().max().unwrap()
+        // Find node with longest chain
+        let mut result = 1;
+        for i in 0..graph.len() {
+            result = max(result, graph.explosion_chain(i));
+        }
+        result
     }
 }
 
