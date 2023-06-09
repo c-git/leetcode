268652c0595c15cab1fa66e9d6a16990cbcf73d5
Che <43485962+c-git@users.noreply.github.com>
2023-06-09 19:05:37 -0400

    41. Sub 3

--- rust/src/_41_first_missing_positive.rs
+++ rust/src/_41_first_missing_positive.rs
@@ -1,41 +1,38 @@
-use std::mem::swap;
+use std::{mem::swap, ops::Add};
 
 impl Solution {
     pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        if n == 1 {
-            return if nums[0] == 1 { 2 } else { 1 };
-        }
-
         for i in 0..n {
-            if nums[i] != i as i32 {
-                Self::swap_sort(&mut nums, i);
+            let val = nums[i];
+            if val.saturating_sub(1) != i as i32 {
+                Self::swap_sort(&mut nums, val);
             }
         }
 
-        for (i, val) in nums.into_iter().enumerate().skip(1) {
-            if i as i32 != val {
-                return i as i32;
+        for (i, val) in nums.into_iter().enumerate() {
+            if i as i32 != val.saturating_sub(1) {
+                return i.add(1) as i32;
             }
         }
 
         // None missing must be n
-        n as i32
+        (n + 1) as i32
     }
 
-    /// Take the value at nums[index] and set that position in the array to the value if it
-    /// is a positive integer within the size of the array and repeating on the value
-    /// at that location as appropriate
-    fn swap_sort(nums: &mut [i32], index: usize) {
-        debug_assert_ne!(nums[index], index as i32);
-
-        let mut curr_value = nums[index];
+    /// Take the curr_value and set the matching position if it is
+    /// a positive integer within the size of the array and repeating this on the value
+    /// at the location that it is swapped with
+    fn swap_sort(nums: &mut [i32], mut curr_value: i32) {
         let n = nums.len() as i32;
-        while 0 <= curr_value && curr_value < n && nums[curr_value as usize] != curr_value {
+        while 0 < curr_value
+            && curr_value <= n
+            && nums[curr_value.saturating_sub(1) as usize] != curr_value
+        {
             // LI:
-            //  - curr_value is a valid index into nums because 0 <= index < n
+            //  - curr_value-1 is a valid index into nums because it is 0 < curr_value <= n implies 0 <= curr_value-1 < n
             //  - curr_value has somewhere else in the array it belongs
-            let target_index = curr_value as usize;
+            let target_index = curr_value.saturating_sub(1) as usize;
             swap(&mut curr_value, &mut nums[target_index]);
         }
     }
@@ -53,6 +50,7 @@ mod tests {
     #[case(vec![7,8,9,11,12],1)]
     #[case(vec![1,0,0],2)]
     #[case(vec![1],2)]
+    #[case(vec![2,1],3)]
     fn case(#[case] input: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::first_missing_positive(input);
         assert_eq!(actual, expected);
