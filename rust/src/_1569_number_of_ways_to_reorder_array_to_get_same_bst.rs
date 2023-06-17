80dd25da6bc22ec97ca823b882f2009654b3839e
One <43485962+c-git@users.noreply.github.com>
2023-06-17 14:47:39 -0400

    Revert "1569. Sub1"
    This reverts commit ae8a7e50b530f7c6b677cd6ce47204c7e0a2792c.

--- rust/src/_1569_number_of_ways_to_reorder_array_to_get_same_bst.rs
+++ rust/src/_1569_number_of_ways_to_reorder_array_to_get_same_bst.rs
@@ -1,49 +1,36 @@
 //! Solution for https://leetcode.com/problems/number-of-ways-to-reorder-array-to-get-same-bst
 //! 1569. Number of Ways to Reorder Array to Get Same BST
 
-type LargerInt = i64;
 impl Solution {
     // Based on sak96's approach and editorial
-    const MOD_BASE: LargerInt = 1_000_000_007;
+    const MOD_BASE: u64 = 1_000_000_007;
 
-    pub fn num_of_ways(nums: Vec<i32>) -> i32 {
+    pub fn num_of_ways(mut nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        let pascal_triangle = Self::generate_pascal_triangle(n);
-        (Self::num_of_ways_helper(nums, &pascal_triangle) - 1) as i32
+        Self::num_of_ways_helper(&mut nums, &Self::generate_pascal_triangle(n)) as i32 - 1
     }
 
     /// This function returns the number of ways this tree can be built (including the starting way)
-    pub fn num_of_ways_helper(nums: Vec<i32>, pascal_triangle: &[Vec<i64>]) -> i64 {
-        let n = nums.len();
-        if nums.len() < 3 {
+    pub fn num_of_ways_helper(nums: &mut [i32], pascal_triangle: &[Vec<i32>]) -> u64 {
+        if nums.len() <= 2 {
             // For a array of size 2 or less there are no changes that can be made because
             // the root if fixed and the root of the child will also be fixed and there will
             // can be no other child
             1
         } else {
-            let root = nums[0];
-
-            let mut left = vec![];
-            let mut right = vec![];
-            for num in nums.into_iter().skip(1) {
-                debug_assert_ne!(root, num);
-                if num < root {
-                    left.push(num)
-                } else {
-                    right.push(num)
-                }
-            }
-
-            let left_len = left.len();
-
-            let left_ways = Self::num_of_ways_helper(left, pascal_triangle) % Self::MOD_BASE;
-            let right_ways = Self::num_of_ways_helper(right, pascal_triangle) % Self::MOD_BASE;
-            (((left_ways * right_ways) % Self::MOD_BASE) * pascal_triangle[n - 1][left_len])
-                % Self::MOD_BASE
+            let (root, rest) = nums.split_at_mut(1); // Get rest of array less root (first node)
+            let n = rest.len();
+            rest.sort_by_key(|i| root[0] < *i); //Use stable sort to split values less than root for left and right child
+            let (left, right) = rest.split_at_mut(rest.partition_point(|i| root[0] > *i));
+            let mut result = pascal_triangle[n][left.len()] as u64;
+            result *= Self::num_of_ways_helper(left, pascal_triangle);
+            result %= Self::MOD_BASE;
+            result *= Self::num_of_ways_helper(right, pascal_triangle);
+            result % Self::MOD_BASE
         }
     }
 
-    fn generate_pascal_triangle(size: usize) -> Vec<Vec<LargerInt>> {
+    fn generate_pascal_triangle(size: usize) -> Vec<Vec<i32>> {
         // This function is mostly here to make testing easy
         let mut result = vec![vec![0; size]; size];
 
@@ -55,13 +42,13 @@ impl Solution {
         for row in 2..size {
             for col in 1..row {
                 result[row][col] =
-                    (result[row - 1][col - 1] + result[row - 1][col]) % Self::MOD_BASE;
+                    (result[row - 1][col - 1] + result[row - 1][col]) % Self::MOD_BASE as i32;
             }
         }
         result
     }
 
-    fn choose(n: usize, r: usize, pascal_triangle: &[Vec<LargerInt>]) -> LargerInt {
+    fn choose(n: usize, r: usize, pascal_triangle: &[Vec<i32>]) -> i32 {
         pascal_triangle[n][r]
     }
 }
@@ -70,8 +57,6 @@ struct Solution;
 
 #[cfg(test)]
 mod tests {
-    use std::vec;
-
     use super::*;
 
     use rstest::rstest;
@@ -81,10 +66,10 @@ mod tests {
     #[case(vec![3,4,5,1,2], 5)]
     #[case(vec![1,2,3], 0)]
     #[case(vec![
-        34, 45, 67, 21, 51, 58, 65, 42, 20, 6, 13, 18, 14, 15, 33, 7, 55, 57, 32, 48, 53, 62,
-        36, 47, 40, 26, 66, 11, 54, 8, 44, 56, 1, 23, 30, 27, 50, 10, 49, 61, 9, 41, 64, 4, 5,
-        39, 16, 22, 63, 35, 12, 52, 38, 24, 60, 43, 19, 3, 31, 25, 37, 59, 28, 29, 17, 2, 46,
-        ], 236400695)]
+            4, 58, 68, 45, 35, 60, 27, 62, 67, 42, 64, 31, 63, 17, 43, 40, 1, 3, 9, 48, 47, 24, 66,
+            37, 36, 12, 29, 5, 65, 46, 30, 57, 2, 21, 32, 55, 39, 53, 54, 11, 51, 7, 28, 13, 18,
+            61, 34, 16, 59, 10, 23, 14, 33, 49, 22, 56, 15, 25, 50, 41, 20, 38, 69, 19, 26, 6, 8,
+        ], 195951021)]
     fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::num_of_ways(nums);
         assert_eq!(actual, expected);
@@ -96,9 +81,8 @@ mod tests {
     #[case(100, 5, 75287520)]
     #[case(200, 5, 535650026)]
     #[case(200, 10, 151856252)]
-    fn choose(#[case] n: usize, #[case] r: usize, #[case] expected: LargerInt) {
+    fn choose(#[case] n: usize, #[case] r: usize, #[case] expected: i32) {
         let table = Solution::generate_pascal_triangle(n + 1);
         assert_eq!(Solution::choose(n, r, &table), expected);
-        let x = vec![];
     }
 }
