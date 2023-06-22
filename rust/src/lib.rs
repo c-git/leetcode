da223f3dcbcdcb320a08a8eb4128075d7141015d
One <43485962+c-git@users.noreply.github.com>
2023-06-22 19:20:56 -0400

    Interim instructions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,7 @@
 // Add new file as a module to make it run
 
 // TODO Add instructions on how to setup code to run and update readme
+// Interim note: Currently using cargo leet https://github.com/rust-practice/cargo-leet to generate the
+// stubs for new problems. Will update readme and notes here after cargo leet stabilizes a bit. Feel free
+// to reach out if you want to make use of my setup before I document it. (Opening an issue on github is a
+// fine way to get in touch)
