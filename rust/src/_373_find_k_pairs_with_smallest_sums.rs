a48150202f9224a414075b4c21aee0391ae85a35
One <43485962+c-git@users.noreply.github.com>
2023-06-27 23:19:03 -0400

    373. Sub 2

--- rust/src/_373_find_k_pairs_with_smallest_sums.rs
+++ rust/src/_373_find_k_pairs_with_smallest_sums.rs
@@ -1,31 +1,50 @@
 //! Solution for https://leetcode.com/problems/find-k-pairs-with-smallest-sums
 //! 373. Find K Pairs with Smallest Sums
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet},
+};
+
 impl Solution {
-    pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+    pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, mut k: i32) -> Vec<Vec<i32>> {
+        // After reading editorial
         let mut result = vec![];
-        let mut iter1 = nums1.into_iter().peekable();
-        let mut iter2 = nums2.into_iter().peekable();
-        let mut val1 = iter1.next().unwrap();
-        let mut val2 = iter2.next().unwrap();
-        for _ in 0..k {
-            // LI:  - val1, val2 is the next pair of values to add
-            //      -  There number of sum of items in iter1 and iter2 decreases each iteration
-            result.push(vec![val1, val2]);
-            match (iter1.peek(), iter2.peek()) {
-                (None, None) => break,
-                (None, Some(_)) => val2 = iter2.next().unwrap(),
-                (Some(_), None) => val1 = iter1.next().unwrap(),
-                (Some(v1), Some(v2)) => {
-                    if v1 < v2 {
-                        val1 = iter1.next().unwrap();
-                    } else {
-                        val2 = iter2.next().unwrap();
-                    }
-                }
+        let len1 = nums1.len();
+        let len2 = nums2.len();
+
+        let mut visited = HashSet::new();
+
+        let mut heap = BinaryHeap::from([Reverse((nums1[0] + nums2[0], 0, 0))]);
+        visited.insert((0, 0));
+
+        while k > 0 && !heap.is_empty() {
+            let Reverse((_sum, index1, index2)) = heap.pop().unwrap();
+            result.push(vec![nums1[index1], nums2[index2]]);
+            if index1 + 1 < len1 {
+                push_pair(index1 + 1, index2, &mut visited, &mut heap, &nums1, &nums2);
             }
+            if index2 + 1 < len2 {
+                push_pair(index1, index2 + 1, &mut visited, &mut heap, &nums1, &nums2);
+            }
+            k -= 1;
         }
         result
+    }
+}
+
+#[inline]
+fn push_pair(
+    a: usize,
+    b: usize,
+    visited: &mut HashSet<(usize, usize)>,
+    heap: &mut BinaryHeap<Reverse<(i32, usize, usize)>>,
+    nums1: &[i32],
+    nums2: &[i32],
+) {
+    if !visited.contains(&(a, b)) {
+        heap.push(Reverse((nums1[a] + nums2[b], a, b)));
+        visited.insert((a, b));
     }
 }
 
@@ -41,13 +60,16 @@ mod tests {
     #[case(vec![1,7,11], vec![2,4,6], 3, vec![vec![1,2],vec![1,4],vec![1,6]])]
     #[case(vec![1,1,2], vec![1,2,3], 2, vec![vec![1,1],vec![1,1]])]
     #[case(vec![1,2], vec![3], 3, vec![vec![1,3],vec![2,3]])]
+    #[case(vec![1,1,2], vec![1,2,3], 10, vec![vec![1,1],vec![1,1],vec![2,1],vec![1,2],vec![1,2],vec![2,2],vec![1,3],vec![1,3],vec![2,3]])]
     fn case(
         #[case] nums1: Vec<i32>,
         #[case] nums2: Vec<i32>,
         #[case] k: i32,
-        #[case] expected: Vec<Vec<i32>>,
+        #[case] mut expected: Vec<Vec<i32>>,
     ) {
-        let actual = Solution::k_smallest_pairs(nums1, nums2, k);
+        let mut actual = Solution::k_smallest_pairs(nums1, nums2, k);
+        actual.sort();
+        expected.sort();
         assert_eq!(actual, expected);
     }
 }
