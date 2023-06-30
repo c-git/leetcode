7014a89e5d453430e9ce91329f169a3edbed48b1
One <43485962+c-git@users.noreply.github.com>
2023-06-30 11:05:23 -0400

    264. Ugly Number II

---
+++ rust/src/_264_ugly_number_ii.rs
@@ -0,0 +1,51 @@
+//! Solution for https://leetcode.com/problems/ugly-number-ii
+//! 264. Ugly Number II
+
+use std::cmp::min;
+
+impl Solution {
+    pub fn nth_ugly_number(n: i32) -> i32 {
+        // Solution from LintCode
+        // version 1: O(n) scan
+        let n = n as usize;
+        let mut uglys = vec![1];
+        // p2, p3 & p5 share the same queue: uglys
+        let mut p2 = 0;
+        let mut p3 = 0;
+        let mut p5 = 0;
+        for i in 1..n {
+            let last_number = uglys[i - 1];
+            while uglys[p2] * 2 <= last_number {
+                p2 += 1;
+            }
+            while uglys[p3] * 3 <= last_number {
+                p3 += 1;
+            }
+            while uglys[p5] * 5 <= last_number {
+                p5 += 1;
+            }
+
+            uglys.push(min(uglys[p2] * 2, min(uglys[p3] * 3, uglys[p5] * 5)));
+        }
+        uglys[n - 1]
+    }
+}
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(10, 12)]
+    #[case(1, 1)]
+    fn case(#[case] n: i32, #[case] expected: i32) {
+        let actual = Solution::nth_ugly_number(n);
+        assert_eq!(actual, expected);
+    }
+}
