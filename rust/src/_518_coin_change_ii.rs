3bae137d03bfc243e1ea725750831d360c8baa73
One <43485962+c-git@users.noreply.github.com>
2023-08-11 22:56:29 -0400

    518. Sub 3

--- rust/src/_518_coin_change_ii.rs
+++ rust/src/_518_coin_change_ii.rs
@@ -1,6 +1,8 @@
 //! Solution for https://leetcode.com/problems/coin-change-ii
 //! 518. Coin Change II
 
+use std::mem;
+
 impl Solution {
     pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
         // Solution from editorial
@@ -8,23 +10,24 @@ impl Solution {
         let coins: Vec<usize> = coins.into_iter().map(|x| x as usize).collect();
         let n = coins.len();
 
+        // Only use two rows as only two are used at a time
         // Each row is indexed by the starting index of the coins you can use, each column is indexed by a target amount
-        let mut dp = vec![vec![0; amount + 1]; n + 1];
-        for row in dp.iter_mut() {
-            row[0] = 1;
-        }
+        let mut curr_row = vec![0; amount + 1]; // Row being calculated
+        curr_row[0] = 1;
+        let mut prev_row = curr_row.clone(); // Previous Row - Corresponds to the row that we last calculated which is when we used one higher index from coins
 
         for start_coin_idx in (0..n).rev() {
+            mem::swap(&mut curr_row, &mut prev_row); // Make current row be previous
             for target_val in 1..=amount {
                 if coins[start_coin_idx] > target_val {
-                    dp[start_coin_idx][target_val] = dp[start_coin_idx + 1][target_val]
+                    curr_row[target_val] = prev_row[target_val];
                 } else {
-                    dp[start_coin_idx][target_val] = dp[start_coin_idx + 1][target_val]
-                        + dp[start_coin_idx][target_val - coins[start_coin_idx]]
+                    curr_row[target_val] =
+                        prev_row[target_val] + curr_row[target_val - coins[start_coin_idx]];
                 }
             }
         }
-        dp[0][amount]
+        curr_row[amount]
     }
 }
 
