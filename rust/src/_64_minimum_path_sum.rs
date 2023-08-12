9707f82075fa76816a12ed46cecb0ba488f2d7f1
One <43485962+c-git@users.noreply.github.com>
2023-08-12 00:32:48 -0400

    64. Minimum Path Sum

---
+++ rust/src/_64_minimum_path_sum.rs
@@ -0,0 +1,38 @@
+//! Solution for https://leetcode.com/problems/minimum-path-sum
+//! 64. Minimum Path Sum
+
+use std::cmp::min;
+
+impl Solution {
+    pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
+        for col in 1..grid[0].len() {
+            grid[0][col] += grid[0][col - 1];
+        }
+
+        for row in 1..grid.len() {
+            for col in 1..grid[row].len() {
+                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1]);
+            }
+        }
+        *grid.last().unwrap().last().unwrap()
+    }
+}
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![vec![1,3,1],vec![1,5,1],vec![4,2,1]], 7)]
+    #[case(vec![vec![1,2,3],vec![4,5,6]], 12)]
+    fn case(#[case] grid: Vec<Vec<i32>>, #[case] expected: i32) {
+        let actual = Solution::min_path_sum(grid);
+        assert_eq!(actual, expected);
+    }
+}
