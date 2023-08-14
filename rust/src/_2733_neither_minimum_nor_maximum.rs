36d46d8353cfba669c0a487c78440aa77ce9b3cc
One <43485962+c-git@users.noreply.github.com>
2023-08-14 19:12:39 -0400

    2733. Sub 2

--- rust/src/_2733_neither_minimum_nor_maximum.rs
+++ rust/src/_2733_neither_minimum_nor_maximum.rs
@@ -1,23 +1,14 @@
 //! Solution for https://leetcode.com/problems/neither-minimum-nor-maximum
 //! 2733. Neither Minimum nor Maximum
 
-use std::collections::BTreeSet;
-
 impl Solution {
-    pub fn find_non_min_or_max(nums: Vec<i32>) -> i32 {
-        Self::find_non_min_or_max_(&nums).unwrap_or(-1)
-    }
-
-    pub fn find_non_min_or_max_(nums: &[i32]) -> Option<i32> {
-        let mut set = BTreeSet::new();
-        for &num in nums {
-            set.insert(num);
-            debug_assert!(set.len() <= 3);
-            if set.len() >= 3 {
-                return Some(*set.iter().nth(1).unwrap());
-            }
+    pub fn find_non_min_or_max(mut nums: Vec<i32>) -> i32 {
+        if nums.len() < 3 {
+            return -1; // Not enough values to not be min or max
         }
-        None // All numbers were either the min or the max
+        let relevant_nums = &mut nums[0..=2];
+        relevant_nums.sort_unstable();
+        relevant_nums[1]
     }
 }
 
@@ -35,6 +26,7 @@ mod tests {
     #[case(vec![3,2,1,4], 2)]
     #[case(vec![1,2], -1)]
     #[case(vec![2,1,3], 2)]
+    #[case(vec![2,40,8,1], 8)] // Both 2 and 8 are correct by my algorithm only looks at first 3
     fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::find_non_min_or_max(nums);
         assert_eq!(actual, expected);
