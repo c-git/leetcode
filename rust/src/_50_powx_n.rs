6525127d9ede111f91bdb8c3deb99cd128b1e427
One <43485962+c-git@users.noreply.github.com>
2023-08-21 09:20:08 -0400

    50. Sub 2

--- rust/src/_50_powx_n.rs
+++ rust/src/_50_powx_n.rs
@@ -3,17 +3,22 @@
 
 impl Solution {
     pub fn my_pow(x: f64, n: i32) -> f64 {
+        // After reading editorial
         if n == 0 {
             return 1.0;
         }
         if n < 0 {
-            return 1.0 / Self::my_pow(x, -n);
+            return 1.0 / (Self::my_pow(x, -(n + 1)) * x); // +1 to avoid negative that cannot be represented as positive
         }
-        let mut result = 1.0;
-        for _ in 0..n {
-            result *= x;
+
+        // Perform Binary Exponentiation
+        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if n % 2 == 1 {
+            x * Self::my_pow(x * x, (n - 1) / 2)
+        } else {
+            // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+            Self::my_pow(x * x, n / 2)
         }
-        result
     }
 }
 
@@ -31,6 +36,8 @@ mod tests {
     #[case(2.00000, 10, 1024.0)]
     #[case(2.10000, 3, 9.261)]
     #[case(2.00000, -2, 0.25)]
+    #[case(1.00000, 2147483647, 1.0)]
+    #[case(1.00000, i32::MIN, 1.0)]
     fn case(#[case] x: f64, #[case] n: i32, #[case] expected: f64) {
         let actual = Solution::my_pow(x, n);
         assert!((actual - expected).abs() < 1e-5, "Assertion failed: actual {actual:.5} but expected {expected:.5}. Diff is more than 1e-5.");
