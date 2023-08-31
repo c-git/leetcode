88203d71cdea8913a17fa85a0984cb99e223e101
One <43485962+c-git@users.noreply.github.com>
2023-08-31 15:06:57 -0400

    1326. Sub 3

--- rust/src/_1326_minimum_number_of_taps_to_open_to_water_a_garden.rs
+++ rust/src/_1326_minimum_number_of_taps_to_open_to_water_a_garden.rs
@@ -27,6 +27,9 @@ impl Solution {
         // Sort ranges so that we can solve in only a forward pass
         // All values that can cover a point are grouped together and the one that covers the most comes first
         ranges.sort_unstable_by_key(|x| (x[0], -(x[1] as i32)));
+        if cfg!(debug_assertions) {
+            println!("Converted and sorted ranges: {ranges:?}");
+        }
 
         // Find minimal required ranges
         let mut current = *ranges.first()?; // If ranges is empty then no solution possible
@@ -35,24 +38,49 @@ impl Solution {
             // No ranges cover 0
             return None;
         }
+        let mut previously_covered = current[1];
+        if previously_covered >= n {
+            return Some(result);
+        } else {
+            result += 1; // Make current be the previous range
+        }
         for range in ranges {
             if current[1] >= n {
                 // All ranges already covered
                 return Some(result);
             }
-            match (range[0] == current[0], range[1] > current[1]) {
-                (true, true) => unreachable!(
-                    "This should never happen because we sorted by reversing ending boundary"
-                ),
-                (_, false) => {
-                    // Ignore this range it doesn't cover as much as the current
-                    // This range starts at least as early but goes further
+            let is_equal_start_bound = range[0] == current[0];
+            let does_cover_further = range[1] > current[1];
+            let is_passed_covered_by_previous = previously_covered < range[0];
+            match (
+                is_equal_start_bound,
+                does_cover_further,
+                is_passed_covered_by_previous,
+            ) {
+                (true, true, _) => unreachable!("Unable to be reached because of sort order"),
+                (true, false, _) => {
+                    // Less covered just skip this one
                 }
-                (false, true) => {
-                    // We need this new range because it covers more of the array we cannot cover yet
-                    // We need the previous range because this new one doesn't cover the start of the last one
+                (false, true, true) => {
+                    // Covers more than current and starts after the previous so we need to keep the current to cover that partial range
+                    if range[0] > current[1] + 1 {
+                        // There is now a gap that cannot be covered
+                        return None;
+                    }
                     result += 1;
+                    debug_assert!(
+                        previously_covered < current[1],
+                        "Must be grater otherwise wouldn't have become current"
+                    );
+                    previously_covered = current[1];
                     current = range;
+                }
+                (false, true, false) => {
+                    // Covers more than current and the part covered by current right now is still covered by the previous
+                    current = range;
+                }
+                (false, false, _) => {
+                    // This range is useless, it is already covered
                 }
             }
         }
@@ -79,6 +107,8 @@ mod tests {
     #[case(5, vec![3,4,1,1,0,0], 1)]
     #[case(3, vec![0,0,0,0], -1)]
     #[case(8, vec![4,0,0,0,4,0,0,0,4], 1)]
+    #[case(9, vec![0,5,0,3,3,3,1,4,0,4], 2)]
+    #[case(9, vec![3,0,0,0,0,0,0,0,0,4], -1)]
     fn case(#[case] n: i32, #[case] ranges: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::min_taps(n, ranges);
         assert_eq!(actual, expected);
