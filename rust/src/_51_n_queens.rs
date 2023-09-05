b4942c2c2d9ea2a8dfc3314fded46cd28d61c8b3
One <43485962+c-git@users.noreply.github.com>
2023-09-04 22:19:08 -0400

    51. Sub 2

--- rust/src/_51_n_queens.rs
+++ rust/src/_51_n_queens.rs
@@ -2,13 +2,14 @@
 //! 51. N-Queens
 
 // Source: https://leetcode.com/problems/n-queens/solutions/3459335/rust-recursive-backtracking-with-bitsets-beats-100/
+// Modified slightly for ease of readability
 
 struct Solver {
     result: Vec<Vec<String>>,
-    table: Vec<String>,
+    board: Vec<String>,
     n: i32,
-    col_bitset: u32,
     row_bitset: u32,
+    col_bitset: u32,
     diag_sum_bitset: u32,
     diag_sub_bitset: u32,
     count: i32,
@@ -18,63 +19,77 @@ impl Solver {
     fn new(n: i32) -> Self {
         Solver {
             result: Vec::new(),
-            table: vec![".".repeat(n as usize); n as usize],
+            board: vec![".".repeat(n as usize); n as usize],
             n,
-            col_bitset: 0,
             row_bitset: 0,
+            col_bitset: 0,
             diag_sum_bitset: 0,
             diag_sub_bitset: 0,
             count: 0,
         }
     }
-    fn can_be_queen(&self, x: i32, y: i32) -> bool {
-        if self.col_bitset & (1 << x) != 0 {
+    fn can_be_queen(&self, row: i32, col: i32) -> bool {
+        if self.row_bitset & (1 << row) != 0 {
             return false;
         }
-        if self.row_bitset & (1 << y) != 0 {
+        if self.col_bitset & (1 << col) != 0 {
             return false;
         }
-        if self.diag_sum_bitset & (1 << (x + y)) != 0 {
+        if self.diag_sum_bitset & (1 << (row + col)) != 0 {
             return false;
         }
-        if self.diag_sub_bitset & (1 << (x - y + self.n)) != 0 {
+        if self.diag_sub_bitset & (1 << (row - col + self.n)) != 0 {
             return false;
         }
         true
     }
-    fn set_elem(&mut self, x: i32, y: i32, c: u8) {
+    fn set_elem(&mut self, row: i32, col: i32, c: u8) {
         if c == b'.' {
-            self.col_bitset &= !(1 << x);
-            self.row_bitset &= !(1 << y);
-            self.diag_sum_bitset &= !(1 << (x + y));
-            self.diag_sub_bitset &= !(1 << (x - y + self.n));
+            self.row_bitset &= !(1 << row);
+            self.col_bitset &= !(1 << col);
+            self.diag_sum_bitset &= !(1 << (row + col));
+            self.diag_sub_bitset &= !(1 << (row - col + self.n));
             self.count -= 1;
         } else {
-            assert_eq!(c, b'Q');
-            self.col_bitset |= (1 << x);
-            self.row_bitset |= (1 << y);
-            self.diag_sum_bitset |= (1 << (x + y));
-            self.diag_sub_bitset |= (1 << (x - y + self.n));
+            debug_assert_eq!(c, b'Q');
+            self.row_bitset |= 1 << row;
+            self.col_bitset |= 1 << col;
+            self.diag_sum_bitset |= 1 << (row + col);
+            self.diag_sub_bitset |= 1 << (row - col + self.n);
             self.count += 1;
         }
-        unsafe { self.table[x as usize].as_mut_vec()[y as usize] = c }
+        unsafe { self.board[row as usize].as_mut_vec()[col as usize] = c }
     }
-    fn solve(&mut self, x: i32) {
-        if x == self.n {
+    fn solve(&mut self, row: i32) {
+        // self.print_board();
+        if row == self.n {
             if self.n == self.count {
-                self.result.push(self.table.clone());
+                self.result.push(self.board.clone());
             }
             return;
         }
 
-        for y in 0..self.n {
-            if !self.can_be_queen(x, y) {
+        for col in 0..self.n {
+            if !self.can_be_queen(row, col) {
                 continue;
             }
-            self.set_elem(x, y, b'Q');
-            self.solve(x + 1);
-            self.set_elem(x, y, b'.');
+            self.set_elem(row, col, b'Q');
+            self.solve(row + 1);
+            self.set_elem(row, col, b'.');
         }
+    }
+
+    fn print_board(&self) {
+        for row in self.board.iter() {
+            println!("{row}");
+        }
+        println!();
+        println!("row {:b}", self.row_bitset);
+        println!("col {:b}", self.col_bitset);
+        println!("sum {:b}", self.diag_sum_bitset);
+        println!("sub {:b}", self.diag_sub_bitset);
+        println!("cnt {:b}", self.count);
+        println!("{}", "-".repeat(80));
     }
 }
 
