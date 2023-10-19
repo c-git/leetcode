d0c40ac6e6c3af48183d78b2f1df66a57624d8de
One <43485962+c-git@users.noreply.github.com>
2023-10-19 14:49:00 -0400

    844. Sub 2

--- rust/src/_844_backspace_string_compare.rs
+++ rust/src/_844_backspace_string_compare.rs
@@ -3,23 +3,56 @@
 
 impl Solution {
     pub fn backspace_compare(s: String, t: String) -> bool {
-        let mut s_clean = vec![];
-        let mut t_clean = vec![];
-        for c in s.chars() {
-            if c == '#' {
-                s_clean.pop();
-            } else {
-                s_clean.push(c);
+        // Based on second solution in editorial
+        // Go back one character at a time in each string and ensure each character is the same
+        let s = s.as_bytes();
+        let t = t.as_bytes();
+        let mut pos_s = s.len() as i32; // Starts to the right of the last letter as the first step is to move back one
+        let mut pos_t = t.len() as i32;
+        loop {
+            // Find "next char" in each final string
+            step_back_to_next_kept_char(&mut pos_s, s);
+            step_back_to_next_kept_char(&mut pos_t, t);
+            let is_s_finished = pos_s < 0;
+            let is_t_finished = pos_t < 0;
+            match (is_s_finished, is_t_finished) {
+                (true, true) => {
+                    // No more characters to compare, they must be the same since they never did not match
+                    return true;
+                }
+                (false, false) => {
+                    if s[pos_s as usize] != t[pos_t as usize] {
+                        // This character stays but it's not the same
+                        return false;
+                    }
+                }
+                (true, false) | (false, true) => {
+                    // Only one string finished
+                    return false;
+                }
             }
         }
-        for c in t.chars() {
-            if c == '#' {
-                t_clean.pop();
-            } else {
-                t_clean.push(c);
-            }
+    }
+}
+
+/// Goes back until it meets a character that is kept or moves pos before first character
+fn step_back_to_next_kept_char(pos: &mut i32, chars: &[u8]) {
+    let mut backspace = 0;
+    loop {
+        *pos -= 1;
+        if *pos < 0 {
+            break;
         }
-        s_clean == t_clean
+        if chars[*pos as usize] == b'#' {
+            // Record need to skip one more
+            backspace += 1;
+        } else if backspace > 0 {
+            // Skip this one
+            backspace -= 1;
+        } else {
+            // This character gets to stay
+            break;
+        }
     }
 }
 
