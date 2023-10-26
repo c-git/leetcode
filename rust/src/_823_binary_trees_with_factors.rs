73b9dc99ce282789499e6bc43b13e77f8fa39309
One <43485962+c-git@users.noreply.github.com>
2023-10-26 15:02:20 -0400

    823. Sub 2

--- rust/src/_823_binary_trees_with_factors.rs
+++ rust/src/_823_binary_trees_with_factors.rs
@@ -4,9 +4,8 @@
 const MOD: i64 = 1_000_000_007;
 
 impl Solution {
-    pub fn num_factored_binary_trees(arr: Vec<i32>) -> i32 {
+    pub fn num_factored_binary_trees(mut arr: Vec<i32>) -> i32 {
         // Based on https://leetcode.com/problems/binary-trees-with-factors/solutions/4208932/97-50-optimized-dp/
-        let mut arr = arr;
         arr.sort();
         let s: std::collections::HashSet<_> = arr.iter().cloned().collect();
         let mut dp = std::collections::HashMap::new();
@@ -16,7 +15,7 @@ impl Solution {
 
         for &i in &arr {
             for &j in &arr {
-                if j > (i as f64).sqrt() as i32 {
+                if (j as i64 * j as i64) > i as i64 {
                     break;
                 }
                 if i % j == 0 && s.contains(&(i / j)) {
@@ -50,6 +49,27 @@ mod tests {
     #[case(vec![2,4,6,8,10,5,3], 16)]
     #[case(vec![2,4,6,8,10,5,3,30], 29)]
     fn case(#[case] arr: Vec<i32>, #[case] expected: i32) {
+        let actual = Solution::num_factored_binary_trees(arr);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn long() {
+        let mut arr = Vec::with_capacity(1000);
+        let expected = 320_844;
+        (2..).step_by(2).take(1000).for_each(|x| arr.push(x));
+        let actual = Solution::num_factored_binary_trees(arr);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn big_numbers() {
+        let mut arr = Vec::with_capacity(1000);
+        let expected = 1000;
+        (1_000_000_000 - 2000..)
+            .step_by(2)
+            .take(1000)
+            .for_each(|x| arr.push(x));
         let actual = Solution::num_factored_binary_trees(arr);
         assert_eq!(actual, expected);
     }
