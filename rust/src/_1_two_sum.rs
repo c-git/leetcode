6a9c49d60f47c8207261060a69544910422297eb
One <43485962+c-git@users.noreply.github.com>
2023-11-17 16:00:45 -0500

    1. Two Sum

--- rust/src/_1_two_sum.rs
+++ rust/src/_1_two_sum.rs
@@ -1,48 +1,31 @@
-use std::collections::HashMap;
-struct Solution;
+//! Solution for https://leetcode.com/problems/two-sum
+//! 1. Two Sum
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut seen = HashMap::new();
-
-        for (i, num) in nums.iter().enumerate() {
-            match seen.get(num) {
-                Some(other_index) => return vec![*other_index, i as i32],
-                None => {
-                    seen.insert(target - num, i as i32);
-                }
-            }
-        }
-        unreachable!("By Assumption given in Problem definition")
+        todo!("Fill in body")
     }
 }
 
-#[test]
-fn case1() {
-    let nums = vec![2, 7, 11, 15];
-    let target = 9;
-    let expected = vec![0, 1];
+// << ---------------- Code below here is only for local use ---------------- >>
 
-    let actual = Solution::two_sum(nums, target);
-    assert_eq!(actual, expected);
-}
+pub struct Solution;
 
-#[test]
-fn case2() {
-    let nums = vec![3, 2, 4];
-    let target = 6;
-    let expected = vec![1, 2];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let actual = Solution::two_sum(nums, target);
-    assert_eq!(actual, expected);
-}
+    use rstest::rstest;
 
-#[test]
-fn case3() {
-    let nums = vec![3, 3];
-    let target = 6;
-    let expected = vec![0, 1];
-
-    let actual = Solution::two_sum(nums, target);
-    assert_eq!(actual, expected);
+    #[rstest]
+    #[case(vec![2,7,11,15], 9, vec![0,1])]
+    #[case(vec![3,2,4], 6, vec![1,2])]
+    #[case(vec![3,3], 6, vec![0,1])]
+    fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] mut expected: Vec<i32>) {
+        let mut actual = Solution::two_sum(nums, target);
+        // Sorts added because any order is correct
+        actual.sort();
+        expected.sort();
+        assert_eq!(actual, expected);
+    }
 }
