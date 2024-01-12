e76b05c2d03aa9ec696c9b4f3a9fb37fd51476e9
One <43485962+c-git@users.noreply.github.com>
2024-01-12 09:54:01 -0500

    1704. Sub 2

--- rust/src/_1704_determine_if_string_halves_are_alike.rs
+++ rust/src/_1704_determine_if_string_halves_are_alike.rs
@@ -7,12 +7,13 @@ impl Solution {
         let vowels = [b'a', b'e', b'i', b'o', b'u', b'A', b'E', b'I', b'O', b'U'];
         let chars = s.as_bytes();
         let half_len = chars.len() / 2;
-        for (i, c) in chars.iter().enumerate(){
-            if vowels.contains(c){
-            if i <= half_len{
-              vowel_count +=1;  
-            }}else{
-                vowel_count-=1;
+        for (i, c) in chars.iter().enumerate() {
+            if vowels.contains(c) {
+                if i < half_len {
+                    vowel_count += 1;
+                } else {
+                    vowel_count -= 1;
+                }
             }
         }
         vowel_count == 0
@@ -32,6 +33,7 @@ mod tests {
     #[rstest]
     #[case("book", true)]
     #[case("textbook", false)]
+    #[case("AbCdEfGh", true)]
     fn case(#[case] s: String, #[case] expected: bool) {
         let actual = Solution::halves_are_alike(s);
         assert_eq!(actual, expected);
