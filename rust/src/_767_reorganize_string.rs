6fa39edebfc42dc6b7f6fd0ad4ec24468a6ad9a7
One <43485962+c-git@users.noreply.github.com>
2024-01-13 23:19:27 -0500

    767. Sub 3
    Wanted to try a solution using a vector

--- rust/src/_767_reorganize_string.rs
+++ rust/src/_767_reorganize_string.rs
@@ -1,80 +1,58 @@
 //! Solution for https://leetcode.com/problems/reorganize-string
 //! 767. Reorganize String
-struct Letter {
-    c: char,
-    count: u16,
-}
 
-impl PartialOrd for Letter {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.count.partial_cmp(&other.count)
-    }
-}
-
-impl PartialEq for Letter {
-    fn eq(&self, other: &Self) -> bool {
-        self.c == other.c && self.count == other.count
-    }
-}
-impl Ord for Letter {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.count.cmp(&other.count)
-    }
-}
-impl Eq for Letter {}
+use std::collections::HashMap;
 
 impl Solution {
     pub fn reorganize_string(s: String) -> String {
-        let mut char_counts = vec![0; 26];
-        for c in s.as_bytes() {
-            char_counts[(*c - b'a') as usize] += 1;
-        }
-        let mut remaining_chars = vec![];
-        for (c, count) in char_counts.into_iter().enumerate() {
-            if count > 0 {
-                remaining_chars.push(Letter {
-                    c: char::from_u32((b'a' + c as u8) as u32).unwrap(),
-                    count,
-                })
-            }
-        }
-        remaining_chars.sort_unstable();
         let mut result = String::with_capacity(s.len());
-        let mut last_seen = remaining_chars.last().unwrap().c;
-        if remaining_chars.last().unwrap().count == 1 {
-            remaining_chars.pop();
-        } else {
-            remaining_chars.last_mut().unwrap().count -= 1;
-        }
-        remaining_chars.sort_unstable();
-        result.push(last_seen);
-        while !remaining_chars.is_empty() {
-            if remaining_chars.last().unwrap().c != last_seen {
-                let letter = remaining_chars.last_mut().unwrap();
-                last_seen = letter.c;
-                result.push(last_seen);
-                if letter.count == 1 {
-                    remaining_chars.pop();
-                } else {
-                    letter.count -= 1;
-                }
-            } else {
-                if remaining_chars.len() < 2 {
-                    return "".to_string();
-                }
-                let n = remaining_chars.len();
-                let letter = remaining_chars.get_mut(n - 2).unwrap();
-                last_seen = letter.c;
-                result.push(last_seen);
-                if letter.count == 1 {
-                    remaining_chars.remove(n - 2);
-                } else {
-                    letter.count -= 1;
-                }
+        let mut freq: HashMap<char, usize> = HashMap::new();
+        s.chars().for_each(|c| *freq.entry(c).or_default() += 1);
+        let mut remaining: Vec<(usize, char)> = freq.into_iter().map(|(k, v)| (v, k)).collect();
+        remaining.sort_unstable();
+        let mut most_freq = remaining.pop();
+        while let Some((mut next_count, next_c)) = remaining.pop() {
+            let (mut most_freq_count, most_freq_c) = most_freq.expect(
+                "This should always be some at the top of the loop otherwise we've made a mistake",
+            );
+            debug_assert!(
+                most_freq_count > 0 && next_count > 0,
+                "Values assumed to be greater than 0 to start"
+            );
+            while most_freq_count > 0 && next_count > 0 {
+                result.push(most_freq_c);
+                result.push(next_c);
+                most_freq_count -= 1;
+                next_count -= 1;
             }
-            remaining_chars.sort_unstable();
+            match (most_freq_count, next_count) {
+                (0, 0) => most_freq = remaining.pop(),
+                (0, x) => {
+                    debug_assert!(
+                        x > 0,
+                        "Because it's a usize and not equal to 0 so must be greater than"
+                    );
+                    most_freq = Some((x, next_c));
+                }
+                (x, 0) => {
+                    debug_assert!(
+                        x > 0,
+                        "Because it's a usize and not equal to 0 so must be greater than"
+                    );
+                    most_freq = Some((x, most_freq_c));
+                }
+                _ => unreachable!("One of them must be 0 to exit the loop"),
+            }
         }
-        result
+
+        match most_freq {
+            Some((1, c)) => {
+                result.push(c);
+                result
+            }
+            None => result,
+            Some(_) => "".to_string(), // More than one left cannot work
+        }
     }
 }
 
