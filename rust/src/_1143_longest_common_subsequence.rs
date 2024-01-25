a8aa7dd7267386c81fbd9caaa972893420521e35
One <43485962+c-git@users.noreply.github.com>
2024-01-25 12:04:12 -0500

    1143. Sub 4
    Rename variables for readability

--- rust/src/_1143_longest_common_subsequence.rs
+++ rust/src/_1143_longest_common_subsequence.rs
@@ -5,23 +5,34 @@ impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
         // Taken from https://leetcode.com/problems/longest-common-subsequence/solutions/2394254/rust-dp-with-comments/
 
+        // Sort string by longer and shorter to reduce the memory usage
+        let (shorter, longer) = if text1.len() < text2.len() {
+            (text1, text2)
+        } else {
+            (text2, text1)
+        };
+
         // Input is ASCII => chars are bytes
-        let (text1, text2) = (text1.as_bytes(), text2.as_bytes());
-        let (n1, n2) = (text1.len(), text2.len());
+        let (longer, shorter) = (longer.as_bytes(), shorter.as_bytes());
 
         // Initialize previous DP row. All zeros represent taking no characters from text1
-        let mut dp_prev = vec![0; n2 + 1];
+        let mut dp_prev = vec![0; shorter.len() + 1];
         let mut dp_curr = dp_prev.clone();
 
         // Iterate in reverse over the text strings, keeping track of the LCS considering the
         // corresponding suffixes
-        for i in (0..n1).rev() {
-            for j in (0..n2).rev() {
-                // Take the best path - either skipping the current character in text2, or
-                // skipping the current character in text1, or using the characters if they match.
-                dp_curr[j] = dp_prev[j]
-                    .max(dp_curr[j + 1])
-                    .max(dp_prev[j + 1] + if text1[i] == text2[j] { 1 } else { 0 });
+        for idx_longer in (0..longer.len()).rev() {
+            for idx_shorter in (0..shorter.len()).rev() {
+                // Take the best path - either skipping the current character in shorter, or
+                // skipping the current character in longer, or using the characters if they match.
+                dp_curr[idx_shorter] = dp_prev[idx_shorter].max(dp_curr[idx_shorter + 1]).max(
+                    dp_prev[idx_shorter + 1]
+                        + if longer[idx_longer] == shorter[idx_shorter] {
+                            1
+                        } else {
+                            0
+                        },
+                );
             }
             // Swap the rows to reuse dp_prev, which is now stale
             std::mem::swap(&mut dp_prev, &mut dp_curr);
