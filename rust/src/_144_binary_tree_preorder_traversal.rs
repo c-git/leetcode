eb9a3e000112438fbe2e442a870a24aa3a1d04e2
One <43485962+c-git@users.noreply.github.com>
2024-01-26 21:03:12 -0500

    144. Sub 2

--- rust/src/_144_binary_tree_preorder_traversal.rs
+++ rust/src/_144_binary_tree_preorder_traversal.rs
@@ -24,17 +24,21 @@ use std::rc::Rc;
 impl Solution {
     pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         let mut result = vec![];
-        Self::preorder_traversal_(&root, &mut result);
-        result
-    }
-
-    fn preorder_traversal_(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
-        if let Some(node) = root {
+        let Some(node) = root else {
+            return result;
+        };
+        let mut stack = vec![node];
+        while let Some(node) = stack.pop() {
             let node = node.borrow();
             result.push(node.val);
-            Self::preorder_traversal_(&node.left, result);
-            Self::preorder_traversal_(&node.right, result);
+            if let Some(left) = node.left.clone() {
+                stack.push(left);
+            }
+            if let Some(right) = node.right.clone() {
+                stack.push(right);
+            }
         }
+        result
     }
 }
 
