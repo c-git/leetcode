8df40fe699328193f2d96158ea6b00b4bd26d637
One <43485962+c-git@users.noreply.github.com>
2024-01-31 15:01:16 -0500

    74. Sub 2

--- rust/src/_74_search_a_2_d_matrix.rs
+++ rust/src/_74_search_a_2_d_matrix.rs
@@ -7,7 +7,7 @@ impl Solution {
         let mut high_index = matrix.len() * matrix[0].len() - 1;
         while low_index < high_index {
             // LI: If the value exists it is in the range low_index..=high_index
-            let mid = (low_index + high_index) / 2 + 1;
+            let mid = (low_index + high_index) / 2;
             match matrix.at_adjusted_index(mid).cmp(&target) {
                 std::cmp::Ordering::Less => low_index = mid + 1,
                 std::cmp::Ordering::Equal => return true,
@@ -43,6 +43,7 @@ mod tests {
     #[rstest]
     #[case(vec![vec![1,3,5,7],vec![10,11,16,20],vec![23,30,34,60]], 3, true)]
     #[case(vec![vec![1,3,5,7],vec![10,11,16,20],vec![23,30,34,60]], 13, false)]
+    #[case(vec![vec![1,1]], 2, false)]
     fn case(#[case] matrix: Vec<Vec<i32>>, #[case] target: i32, #[case] expected: bool) {
         let actual = Solution::search_matrix(matrix, target);
         assert_eq!(actual, expected);
