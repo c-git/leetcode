2dc140556bb492577fd27d5c972d09ec9e026a9f
One <43485962+c-git@users.noreply.github.com>
2024-05-18 01:56:49 -0400

    56. Sub 2

--- rust/src/_56_merge_intervals.rs
+++ rust/src/_56_merge_intervals.rs
@@ -3,22 +3,26 @@
 
 impl Solution {
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let mut result = vec![];
+        // Sort intervals so that any interval that starts before the end of the current one being merged ends must show up after
         intervals.sort_unstable();
-        let mut curr_start = intervals[0][0];
-        let mut curr_end = intervals[0][1];
-        for interval in intervals.into_iter().skip(1) {
-            let start = interval[0];
-            let end = interval[1];
-            if start <= curr_end {
-                curr_end = curr_end.max(end);
+
+        // Store current interval being merged at tail of result
+        let mut result = vec![intervals
+            .first()
+            .expect("constraint guarantees at least one interval")
+            .clone()];
+
+        // Step though intervals merging or creating a new interval for each seen
+        for interval in intervals.into_iter() {
+            let last = result.last_mut().expect("starts off non-empty");
+            if interval[0] <= last[1] {
+                // Overlapping merge
+                last[1] = last[1].max(interval[1]);
             } else {
-                result.push(vec![curr_start, curr_end]);
-                curr_start = start;
-                curr_end = end;
+                // Non-overlapping start a new interval
+                result.push(interval);
             }
         }
-        result.push(vec![curr_start, curr_end]);
         result
     }
 }
@@ -36,6 +40,7 @@ mod tests {
     #[rstest]
     #[case(vec![vec![1,3],vec![2,6],vec![8,10],vec![15,18]], vec![vec![1,6],vec![8,10],vec![15,18]])]
     #[case(vec![vec![1,4],vec![4,5]], vec![vec![1,5]])]
+    #[case(vec![vec![1,4],vec![4,5],vec![1,3]], vec![vec![1,5]])]
     fn case(#[case] intervals: Vec<Vec<i32>>, #[case] expected: Vec<Vec<i32>>) {
         let actual = Solution::merge(intervals);
         assert_eq!(actual, expected);
