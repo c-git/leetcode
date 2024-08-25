439754fe6d26de21ad6bb77a00347ba783962b69
One <43485962+c-git@users.noreply.github.com>
2024-08-25 11:58:53 -0400

    Update comment in lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,2 @@
 // Add new file as a module to make it run
-
-// TODO Add instructions on how to setup code to run and update readme
-// Interim note: Currently using cargo leet https://github.com/rust-practice/cargo-leet to generate the
-// stubs for new problems. Will update readme and notes here after cargo leet stabilizes a bit. Feel free
-// to reach out if you want to make use of my setup before I document it. (Opening an issue on github is a
-// fine way to get in touch)
+// See cargo-leet for more info https://github.com/rust-practice/cargo-leet
