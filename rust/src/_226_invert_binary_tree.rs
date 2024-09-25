546fc1117a7e9f924c42cdb5a13048acbc37b847
One <43485962+c-git@users.noreply.github.com>
2024-09-24 21:07:12 -0400

    226. Sub 2

--- rust/src/_226_invert_binary_tree.rs
+++ rust/src/_226_invert_binary_tree.rs
@@ -1,3 +1,6 @@
+//! Solution for https://leetcode.com/problems/invert-binary-tree
+//! 226. Invert Binary Tree
+
 // Definition for a binary tree node.
 // #[derive(Debug, PartialEq, Eq)]
 // pub struct TreeNode {
@@ -18,59 +21,39 @@
 // }
 use std::cell::RefCell;
 use std::rc::Rc;
-
-use cargo_leet::TreeNode;
-
-pub struct Solution;
-
 impl Solution {
-    pub fn invert_tree(mut root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        if let Some(node) = root {
-            {
-                let mut node = node.borrow_mut();
-                let temp = node.left.clone();
-                node.left = Self::invert_tree(node.right.clone());
-                node.right = Self::invert_tree(temp);
-            }
-            root = Some(node);
-        }
-        root
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let root = root?;
+        let (left, right) = (root.borrow().left.clone(), root.borrow().right.clone());
+        root.borrow_mut().left = right;
+        root.borrow_mut().right = left;
+        Self::invert_tree(root.borrow().left.clone());
+        Self::invert_tree(root.borrow().right.clone());
+        Some(root)
     }
 }
 
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+use cargo_leet::TreeNode;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use cargo_leet::TreeRoot;
 
-    use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn case1() {
-        let root: TreeRoot = vec![4, 2, 7, 1, 3, 6, 9].into();
-        let expected: TreeRoot = vec![4, 7, 2, 9, 6, 3, 1].into();
-
-        let actual = Solution::invert_tree(root.into());
-        let debug_friendly_format: TreeRoot = actual.into();
-        assert_eq!(debug_friendly_format, expected);
-    }
-
-    #[test]
-    fn case2() {
-        let root: TreeRoot = vec![2, 1, 3].into();
-        let expected: TreeRoot = vec![2, 3, 1].into();
-
-        let actual = Solution::invert_tree(root.into());
-        let debug_friendly_format: TreeRoot = actual.into();
-        assert_eq!(debug_friendly_format, expected);
-    }
-
-    #[test]
-    fn case3() {
-        let root: TreeRoot = Vec::<i32>::new().into();
-        let expected: TreeRoot = Vec::<i32>::new().into();
-
-        let actual = Solution::invert_tree(root.into());
-        let debug_friendly_format: TreeRoot = actual.into();
-        assert_eq!(debug_friendly_format, expected);
+    #[rstest]
+    #[case(TreeRoot::from("[4,2,7,1,3,6,9]").into(), TreeRoot::from("[4,7,2,9,6,3,1]").into())]
+    #[case(TreeRoot::from("[2,1,3]").into(), TreeRoot::from("[2,3,1]").into())]
+    #[case(TreeRoot::from("[]").into(), TreeRoot::from("[]").into())]
+    fn case(
+        #[case] root: Option<Rc<RefCell<TreeNode>>>,
+        #[case] expected: Option<Rc<RefCell<TreeNode>>>,
+    ) {
+        let actual = Solution::invert_tree(root);
+        assert_eq!(actual, expected);
     }
 }
