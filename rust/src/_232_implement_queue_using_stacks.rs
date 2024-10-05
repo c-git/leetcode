c1e73ca10d22a36333a8422a190c78de49d8e88e
One <43485962+c-git@users.noreply.github.com>
2024-10-04 20:03:47 -0400

    232.  Sub 2

--- rust/src/_232_implement_queue_using_stacks.rs
+++ rust/src/_232_implement_queue_using_stacks.rs
@@ -1,8 +1,6 @@
 //! Solution for https://leetcode.com/problems/implement-queue-using-stacks
 //! 232. Implement Queue using Stacks
 
-#![allow(dead_code)]
-
 struct MyQueue {
     stack1: Vec<i32>,
     stack2: Vec<i32>,
@@ -15,8 +13,8 @@ struct MyQueue {
 impl MyQueue {
     fn new() -> Self {
         Self {
-            stack1: vec![],
-            stack2: vec![],
+            stack1: Default::default(),
+            stack2: Default::default(),
         }
     }
 
@@ -25,40 +23,34 @@ impl MyQueue {
     }
 
     fn pop(&mut self) -> i32 {
-        if let Some(val) = self.stack2.pop() {
-            val
-        } else {
-            // Fill stack 2 from stack 1 then return the top value (if both empty it panics)
-            self.fill_stack2_from_stack1();
-            self.stack2.pop().expect("Panics if both are empty")
-        }
-    }
-
-    fn fill_stack2_from_stack1(&mut self) {
-        debug_assert!(
-            self.stack2.is_empty(),
-            "This should only be called if stack 2 is empty"
-        );
-        while let Some(val) = self.stack1.pop() {
-            self.stack2.push(val);
-        }
-    }
-
-    fn peek(&mut self) -> i32 {
         if self.stack2.is_empty() {
-            self.fill_stack2_from_stack1();
+            // Only transfers items if stack2 is empty to preserve order
+            while let Some(item) = self.stack1.pop() {
+                self.stack2.push(item);
+            }
         }
+        self.stack2
+            .pop()
+            .expect("should have an item by problem constraints")
+    }
 
-        *self
-            .stack2
-            .last()
-            .expect("Panics if both stacks were empty")
+    fn peek(&self) -> i32 {
+        if let Some(last) = self.stack2.last() {
+            last
+        } else {
+            self.stack1
+                .first()
+                .expect("should have an item by problem constraints")
+        }
+        .to_owned()
     }
 
     fn empty(&self) -> bool {
         self.stack1.is_empty() && self.stack2.is_empty()
     }
 }
+
+// << ---------------- Code below here is only for local use ---------------- >>
 
 /**
  * Your MyQueue object will be instantiated and called as such:
@@ -68,8 +60,6 @@ impl MyQueue {
  * let ret_3: i32 = obj.peek();
  * let ret_4: bool = obj.empty();
  */
-
-// << ---------------- Code below here is only for local use ---------------- >>
 
 #[cfg(test)]
 mod tests {
