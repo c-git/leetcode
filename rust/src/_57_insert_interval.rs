a0280bf1ba893d86888e944e4504139403678d7c
One <43485962+c-git@users.noreply.github.com>
2024-10-10 15:12:40 -0400

    57. Sub 5

--- rust/src/_57_insert_interval.rs
+++ rust/src/_57_insert_interval.rs
@@ -1,58 +1,35 @@
 //! Solution for https://leetcode.com/problems/insert-interval
 //! 57. Insert Interval
 
-enum MergeStatus {
-    /// Before starting to merge
-    Before,
-    /// During the process of merging
-    During([i32; 2]),
-    /// New point either merged already inserted
-    Completed,
-}
-
 impl Solution {
-    pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+    pub fn insert(mut intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
         let mut result = Vec::with_capacity(intervals.len() + 1);
-        let mut merge_status = MergeStatus::Before;
-        for current_interval in intervals {
-            match merge_status {
-                MergeStatus::Before => {
-                    if new_interval[1] < current_interval[0] {
-                        // The new_interval just fits here before current_interval starts
-                        result.push(new_interval.clone());
-                        result.push(current_interval);
-                        merge_status = MergeStatus::Completed;
-                    } else if new_interval[0] <= current_interval[1] {
-                        // The new_interval and the current_interval overlap, start the merge
-                        merge_status = MergeStatus::During([
-                            new_interval[0].min(current_interval[0]),
-                            current_interval[1].max(new_interval[1]),
-                        ]);
-                    } else {
-                        result.push(current_interval);
-                    }
-                }
-                MergeStatus::During(merging) => {
-                    if current_interval[0] <= merging[1] {
-                        // Another overlap detected, continue merging
-                        merge_status =
-                            MergeStatus::During([merging[0], merging[1].max(current_interval[1])]);
-                    } else {
-                        // No more overlap insert the merged one and then the current_interval
-                        result.push(merging.to_vec());
-                        result.push(current_interval);
-                        merge_status = MergeStatus::Completed;
-                    }
-                }
-                MergeStatus::Completed => result.push(current_interval),
+        intervals.push(new_interval);
+        intervals.sort_by(|x, y| {
+            if x[0] == y[0] {
+                x[1].cmp(&y[1])
+            } else {
+                x[0].cmp(&y[0])
+            }
+        });
+        let mut intervals = intervals.into_iter();
+        let mut curr_interval = intervals
+            .next()
+            .expect("we just inserted at least 1 into it");
+        for interval in intervals {
+            if interval[0] <= curr_interval[1] {
+                // Merge
+                debug_assert!(
+                    interval[0] >= curr_interval[0],
+                    "supposed to be sorted by start time"
+                );
+                curr_interval[1] = curr_interval[1].max(interval[1]);
+            } else {
+                result.push(curr_interval);
+                curr_interval = interval
             }
         }
-        match merge_status {
-            MergeStatus::Before => result.push(new_interval),
-            MergeStatus::During(merged_interval) => result.push(merged_interval.to_vec()),
-            MergeStatus::Completed => {}
-        }
-
+        result.push(curr_interval);
         result
     }
 }
