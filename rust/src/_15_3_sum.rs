18919bdcef8fd2daadfc29118feff85f807c17f1
One <43485962+c-git@users.noreply.github.com>
2024-10-12 19:52:40 -0400

    15. Sub 3

--- rust/src/_15_3_sum.rs
+++ rust/src/_15_3_sum.rs
@@ -1,24 +1,21 @@
 //! Solution for https://leetcode.com/problems/3sum
 //! 15. 3Sum
 
+use std::collections::BTreeSet;
+
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort_unstable();
-        let mut result: Vec<Vec<i32>> = Vec::new();
+        let mut result: BTreeSet<Vec<i32>> = BTreeSet::new();
         for first in 0..nums.len() - 2 {
             for second in first + 1..nums.len() - 1 {
                 let target = 0 - nums[first] - nums[second];
                 if nums[second + 1..].binary_search(&target).is_ok() {
-                    if let Some(last) = result.last() {
-                        if nums[first] == last[0] && nums[second] == last[1] && target == last[2] {
-                            continue;
-                        }
-                    }
-                    result.push(vec![nums[first], nums[second], target]);
+                    result.insert(vec![nums[first], nums[second], target]);
                 }
             }
         }
-        result
+        result.into_iter().collect()
     }
 }
 
@@ -38,6 +35,7 @@ mod tests {
     #[case(vec![0,0,0], vec![vec![0,0,0]])]
     #[case(vec![0,0,0,0], vec![vec![0,0,0]])]
     #[case(vec![-1,0,1,2,-1,-4,-2,-3,3,0,4], vec![vec![-4,0,4],vec![-4,1,3],vec![-3,-1,4],vec![-3,0,3],vec![-3,1,2],vec![-2,-1,3],vec![-2,0,2],vec![-1,-1,2],vec![-1,0,1]])]
+    #[case(vec![-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6], vec![vec![-4,-2,6],vec![-4,0,4],vec![-4,1,3],vec![-4,2,2],vec![-2,-2,4],vec![-2,0,2]])]
     fn case(#[case] nums: Vec<i32>, #[case] mut expected: Vec<Vec<i32>>) {
         let mut actual = Solution::three_sum(nums);
         actual.sort();
