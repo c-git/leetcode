10b6068bcbdce46a1149925d27ea5d24f4337064
One <43485962+c-git@users.noreply.github.com>
2024-10-12 19:47:38 -0400

    15. Sub 2

--- rust/src/_15_3_sum.rs
+++ rust/src/_15_3_sum.rs
@@ -11,7 +11,7 @@ impl Solution {
                 if nums[second + 1..].binary_search(&target).is_ok() {
                     if let Some(last) = result.last() {
                         if nums[first] == last[0] && nums[second] == last[1] && target == last[2] {
-                            break;
+                            continue;
                         }
                     }
                     result.push(vec![nums[first], nums[second], target]);
@@ -37,6 +37,7 @@ mod tests {
     #[case(vec![0,1,1], vec![])]
     #[case(vec![0,0,0], vec![vec![0,0,0]])]
     #[case(vec![0,0,0,0], vec![vec![0,0,0]])]
+    #[case(vec![-1,0,1,2,-1,-4,-2,-3,3,0,4], vec![vec![-4,0,4],vec![-4,1,3],vec![-3,-1,4],vec![-3,0,3],vec![-3,1,2],vec![-2,-1,3],vec![-2,0,2],vec![-1,-1,2],vec![-1,0,1]])]
     fn case(#[case] nums: Vec<i32>, #[case] mut expected: Vec<Vec<i32>>) {
         let mut actual = Solution::three_sum(nums);
         actual.sort();
