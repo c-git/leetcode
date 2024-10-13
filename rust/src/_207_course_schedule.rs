88ad2a4434bf3ae53009ec1822969fa70387b44a
One <43485962+c-git@users.noreply.github.com>
2024-10-12 22:12:56 -0400

    207. Sub 6

--- rust/src/_207_course_schedule.rs
+++ rust/src/_207_course_schedule.rs
@@ -1,75 +1,79 @@
-#[derive(Clone, PartialEq, Eq)]
-enum Status {
-    NotVisited,
-    VisitInProgress,
-    VisitCompleted,
+//! Solution for https://leetcode.com/problems/course-schedule
+//! 207. Course Schedule
+
+struct UnionFind {
+    components: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            components: (0..n).collect(),
+            rank: vec![1; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if x != self.components[x] {
+            self.components[x] = self.find(self.components[x]);
+        }
+        self.components[x]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let mut root_x = self.find(x);
+        let mut root_y = self.find(y);
+        if root_x != root_y {
+            if self.rank[root_x] > self.rank[root_y] {
+                std::mem::swap(&mut root_x, &mut root_y)
+            }
+            self.rank[root_y] += self.rank[root_x];
+            self.components[root_x] = root_y;
+        }
+    }
+
+    fn is_same_set(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
 }
 
 impl Solution {
     pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
-        // Source: "Faster Solutions" Modified for readability
-        let mut graph = vec![Vec::new(); num_courses as usize];
-        let mut visited = vec![Status::NotVisited; num_courses as usize];
+        // Use a union find to store courses that share a dependency. If we come to add
+        // a course and it is going to be added to the same set then we found a cycle
+        // and it's not possible to finish
 
-        for item in prerequisites {
-            graph[item[0] as usize].push(item[1]);
-        }
-
-        for i in 0..num_courses {
-            if Solution::contains_cycle(&graph, &mut visited, i as usize) {
+        let mut uf = UnionFind::new(num_courses as _);
+        for pre_req in prerequisites {
+            let (x, y) = (pre_req[0] as _, pre_req[1] as _);
+            if uf.is_same_set(x, y) {
                 return false;
             }
+            uf.union(x, y);
         }
-
         true
     }
-
-    fn contains_cycle(graph: &Vec<Vec<i32>>, visited: &mut Vec<Status>, course: usize) -> bool {
-        if visited[course] == Status::VisitInProgress {
-            return true;
-        } else if visited[course] == Status::VisitCompleted {
-            return false;
-        }
-
-        visited[course] = Status::VisitInProgress;
-        for &prerequisite in &graph[course] {
-            if Solution::contains_cycle(graph, visited, prerequisite as usize) {
-                return true;
-            }
-        }
-
-        visited[course] = Status::VisitCompleted;
-        false
-    }
 }
-struct Solution;
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn case1() {
-        let num_courses = 2;
-        let prerequisites = vec![vec![1, 0]];
-        let expected = true;
-        let actual = Solution::can_finish(num_courses, prerequisites);
-        assert_eq!(actual, expected);
-    }
+    use rstest::rstest;
 
-    #[test]
-    fn case2() {
-        let num_courses = 2;
-        let prerequisites = vec![vec![1, 0], vec![0, 1]];
-        let expected = false;
-        let actual = Solution::can_finish(num_courses, prerequisites);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case3() {
-        let num_courses = 4;
-        let prerequisites = vec![vec![0, 1], vec![2, 1], vec![3, 2]];
-        let expected = true;
+    #[rstest]
+    #[case(2, vec![vec![1,0]], true)]
+    #[case(2, vec![vec![1,0],vec![0,1]], false)]
+    fn case(
+        #[case] num_courses: i32,
+        #[case] prerequisites: Vec<Vec<i32>>,
+        #[case] expected: bool,
+    ) {
         let actual = Solution::can_finish(num_courses, prerequisites);
         assert_eq!(actual, expected);
     }
