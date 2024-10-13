a8254828d68722630e0bed3e47eeb1394630ba84
One <43485962+c-git@users.noreply.github.com>
2024-10-12 23:18:24 -0400

    Revert "207. Sub 6" (Mostly)
    Kept the updated testing
    
    This reverts commit 88ad2a4434bf3ae53009ec1822969fa70387b44a.

--- rust/src/_207_course_schedule.rs
+++ rust/src/_207_course_schedule.rs
@@ -1,58 +1,48 @@
 //! Solution for https://leetcode.com/problems/course-schedule
 //! 207. Course Schedule
 
-struct UnionFind {
-    components: Vec<usize>,
-    rank: Vec<usize>,
-}
-
-impl UnionFind {
-    fn new(n: usize) -> Self {
-        Self {
-            components: (0..n).collect(),
-            rank: vec![1; n],
-        }
-    }
-
-    fn find(&mut self, x: usize) -> usize {
-        if x != self.components[x] {
-            self.components[x] = self.find(self.components[x]);
-        }
-        self.components[x]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let mut root_x = self.find(x);
-        let mut root_y = self.find(y);
-        if root_x != root_y {
-            if self.rank[root_x] > self.rank[root_y] {
-                std::mem::swap(&mut root_x, &mut root_y)
-            }
-            self.rank[root_y] += self.rank[root_x];
-            self.components[root_x] = root_y;
-        }
-    }
-
-    fn is_same_set(&mut self, a: usize, b: usize) -> bool {
-        self.find(a) == self.find(b)
-    }
+#[derive(Clone, PartialEq, Eq)]
+enum Status {
+    NotVisited,
+    VisitInProgress,
+    VisitCompleted,
 }
 
 impl Solution {
     pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
-        // Use a union find to store courses that share a dependency. If we come to add
-        // a course and it is going to be added to the same set then we found a cycle
-        // and it's not possible to finish
+        // Source: "Faster Solutions" Modified for readability
+        let mut graph = vec![Vec::new(); num_courses as usize];
+        let mut visited = vec![Status::NotVisited; num_courses as usize];
 
-        let mut uf = UnionFind::new(num_courses as _);
-        for pre_req in prerequisites {
-            let (x, y) = (pre_req[0] as _, pre_req[1] as _);
-            if uf.is_same_set(x, y) {
+        for item in prerequisites {
+            graph[item[0] as usize].push(item[1]);
+        }
+
+        for i in 0..num_courses {
+            if Solution::contains_cycle(&graph, &mut visited, i as usize) {
                 return false;
             }
-            uf.union(x, y);
         }
+
         true
+    }
+
+    fn contains_cycle(graph: &Vec<Vec<i32>>, visited: &mut Vec<Status>, course: usize) -> bool {
+        if visited[course] == Status::VisitInProgress {
+            return true;
+        } else if visited[course] == Status::VisitCompleted {
+            return false;
+        }
+
+        visited[course] = Status::VisitInProgress;
+        for &prerequisite in &graph[course] {
+            if Solution::contains_cycle(graph, visited, prerequisite as usize) {
+                return true;
+            }
+        }
+
+        visited[course] = Status::VisitCompleted;
+        false
     }
 }
 
@@ -69,6 +59,8 @@ mod tests {
     #[rstest]
     #[case(2, vec![vec![1,0]], true)]
     #[case(2, vec![vec![1,0],vec![0,1]], false)]
+    #[case(4, vec![vec![0, 1], vec![2, 1], vec![3, 2]], true)]
+    #[case(5, vec![vec![1,4],vec![2,4],vec![3,1],vec![3,2]], true)]
     fn case(
         #[case] num_courses: i32,
         #[case] prerequisites: Vec<Vec<i32>>,
