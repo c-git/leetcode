66234ba0af907cb487f4038020f83001a9ef6e0b
One <43485962+c-git@users.noreply.github.com>
2025-01-06 22:03:09 -0500

    20. Sub 3

--- rust/src/_20_valid_parentheses.rs
+++ rust/src/_20_valid_parentheses.rs
@@ -5,28 +5,33 @@ impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut stack = Vec::new();
         for c in s.chars() {
-            if ['[', '(', '{'].contains(&c) {
-                stack.push(c);
-                continue;
-            }
-
-            let expected = match c {
-                ']' => '[',
-                ')' => '(',
-                '}' => '{',
-                _ => unreachable!("by constraint in question regarding valid input"),
-            };
-
-            if let Some(top) = stack.pop() {
-                if expected != top {
-                    return false;
+            match c {
+                '(' | '[' | '{' => stack.push(c),
+                ')' => {
+                    if !expect_top_char('(', &mut stack) {
+                        return false;
+                    }
                 }
-            } else {
-                return false;
+                ']' => {
+                    if !expect_top_char('[', &mut stack) {
+                        return false;
+                    }
+                }
+                '}' => {
+                    if !expect_top_char('{', &mut stack) {
+                        return false;
+                    }
+                }
+                _ => unreachable!("problem guarantee: s consists of parentheses only '()[]{{}}'"),
             }
         }
+
         stack.is_empty()
     }
+}
+
+fn expect_top_char(expected_char: char, stack: &mut Vec<char>) -> bool {
+    Some(expected_char) == stack.pop()
 }
 
 // << ---------------- Code below here is only for local use ---------------- >>
