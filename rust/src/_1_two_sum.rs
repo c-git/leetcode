9e49813cef0a30dee04c49e418bb80be6189b207
One <43485962+c-git@users.noreply.github.com>
2025-01-06 21:46:22 -0500

    1. Sub 2

--- rust/src/_1_two_sum.rs
+++ rust/src/_1_two_sum.rs
@@ -1,21 +1,20 @@
 //! Solution for https://leetcode.com/problems/two-sum
 //! 1. Two Sum
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut wanted = HashMap::new();
-        for (i, num) in nums.into_iter().enumerate() {
-            let i = i as i32;
+        let mut seen = BTreeMap::new();
+        for (i, &num) in nums.iter().enumerate() {
             let diff = target - num;
-            if let Some(other) = wanted.get(&diff) {
-                // We've seen that number we need before
-                return vec![*other, i];
+            if let Some(&other_index) = seen.get(&diff) {
+                return vec![i as _, other_index as _];
+            } else {
+                seen.insert(num, i);
             }
-            wanted.insert(num, i); // Store this as a possibility
         }
-        unreachable!("Problem guarantees a solution exists")
+        unreachable!("question says, 'each input would have exactly one solution'")
     }
 }
 
@@ -35,7 +34,6 @@ mod tests {
     #[case(vec![3,3], 6, vec![0,1])]
     fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] mut expected: Vec<i32>) {
         let mut actual = Solution::two_sum(nums, target);
-        // Sorts added because any order is correct
         actual.sort();
         expected.sort();
         assert_eq!(actual, expected);
