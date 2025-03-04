241a3ad9e9367f3fd9fed6c7e67a084a4fa47054
One <43485962+c-git@users.noreply.github.com>
2025-03-04 15:40:51 -0500

    873. Sub 2

--- rust/src/_873_length_of_longest_fibonacci_subsequence.rs
+++ rust/src/_873_length_of_longest_fibonacci_subsequence.rs
@@ -15,36 +15,38 @@ impl Solution {
         let mut prev_sequences = BTreeMap::new();
         prev_sequences.insert(
             arr[0] + arr[1],
-            PrevSeq {
+            vec![PrevSeq {
                 last_value: arr[1],
                 length: 2,
-            },
+            }],
         );
         for (i, &x) in arr.iter().enumerate().skip(2) {
-            if let Some(prev) = prev_sequences.get(&x) {
-                let candidate = PrevSeq {
-                    last_value: x,
-                    length: prev.length + 1,
-                };
-                result = result.max(candidate.length);
-                let next = prev.last_value + x;
-                prev_sequences
-                    .entry(next)
-                    .and_modify(|curr| {
-                        if curr.length < candidate.length {
-                            *curr = candidate;
-                        }
-                    })
-                    .or_insert(candidate);
+            if let Some(prev_list) = prev_sequences.get(&x).cloned() {
+                for prev in prev_list {
+                    let next = prev.last_value + x;
+                    let candidate = PrevSeq {
+                        last_value: x,
+                        length: prev.length + 1,
+                    };
+                    result = result.max(candidate.length);
+                    prev_sequences
+                        .entry(next)
+                        .and_modify(|curr_list| {
+                            curr_list.push(candidate);
+                        })
+                        .or_insert(vec![candidate]);
+                }
             }
             for &past_value in arr.iter().take(i) {
-                // Only insert if not already exists as this is minimum length and any existing
-                // length must be min or more
                 let next = past_value + x;
-                prev_sequences.entry(next).or_insert(PrevSeq {
+                let candidate = PrevSeq {
                     last_value: x,
                     length: 2,
-                });
+                };
+                prev_sequences
+                    .entry(next)
+                    .and_modify(|curr_list| curr_list.push(candidate))
+                    .or_insert(vec![candidate]);
             }
         }
         result
@@ -64,6 +66,7 @@ mod tests {
     #[rstest]
     #[case(vec![1,2,3,4,5,6,7,8], 5)]
     #[case(vec![1,3,7,11,12,14,18], 3)]
+    #[case(vec![2,4,7,8,9,10,14,15,18,23,32,50], 5)]
     fn case(#[case] arr: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::len_longest_fib_subseq(arr);
         assert_eq!(actual, expected);
