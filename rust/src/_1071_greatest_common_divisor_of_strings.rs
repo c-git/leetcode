4a77785cfbde85a13cb89dec7660bc41692f48d7
One <43485962+c-git@users.noreply.github.com>
2025-03-18 21:27:07 -0400

    1071.Sub 2

--- rust/src/_1071_greatest_common_divisor_of_strings.rs
+++ rust/src/_1071_greatest_common_divisor_of_strings.rs
@@ -1,53 +1,50 @@
 //! Solution for https://leetcode.com/problems/greatest-common-divisor-of-strings
 //! 1071. Greatest Common Divisor of Strings
 
-use std::mem::swap;
-
 impl Solution {
     pub fn gcd_of_strings(mut str1: String, mut str2: String) -> String {
-        // Ensure str1 is the shorter of the two
-        if str1.len() > str2.len() {
-            swap(&mut str1, &mut str2);
-        }
-        let mut scratch_space = String::with_capacity(str1.len());
-        let mut divisor = str1.clone();
-        while !Self::divisible(&str1, &divisor, &mut scratch_space)
-            || !Self::divisible(&str2, &divisor, &mut scratch_space)
-        {
-            divisor.pop(); // Shorten divisor until it works
-        }
-        divisor
-    }
-
-    fn divisible(s: &str, divisor: &str, scratch_space: &mut String) -> bool {
-        debug_assert!(
-            divisor.len() <= s.len(),
-            "Expected divisor length '{} - ({})' to be at least as short as the length of s '{} - ({})' but this did not hold",
-            divisor, divisor.len(), s, s.len()
-        );
-
-        if divisor.is_empty() {
-            // Nothing divides everything (As per example, even thought it doesn't quite make sense)
-            return true;
-        }
-        if s.len() % divisor.len() != 0 {
-            // Will never be equal as it is not a multiple of it's length
-            return false;
+        if str1.is_empty() || str2.is_empty() {
+            return Default::default();
         }
 
-        scratch_space.clear();
-        scratch_space.push_str(divisor);
-        while scratch_space.len() <= s.len() {
-            if scratch_space == s {
-                return true;
+        // Move shorter string into str1
+        if str2.len() < str1.len() {
+            std::mem::swap(&mut str1, &mut str2);
+        }
+
+        let str1 = str1.as_bytes();
+        let str2 = str2.as_bytes();
+
+        let mut result = &str1[..0];
+
+        for (i, c) in str1.iter().copied().enumerate() {
+            if str2[i] != c {
+                break;
             }
-            scratch_space.push_str(divisor);
+            let prefix = &str1[..=i];
+            if str1.len() % prefix.len() == 0
+                && str2.len() % prefix.len() == 0
+                && str1
+                    .windows(prefix.len())
+                    .step_by(prefix.len())
+                    .all(|win| dbg!(win == prefix))
+                && str2
+                    .windows(prefix.len())
+                    .step_by(prefix.len())
+                    .all(|win| dbg!(win == prefix))
+            {
+                result = &str1[..=i];
+            }
         }
-        false
+        std::str::from_utf8(result)
+            .expect("str1 and str2 consist of English uppercase letters.")
+            .to_string()
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
 
 #[cfg(test)]
 mod tests {
@@ -59,6 +56,7 @@ mod tests {
     #[case("ABCABC", "ABC", "ABC")]
     #[case("ABABAB", "ABAB", "AB")]
     #[case("LEET", "CODE", "")]
+    #[case("ABCABD", "ABC", "")]
     fn case(#[case] str1: String, #[case] str2: String, #[case] expected: String) {
         let actual = Solution::gcd_of_strings(str1, str2);
         assert_eq!(actual, expected);
