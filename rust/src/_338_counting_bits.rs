64defaf22d4754889852496b996f99f7f571ab7f
One <43485962+c-git@users.noreply.github.com>
2025-03-27 23:54:02 -0400

    151. Sub 2

--- rust/src/_338_counting_bits.rs
+++ rust/src/_338_counting_bits.rs
@@ -3,11 +3,7 @@
 
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
-        let mut result = Vec::with_capacity(n as usize + 1);
-        for i in 0..=n {
-            result.push(i.count_ones() as i32);
-        }
-        result
+        (0..=n).map(|x| x.count_ones() as i32).collect()
     }
 }
 
@@ -24,7 +20,6 @@ mod tests {
     #[rstest]
     #[case(2, vec![0,1,1])]
     #[case(5, vec![0,1,1,2,1,2])]
-    #[case(20, vec![0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2])]
     fn case(#[case] n: i32, #[case] expected: Vec<i32>) {
         let actual = Solution::count_bits(n);
         assert_eq!(actual, expected);
