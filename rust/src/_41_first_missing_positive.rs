ffae89105706dc49d280989ec367f5b464b50c9f
One <43485962+c-git@users.noreply.github.com>
2025-03-27 22:45:36 -0400

    41. Sub 7

--- rust/src/_41_first_missing_positive.rs
+++ rust/src/_41_first_missing_positive.rs
@@ -4,22 +4,20 @@
 // [0,1,2]
 impl Solution {
     pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
-        if nums.len() == 1 {
-            return if nums[0] == 1 { 2 } else { 1 };
-        }
+        let n = nums.len() as i32;
         for mut index in 0..nums.len() {
             let mut num = nums[index];
             loop {
-                let Ok(index_candidate) = usize::try_from(num) else {
-                    // Not a valid usize and we are just going to skip negative values
-                    break;
-                };
-
-                if index_candidate >= nums.len() {
-                    // Not a valid index we can skip because then a number from 1..nums.len() will
-                    // be missing and that will be our answer
+                if num < 1 || num > n {
+                    // We aren't interested in values outside of 1..n
                     break;
                 }
+
+                // Not a valid usize and we are just going to skip negative values
+                let Ok(index_candidate) = usize::try_from(num) else {
+                    unreachable!("we already checked for negative values")
+                };
+                let index_candidate = index_candidate - 1; // So 1 -> 0, 2 -> 1 and so on
 
                 if index == index_candidate {
                     // Already in the right place
@@ -27,22 +25,20 @@ impl Solution {
                 }
 
                 num = nums[index_candidate]; // Store next value in chain before we overwrite
-                nums[index_candidate] = index_candidate as _; // store index in it's place so we know it's done
+                nums[index_candidate] = index_candidate as i32 + 1; // store index in it's place so we know it's done
                 index = index_candidate; // Save index for next loop
             }
         }
-        let n = nums.len();
         nums.into_iter()
             .enumerate()
-            .skip(1)
             .find_map(|(i, num)| {
-                if num == i as i32 {
+                if num > 0 && num == i as i32 + 1 {
                     None
                 } else {
-                    Some(i as i32)
+                    Some(i as i32 + 1)
                 }
             })
-            .unwrap_or(n as i32)
+            .unwrap_or(n + 1)
     }
 }
 
@@ -61,6 +57,7 @@ mod tests {
     #[case(vec![3,4,-1,1], 2)]
     #[case(vec![7,8,9,11,12], 1)]
     #[case(vec![1], 2)]
+    #[case(vec![2,1], 3)]
     fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::first_missing_positive(nums);
         assert_eq!(actual, expected);
