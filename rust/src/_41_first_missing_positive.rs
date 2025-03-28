aa0371f73ad5e191e070492f503dc9ed215f25f4
One <43485962+c-git@users.noreply.github.com>
2025-03-27 22:29:45 -0400

    41. Sub 5

--- rust/src/_41_first_missing_positive.rs
+++ rust/src/_41_first_missing_positive.rs
@@ -1,52 +1,64 @@
-use std::{
-    mem::swap,
-    ops::{Add, Sub},
-};
+//! Solution for https://leetcode.com/problems/first-missing-positive
+//! 41. First Missing Positive
 
+// [0,1,2]
 impl Solution {
     pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
+        for mut index in 0..nums.len() {
+            let mut num = nums[index];
+            loop {
+                let Ok(index_candidate) = usize::try_from(num) else {
+                    // Not a valid usize and we are just going to skip negative values
+                    break;
+                };
+
+                if index_candidate >= nums.len() {
+                    // Not a valid index we can skip because then a number from 1..nums.len() will
+                    // be missing and that will be our answer
+                    break;
+                }
+
+                if index == index_candidate {
+                    // Already in the right place
+                    break;
+                }
+
+                num = nums[index_candidate]; // Store next value in chain before we overwrite
+                nums[index_candidate] = index_candidate as _; // store index in it's place so we know it's done
+                index = index_candidate; // Save index for next loop
+            }
+        }
         let n = nums.len();
-        let n_i32 = n as i32;
-        for i in 0..n {
-            let mut curr_value = nums[i];
-            while 1 <= curr_value
-                && curr_value <= n_i32
-                && nums[curr_value.sub(1) as usize] != curr_value
-            {
-                // LI:
-                //  - curr_value-1 is a valid index into nums because it is 1 <= curr_value <= n implies 0 <= curr_value-1 < n
-                //  - curr_value has somewhere else in the array it belongs
-                let target_index = curr_value.saturating_sub(1) as usize;
-                swap(&mut curr_value, &mut nums[target_index]);
-            }
-        }
-
-        for (i, val) in nums.into_iter().enumerate() {
-            if i as i32 != val.saturating_sub(1) {
-                return i.add(1) as i32;
-            }
-        }
-
-        // None missing must be n
-        (n + 1) as i32
+        nums.into_iter()
+            .enumerate()
+            .skip(1)
+            .find_map(|(i, num)| {
+                if num == i as i32 {
+                    None
+                } else {
+                    Some(i as i32)
+                }
+            })
+            .unwrap_or(n as i32)
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     use rstest::rstest;
 
     #[rstest]
-    #[case(vec![1,2,0],3)]
-    #[case(vec![3,4,-1,1],2)]
-    #[case(vec![7,8,9,11,12],1)]
-    #[case(vec![1,0,0],2)]
-    #[case(vec![1],2)]
-    #[case(vec![2,1],3)]
-    fn case(#[case] input: Vec<i32>, #[case] expected: i32) {
-        let actual = Solution::first_missing_positive(input);
+    #[case(vec![1,2,0], 3)]
+    #[case(vec![3,4,-1,1], 2)]
+    #[case(vec![7,8,9,11,12], 1)]
+    fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
+        let actual = Solution::first_missing_positive(nums);
         assert_eq!(actual, expected);
     }
 }
