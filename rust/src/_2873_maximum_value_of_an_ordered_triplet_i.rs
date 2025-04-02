c41b6c5582fe00d8823fe6b21571a8f81011dfe7
One <43485962+c-git@users.noreply.github.com>
2025-04-02 19:57:18 -0400

    2873. Sub 2

--- rust/src/_2873_maximum_value_of_an_ordered_triplet_i.rs
+++ rust/src/_2873_maximum_value_of_an_ordered_triplet_i.rs
@@ -11,7 +11,14 @@ impl Solution {
                 i_val = j_val;
                 continue;
             }
-            for k_val in nums.iter().skip(j).copied() {
+            for k_val in nums.iter().skip(j + 2).copied() {
+                dbg!(
+                    j,
+                    i_val,
+                    j_val,
+                    k_val,
+                    (i_val - j_val) as i64 * k_val as i64
+                );
                 result = result.max((i_val - j_val) as i64 * k_val as i64)
             }
         }
@@ -33,6 +40,7 @@ mod tests {
     #[case(vec![12,6,1,2,7], 77)]
     #[case(vec![1,10,3,4,19], 133)]
     #[case(vec![1,2,3], 0)]
+    #[case(vec![2,3,1], 0)]
     fn case(#[case] nums: Vec<i32>, #[case] expected: i64) {
         let actual = Solution::maximum_triplet_value(nums);
         assert_eq!(actual, expected);
