8b26b1aee9de06146a815013fb12e942120ec341
One <43485962+c-git@users.noreply.github.com>
2025-04-07 10:36:56 -0400

    560. Subarray Sum Equals K

---
+++ rust/src/_560_subarray_sum_equals_k.rs
@@ -0,0 +1,43 @@
+//! Solution for https://leetcode.com/problems/subarray-sum-equals-k
+//! 560. Subarray Sum Equals K
+
+impl Solution {
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut result = 0;
+        let mut curr_sum = 0;
+        let mut left = 0;
+        for right_val in nums.iter() {
+            curr_sum += right_val;
+            if curr_sum == k {
+                result += 1;
+            }
+            while curr_sum > k {
+                curr_sum -= nums[left];
+                left += 1;
+                if curr_sum == k {
+                    result += 1;
+                }
+            }
+        }
+        result
+    }
+}
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![1,1,1], 2, 2)]
+    #[case(vec![1,2,3], 3, 2)]
+    fn case(#[case] nums: Vec<i32>, #[case] k: i32, #[case] expected: i32) {
+        let actual = Solution::subarray_sum(nums, k);
+        assert_eq!(actual, expected);
+    }
+}
