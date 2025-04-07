7a73428121372e3df2f3fe8c200107fc0d173b44
One <43485962+c-git@users.noreply.github.com>
2025-04-07 11:00:51 -0400

    560. Sub 4

--- rust/src/_560_subarray_sum_equals_k.rs
+++ rust/src/_560_subarray_sum_equals_k.rs
@@ -3,28 +3,28 @@
 
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let prefix_sum = {
+            // Build prefix sums
+            let mut sum = 0;
+            let mut res = Vec::with_capacity(nums.len());
+            for num in nums.iter() {
+                sum += num;
+                res.push(sum);
+            }
+            res
+        };
+
         let mut result = 0;
-        let mut curr_sum = 0;
-        let mut left = 0;
-        for (right, right_val) in nums.iter().enumerate() {
-            curr_sum += right_val;
-            if curr_sum == k {
+
+        for (right, &right_val) in prefix_sum.iter().enumerate() {
+            if right_val == k {
                 result += 1;
             }
-            while left < right && curr_sum > k {
-                curr_sum -= nums[left];
-                if curr_sum == k {
+            for left_val in prefix_sum.iter().take(right) {
+                if right_val - left_val == k {
                     result += 1;
                 }
-                left += 1;
             }
-        }
-        while left < nums.len() - 1 {
-            curr_sum -= nums[left];
-            if curr_sum == k {
-                result += 1;
-            }
-            left += 1;
         }
         result
     }
@@ -45,6 +45,7 @@ mod tests {
     #[case(vec![1,2,3], 3, 2)]
     #[case(vec![1], 0, 0)]
     #[case(vec![-1,-1,1], 0, 1)]
+    #[case(vec![28,54,7,-70,22,65,-6], 100, 1)]
     fn case(#[case] nums: Vec<i32>, #[case] k: i32, #[case] expected: i32) {
         let actual = Solution::subarray_sum(nums, k);
         assert_eq!(actual, expected);
