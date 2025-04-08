a7c6ea55ef1ee034461a3dacf108e4b856744209
One <43485962+c-git@users.noreply.github.com>
2025-04-08 18:41:28 -0400

    54. Sub 3

--- rust/src/_54_spiral_matrix.rs
+++ rust/src/_54_spiral_matrix.rs
@@ -1,186 +1,87 @@
-use Direction::*;
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-impl Direction {
-    #[must_use]
-    fn next_direction(self) -> Direction {
-        match self {
-            Up => Right,
-            Down => Left,
-            Left => Up,
-            Right => Down,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Position {
-    row: usize,
-    col: usize,
-}
+//! Solution for https://leetcode.com/problems/spiral-matrix
+//! 54. Spiral Matrix
 
 impl Solution {
     pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        if matrix.is_empty() {
-            return vec![];
-        }
-
-        let mut result = Vec::with_capacity(matrix.len() * matrix[0].len());
-
-        let (mut bound_top, mut bound_left, mut bound_bottom, mut bound_right) =
-            (0, 0, matrix.len() - 1, matrix[0].len() - 1);
-        let mut pos = Position { row: 0, col: 0 };
-        let mut direction = Right;
-        while bound_top <= bound_bottom && bound_left <= bound_right {
-            if cfg!(debug_assert) {
-                println!("{result:?}");
-            }
-            debug_assert!(
-                pos.row >= bound_top
-                    && pos.row <= bound_bottom
-                    && pos.col >= bound_left
-                    && pos.col <= bound_right,
-                    "{pos:?} Bounds left: {bound_left} right: {bound_right} top: {bound_top} bottom: {bound_bottom}"
-            );
-            result.push(matrix[pos.row][pos.col]);
-
-            // Bounds check
-            match (&pos, &direction) {
-                (Position { row, col: _ }, Up) if *row == bound_top => {
-                    bound_left += 1;
-                    direction = direction.next_direction();
-                }
-                (Position { row, col: _ }, Down) if *row == bound_bottom => {
-                    if bound_right == 0 {
-                        // Prevent underflow during subtraction
-                        break;
+        let row_count = matrix.len();
+        let col_count = matrix[0].len();
+        let mut result = Vec::with_capacity(row_count * col_count);
+        let mut bound_left = 0;
+        let mut bound_right = col_count - 1;
+        let mut bound_top = 1;
+        let mut bound_bottom = row_count - 1;
+        let mut is_row_changing = false;
+        let mut is_increasing = true;
+        let mut curr_row = 0;
+        let mut curr_col = 0;
+        while (bound_left <= bound_right && !is_row_changing)
+            || (bound_top <= bound_bottom && is_row_changing)
+        {
+            result.push(matrix[curr_row][curr_col]);
+            match (is_row_changing, is_increasing) {
+                (true, true) => {
+                    // Going Down
+                    if curr_row < bound_bottom {
+                        curr_row += 1;
+                    } else {
+                        is_row_changing = false;
+                        is_increasing = false;
+                        curr_col -= 1;
+                        bound_bottom -= 1;
                     }
-                    bound_right -= 1;
-                    direction = direction.next_direction();
                 }
-                (Position { row: _, col }, Left) if *col == bound_left => {
-                    bound_bottom -= 1;
-                    direction = direction.next_direction();
+                (true, false) => {
+                    // Going Up
+                    if curr_row > bound_top {
+                        curr_row -= 1;
+                    } else {
+                        is_row_changing = false;
+                        is_increasing = true;
+                        curr_col += 1;
+                        bound_top += 1;
+                    }
                 }
-                (Position { row: _, col }, Right) if *col == bound_right => {
-                    bound_top += 1;
-                    direction = direction.next_direction();
+                (false, true) => {
+                    // Going Right
+                    if curr_col < bound_right {
+                        curr_col += 1;
+                    } else {
+                        is_row_changing = true;
+                        curr_row += 1;
+                        bound_right -= 1;
+                    }
                 }
-                _ => (),
-            }
-
-            // Move forward
-            match &direction {
-                Up => pos.row -= 1,
-                Down => pos.row += 1,
-                Left => pos.col -= 1,
-                Right => pos.col += 1,
+                (false, false) => {
+                    // Going Left
+                    if curr_col > bound_left {
+                        curr_col -= 1;
+                    } else {
+                        is_row_changing = true;
+                        curr_row -= 1;
+                        bound_left += 1;
+                    }
+                }
             }
         }
         result
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn case1() {
-        let input = [
-            [1, 2, 3], // To put multiline
-            [4, 5, 6],
-            [7, 8, 9],
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [1, 2, 3, 6, 9, 8, 7, 4, 5];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
+    use rstest::rstest;
 
-    #[test]
-    fn case2() {
-        let input = [
-            [1, 2, 3, 4], //
-            [5, 6, 7, 8],
-            [9, 10, 11, 12],
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case3() {
-        let input = [
-            [11, 12, 13, 14, 15],
-            [21, 22, 23, 24, 25],
-            [31, 32, 33, 34, 35],
-            [41, 42, 43, 44, 45],
-            [51, 52, 53, 54, 55],
-            [61, 62, 63, 64, 65],
-            [71, 72, 73, 74, 75],
-            [81, 82, 83, 84, 85],
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [
-            11, 12, 13, 14, 15, 25, 35, 45, 55, 65, 75, 85, 84, 83, 82, 81, 71, 61, 51, 41, 31, 21,
-            22, 23, 24, 34, 44, 54, 64, 74, 73, 72, 62, 52, 42, 32, 33, 43, 53, 63,
-        ];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case4() {
-        let input = [
-            [3], //
-            [2],
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [3, 2];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case5() {
-        let input = [
-            [1, 2], //
-            [3, 4],
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [1, 2, 4, 3];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case6() {
-        let input = [
-            [1, 2], //
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [1, 2];
-        let actual = Solution::spiral_order(input);
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case7() {
-        let input = [
-            [1], //
-        ];
-        let input = input.into_iter().map(|x| x.into()).collect();
-        let expected = [1];
-        let actual = Solution::spiral_order(input);
+    #[rstest]
+    #[case(vec![vec![1,2,3],vec![4,5,6],vec![7,8,9]], vec![1,2,3,6,9,8,7,4,5])]
+    #[case(vec![vec![1,2,3,4],vec![5,6,7,8],vec![9,10,11,12]], vec![1,2,3,4,8,12,11,10,9,5,6,7])]
+    fn case(#[case] matrix: Vec<Vec<i32>>, #[case] expected: Vec<i32>) {
+        let actual = Solution::spiral_order(matrix);
         assert_eq!(actual, expected);
     }
 }
