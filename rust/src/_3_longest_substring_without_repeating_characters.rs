c0d3bb939401c2e6a79403f4580b9fd40fe28579
One <43485962+c-git@users.noreply.github.com>
2025-04-07 22:11:27 -0400

    3. Sub 3

--- rust/src/_3_longest_substring_without_repeating_characters.rs
+++ rust/src/_3_longest_substring_without_repeating_characters.rs
@@ -1,20 +1,21 @@
 //! Solution for https://leetcode.com/problems/longest-substring-without-repeating-characters
 //! 3. Longest Substring Without Repeating Characters
 
+use std::collections::HashMap;
+
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut result = 0;
         let s = s.as_bytes();
         let mut left_idx = 0;
-        let mut freq_count = [0u8; 26];        
-        for (right_idx, right_val) in s.iter().enumerate() {
-            let right_char_idx = (right_val - b'a') as usize;
-            freq_count[right_char_idx] += 1;
-            while freq_count[right_char_idx] > 1 {
-                freq_count[(s[left_idx] - b'a') as usize] -= 1;
+        let mut freq_count: HashMap<u8, u8> = HashMap::new();
+        for (right_idx, &right_val) in s.iter().enumerate() {
+            *freq_count.entry(right_val).or_default() += 1;
+            while *freq_count.get(&right_val).unwrap() > 1 {
+                *freq_count.get_mut(&s[left_idx]).unwrap() -= 1;
                 left_idx += 1;
             }
-            result = result.max(right_idx-left_idx+1);
+            result = result.max(right_idx - left_idx + 1);
         }
         result as i32
     }
