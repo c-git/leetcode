90aefc0d79e4da835b11663e1b415e4c012426ba
One <43485962+c-git@users.noreply.github.com>
2025-04-09 00:33:01 -0400

    443. String Compression

---
+++ rust/src/_443_string_compression.rs
@@ -0,0 +1,37 @@
+//! Solution for https://leetcode.com/problems/string-compression
+//! 443. String Compression
+
+impl Solution {
+    pub fn compress(chars: &mut Vec<char>) -> i32 {        
+        let mut last_char = *chars.first().unwrap();
+        let mut curr_count = 1;
+        let mut next_write_index = 0;
+        for i in 1..chars.len() {
+            if last_char == chars[i] {
+                curr_count += 1;
+            } else {
+                chars[next_write_index] = last_char;
+                next_write_index += 1;
+                if curr_count > 1 {                    
+                    for digit in curr_count.to_string().chars() {                        
+                        chars[next_write_index] = digit;
+                        next_write_index += 1;
+                    }
+                }
+                last_char = chars[i];
+                curr_count = 1;
+            }
+            
+        }        
+        chars[next_write_index] = last_char;
+        next_write_index += 1;
+        if curr_count > 1 {                    
+            for digit in curr_count.to_string().chars() {                        
+                chars[next_write_index] = digit;
+                next_write_index += 1;
+            }
+        }
+
+        next_write_index as i32
+    }
+}
