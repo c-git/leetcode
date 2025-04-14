589c312ab4431d13f8c6f22f072526a544f112e8
One <43485962+c-git@users.noreply.github.com>
2025-04-14 01:33:05 -0400

    2. Sub 2

--- rust/src/_2_add_two_numbers.rs
+++ rust/src/_2_add_two_numbers.rs
@@ -1,72 +1,77 @@
+//! Solution for https://leetcode.com/problems/add-two-numbers
+//! 2. Add Two Numbers
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
 impl Solution {
     pub fn add_two_numbers(
-        l1: Option<Box<ListNode>>,
-        l2: Option<Box<ListNode>>,
+        mut l1: Option<Box<ListNode>>,
+        mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        // Source: https://github.com/zwhitchcox/leetcode/blob/master/src/0002_add_two_numbers.rs
+        let mut has_carry = false;
         let mut result = None;
-
-        // Make inputs mutable to be able to walk down the list
-        let mut l1 = l1;
-        let mut l2 = l2;
-
-        let mut carry = 0;
-        let mut curr = &mut result;
-        while l1.is_some() || l2.is_some() || carry != 0 {
-            let mut sum = carry;
-            if let Some(node) = l1 {
-                sum += node.val;
-                l1 = node.next;
+        let mut tail = &mut result;
+        loop {
+            (l1, l2, has_carry) = match (l1, l2, has_carry) {
+                (None, None, true) => {
+                    *tail = Some(Box::new(ListNode::new(1)));
+                    (None, None, false)
+                }
+                (None, None, false) => break,
+                (None, Some(x), has_carry) | (Some(x), None, has_carry) => {
+                    let next_val = x.val + if has_carry { 1 } else { 0 };
+                    *tail = Some(Box::new(ListNode::new(next_val % 10)));
+                    tail = &mut tail.as_mut().unwrap().next;
+                    (x.next, None, next_val >= 10)
+                }
+                (Some(x), Some(y), has_carry) => {
+                    let next_val = x.val + y.val + if has_carry { 1 } else { 0 };
+                    *tail = Some(Box::new(ListNode::new(next_val % 10)));
+                    tail = &mut tail.as_mut().unwrap().next;
+                    (x.next, y.next, next_val >= 10)
+                }
             }
-            if let Some(node) = l2 {
-                sum += node.val;
-                l2 = node.next;
-            }
-            carry = sum / 10;
-            *curr = Some(Box::new(ListNode::new(sum % 10)));
-            curr = &mut curr.as_mut().unwrap().next;
         }
-
         result
     }
 }
 
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
 use cargo_leet::ListNode;
 
-struct Solution;
 #[cfg(test)]
-mod test {
+mod tests {
+    use super::*;
     use cargo_leet::ListHead;
 
-    use super::Solution;
+    use rstest::rstest;
 
-    #[test]
-    fn case1() {
-        let l1: ListHead = vec![2, 4, 3].into();
-        let l2: ListHead = vec![5, 6, 4].into();
-        let expected: ListHead = vec![7, 0, 8].into();
-
-        let actual = Solution::add_two_numbers(l1.into(), l2.into());
-        assert_eq!(actual, expected.into());
-    }
-
-    #[test]
-    fn case2() {
-        let l1: ListHead = vec![0].into();
-        let l2: ListHead = vec![0].into();
-        let expected: ListHead = vec![0].into();
-
-        let actual = Solution::add_two_numbers(l1.into(), l2.into());
-        assert_eq!(actual, expected.into());
-    }
-
-    #[test]
-    fn case3() {
-        let l1: ListHead = vec![9, 9, 9, 9, 9, 9, 9].into();
-        let l2: ListHead = vec![9, 9, 9, 9].into();
-        let expected: ListHead = vec![8, 9, 9, 9, 0, 0, 0, 1].into();
-
-        let actual = Solution::add_two_numbers(l1.into(), l2.into());
-        assert_eq!(actual, expected.into());
+    #[rstest]
+    #[case(ListHead::from(vec![2,4,3]).into(), ListHead::from(vec![5,6,4]).into(), ListHead::from(vec![7,0,8]).into())]
+    #[case(ListHead::from(vec![0]).into(), ListHead::from(vec![0]).into(), ListHead::from(vec![0]).into())]
+    #[case(ListHead::from(vec![9,9,9,9,9,9,9]).into(), ListHead::from(vec![9,9,9,9]).into(), ListHead::from(vec![8,9,9,9,0,0,0,1]).into())]
+    fn case(
+        #[case] l1: Option<Box<ListNode>>,
+        #[case] l2: Option<Box<ListNode>>,
+        #[case] expected: Option<Box<ListNode>>,
+    ) {
+        let actual = Solution::add_two_numbers(l1, l2);
+        assert_eq!(actual, expected);
     }
 }
