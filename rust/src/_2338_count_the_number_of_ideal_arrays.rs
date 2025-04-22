bc306b4aa437985ba53fb7f7e84fd1c8a86970fb
One <43485962+c-git@users.noreply.github.com>
2025-04-22 00:21:52 -0400

    2338. Sub 6

--- rust/src/_2338_count_the_number_of_ideal_arrays.rs
+++ rust/src/_2338_count_the_number_of_ideal_arrays.rs
@@ -3,22 +3,30 @@
 
 impl Solution {
     /// Copied from Editorial
+    /// See also explanation at https://leetcode.com/problems/count-the-number-of-ideal-arrays/solutions/6675762/explaining-the-editorial-by-kosievdmerwe-gvhh/
+    /// Unable to fully grok, decided to abandon in the interest of time
     pub fn ideal_arrays(n: i32, max_value: i32) -> i32 {
         const MOD: i64 = 1_000_000_007;
-        const MAX_N: usize = 10_000 + 10;
-        const MAX_P: usize = 15; // There are up to 15 prime factors
+        let max_n: usize = max_value as usize + 10; // One: Don't know why the 10 is added
+        const MAX_PRIMES: usize = 15; // There are up to 15 prime factors [One: don't know where the 15 came from but
+                                      // guessing it's related to 2^14 being more than 10^4]
 
-        let mut sieve = vec![0; MAX_N]; // Minimum prime factor
-        for i in 2..MAX_N {
+        let mut sieve = vec![0; max_n]; // Minimum prime factor [One: based on code looks like max prime factor]
+        for i in 2..max_n {
             if sieve[i] == 0 {
-                for j in (i..MAX_N).step_by(i) {
+                for j in (i..max_n).step_by(i) {
                     sieve[j] = i as i32;
                 }
             }
         }
 
-        let mut ps = vec![vec![]; MAX_N]; // List of prime factor counts
-        for i in 2..=max_value as usize {
+        let mut prime_counts = vec![vec![]; max_n]; // List of prime factor counts
+        for (i, prime_count) in prime_counts
+            .iter_mut()
+            .take(max_value as usize + 1)
+            .enumerate()
+            .skip(2)
+        {
             let mut x = i;
             while x > 1 {
                 let p = sieve[x] as usize;
@@ -27,30 +35,31 @@ impl Solution {
                     x /= p;
                     cnt += 1;
                 }
-                ps[i].push(cnt);
+                prime_count.push(cnt);
             }
         }
 
-        let mut c = vec![vec![0; MAX_P + 1]; n as usize + MAX_P + 1];
-        c[0][0] = 1;
-        for i in 1..n as usize + MAX_P + 1 {
-            c[i][0] = 1;
-            for j in 1..=i.min(MAX_P) {
-                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
+        let mut dp = vec![vec![0; MAX_PRIMES + 1]; n as usize + MAX_PRIMES + 1];
+        dp[0][0] = 1;
+        for i in 1..n as usize + MAX_PRIMES + 1 {
+            dp[i][0] = 1;
+            for j in 1..=i.min(MAX_PRIMES) {
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
             }
         }
 
-        let mut ans = 0i64;
+        let mut result = 0i64;
         let n = n as usize;
-        for x in 1..=max_value as usize {
+        for prime_count in prime_counts.iter().take(max_value as usize + 1).skip(1) {
             let mut mul = 1i64;
-            for &p in &ps[x] {
-                mul = mul * c[n + p as usize - 1][p as usize] % MOD;
+            for &prime_count_element in prime_count {
+                mul = mul * dp[n + prime_count_element as usize - 1][prime_count_element as usize]
+                    % MOD;
             }
-            ans = (ans + mul) % MOD;
+            result = (result + mul) % MOD;
         }
 
-        ans as i32
+        result as i32
     }
 }
 
