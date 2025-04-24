1dc6a990400c987a8e3e79726647610ed7eab29c
One <43485962+c-git@users.noreply.github.com>
2025-04-23 22:34:47 -0400

    4. Sub 5

--- rust/src/_4_median_of_two_sorted_arrays.rs
+++ rust/src/_4_median_of_two_sorted_arrays.rs
@@ -2,33 +2,60 @@
 //! 4. Median of Two Sorted Arrays
 
 impl Solution {
-    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        // Testing lint code solution
-        let total = nums1.len() + nums2.len();
-        if total % 2 == 1 {
-            Self::get_k_th(&nums1, &nums2, total / 2 + 1) as f64
-        } else {
-            let left = Self::get_k_th(&nums1, &nums2, total / 2);
-            let right = Self::get_k_th(&nums1, &nums2, total / 2 + 1);
-            (left + right) as f64 / 2f64
+    /// Based on https://www.youtube.com/watch?v=q6IEA26hvXc
+    pub fn find_median_sorted_arrays(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> f64 {
+        let total_len = nums1.len() + nums2.len();
+        let half_len = total_len / 2;
+        // Ensure nums1 is shorter as we are going to binary search on that one
+        if nums1.len() > nums2.len() {
+            std::mem::swap(&mut nums1, &mut nums2);
         }
-    }
+        let mut left = 0;
+        let mut right = nums1.len() as i32 - 1;
+        loop {
+            let nums1_mid = (left + right).div_euclid(2);
+            let nums2_mid = half_len as i32 - nums1_mid - 2;
 
-    fn get_k_th(a: &[i32], b: &[i32], k: usize) -> i32 {
-        // Switch if a is longer than b
-        if a.len() > b.len() {
-            Self::get_k_th(b, a, k)
-        } else if a.is_empty() {
-            b[k - 1]
-        } else if k == 1 {
-            a[0].min(b[0])
-        } else {
-            let mid1 = a.len().min(k / 2) - 1;
-            let mid2 = b.len().min(k / 2) - 1;
-            if a[mid1] > b[mid2] {
-                Self::get_k_th(a, &b[mid2 + 1..], k - (mid2 + 1))
+            let nums1_left = if nums1_mid >= 0 {
+                nums1[nums1_mid as usize]
             } else {
-                Self::get_k_th(&a[mid1 + 1..], b, k - (mid1 + 1))
+                i32::MIN
+            };
+
+            let nums1_right = if nums1_mid + 1 < nums1.len() as i32 {
+                nums1[(nums1_mid + 1) as usize]
+            } else {
+                i32::MAX
+            };
+
+            let nums2_left = if nums2_mid >= 0 {
+                nums2[nums2_mid as usize]
+            } else {
+                i32::MIN
+            };
+
+            let nums2_right = if nums2_mid + 1 < nums2.len() as i32 {
+                nums2[(nums2_mid + 1) as usize]
+            } else {
+                i32::MAX
+            };
+
+            if nums1_left <= nums2_right && nums2_left <= nums1_right {
+                // Partition is not correct, return median
+                if total_len % 2 == 0 {
+                    // Even
+                    return (nums1_left.max(nums2_left) + nums1_right.min(nums2_right)) as f64
+                        / 2.0;
+                } else {
+                    // Odd
+                    return nums1_right.min(nums2_right).into();
+                }
+            } else if nums1_left > nums2_right {
+                right = nums1_mid - 1
+            } else if nums2_left > nums1_right {
+                left = nums1_mid + 1
+            } else {
+                unreachable!("One of the two above should have triggered")
             }
         }
     }
