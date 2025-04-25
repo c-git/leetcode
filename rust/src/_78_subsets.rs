fb5ef4a5088457ecb8bbac3b1839ab264b8a5fd5
One <43485962+c-git@users.noreply.github.com>
2025-04-25 02:43:38 -0400

    78. Sub 2

--- rust/src/_78_subsets.rs
+++ rust/src/_78_subsets.rs
@@ -2,18 +2,26 @@
 //! 78. Subsets
 
 impl Solution {
+    /// Based on https://www.youtube.com/watch?v=REOH22Xwdkk
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        // Based on editorial
-        let mut result = vec![vec![]];
-        for num in nums {
-            let n = result.len();
-            for i in 0..n {
-                let mut next = result[i].clone();
-                next.push(num);
-                result.push(next);
-            }
-        }
+        let mut result = vec![];
+        Self::dfs(&nums, 0, &mut vec![], &mut result);
         result
+    }
+
+    fn dfs(nums: &[i32], i: usize, subset: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
+        if i >= nums.len() {
+            result.push(subset.clone());
+            return;
+        }
+
+        // Include nums[i]
+        subset.push(nums[i]); // Add value to include it
+        Self::dfs(nums, i + 1, subset, result);
+
+        // Skip nums[i]
+        subset.pop(); // Remove value added (Backtrack step)
+        Self::dfs(nums, i + 1, subset, result);
     }
 }
 
@@ -30,8 +38,10 @@ mod tests {
     #[rstest]
     #[case(vec![1,2,3], vec![vec![],vec![1],vec![2],vec![1,2],vec![3],vec![1,3],vec![2,3],vec![1,2,3]])]
     #[case(vec![0], vec![vec![],vec![0]])]
-    fn case(#[case] nums: Vec<i32>, #[case] expected: Vec<Vec<i32>>) {
-        let actual = Solution::subsets(nums);
+    fn case(#[case] nums: Vec<i32>, #[case] mut expected: Vec<Vec<i32>>) {
+        let mut actual = Solution::subsets(nums);
+        actual.sort_unstable();
+        expected.sort_unstable();
         assert_eq!(actual, expected);
     }
 }
