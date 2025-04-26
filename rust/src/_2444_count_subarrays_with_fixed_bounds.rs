427fcb0692276bc102c424f206f901866354a68d
One <43485962+c-git@users.noreply.github.com>
2025-04-26 16:15:20 -0400

    2444. Sub 2

--- rust/src/_2444_count_subarrays_with_fixed_bounds.rs
+++ rust/src/_2444_count_subarrays_with_fixed_bounds.rs
@@ -3,47 +3,24 @@
 
 impl Solution {
     /// Based on https://www.youtube.com/watch?v=Bk-HxzaooqM
-    /// I wasn't fully sold by his explanation but the code matches what I
-    /// eventually understood.
-    ///
-    /// My understanding is that you want
-    /// - Only count subarrays when you have both exactly the correct min and
-    ///   max in the current partition being considered
-    /// - Count the number of subarrays you can make until you can't go back any
-    ///   further because there is an out of bounds value using the smaller of
-    ///   the min and max as the point from where we can take each subset
     pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
         let mut result = 0;
-        // First valid position we can use
-        let Some(mut partition_start) = nums.iter().enumerate().find_map(|(i, &num)| {
-            if min_k <= num && num <= max_k {
-                Some(i)
-            } else {
-                None
-            }
-        }) else {
-            return result;
-        };
-        let mut closest_min = None;
-        let mut closest_max = None;
-        for (i, num) in nums.into_iter().enumerate().skip(partition_start) {
+        let mut bad_i = -1;
+        let mut min_i = -1;
+        let mut max_i = -1;
+        let n = nums.len() as i32;
+        for i in 0..n {
+            let num = nums[i as usize];
             if num < min_k || num > max_k {
-                closest_min = None;
-                closest_max = None;
-                partition_start = i + 1; // Let's try for the next index
-                continue;
+                bad_i = i
             }
             if num == min_k {
-                closest_min = Some(i);
+                min_i = i
             }
             if num == max_k {
-                closest_max = Some(i);
+                max_i = i
             }
-            if let (Some(min), Some(max)) = (closest_min, closest_max) {
-                // We have the min and max in range so calculate how many at the start can be
-                // left out plus one for none included
-                result += (std::cmp::min(min, max) - partition_start + 1) as i64
-            }
+            result += std::cmp::max(0, std::cmp::min(min_i, max_i) - bad_i) as i64;
         }
         result
     }
