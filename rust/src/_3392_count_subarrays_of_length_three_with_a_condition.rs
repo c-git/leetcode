63f9ff09c6fc37220906ef24d57e8007bd194401
One <43485962+c-git@users.noreply.github.com>
2025-04-27 12:12:53 -0400

    3392. Count Subarrays of Length Three With a Condition

---
+++ rust/src/_3392_count_subarrays_of_length_three_with_a_condition.rs
@@ -0,0 +1,33 @@
+//! Solution for https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition
+//! 3392. Count Subarrays of Length Three With a Condition
+
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+        for window in nums.windows(3){
+            if (window[0] + window[2]) == window[1] {
+                result += 1;
+            }
+        }
+        result
+    }
+}
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![1,2,1,4,1], 1)]
+    #[case(vec![1,1,1], 0)]
+    fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
+        let actual = Solution::count_subarrays(nums);
+        assert_eq!(actual, expected);
+    }
+}
