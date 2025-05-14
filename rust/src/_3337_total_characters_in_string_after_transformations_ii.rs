32a078c1800ac2a26ac8e5fc2e8d85842014df5d
One <43485962+c-git@users.noreply.github.com>
2025-05-14 12:05:13 -0400

    3337. Sub 3

--- rust/src/_3337_total_characters_in_string_after_transformations_ii.rs
+++ rust/src/_3337_total_characters_in_string_after_transformations_ii.rs
@@ -1,31 +1,85 @@
 //! Solution for https://leetcode.com/problems/total-characters-in-string-after-transformations-ii
 //! 3337. Total Characters in String After Transformations II
 
-impl Solution {
-    pub fn length_after_transformations(s: String, t: i32, nums: Vec<i32>) -> i32 {
-        const MOD: i32 = 1_000_000_007;
-        let mut char_count = [0; 26];
-        for ch in s.chars() {
-            char_count[(ch as u8 - b'a') as usize] += 1;
+const MOD: i64 = 1_000_000_007;
+const L: usize = 26;
+
+struct Mat {
+    a: [[i64; L]; L],
+}
+
+impl Mat {
+    fn new() -> Self {
+        Mat { a: [[0; L]; L] }
+    }
+
+    fn copy_from(&mut self, other: &Mat) {
+        for i in 0..L {
+            for j in 0..L {
+                self.a[i][j] = other.a[i][j];
+            }
         }
-        for _ in 0..t {
-            let mut alt_count = [0; 26];
-            for (char_idx, shift_count) in nums
-                .iter()
-                .map(|&shift_count| shift_count as usize)
-                .enumerate()
-            {
-                for shift in 1..=shift_count {
-                    let new_idx = (char_idx + shift) % 26;
-                    alt_count[new_idx] = (alt_count[new_idx] + char_count[char_idx]) % MOD;
+    }
+
+    fn mul(&self, other: &Mat) -> Mat {
+        let mut result = Mat::new();
+        for i in 0..L {
+            for j in 0..L {
+                for k in 0..L {
+                    result.a[i][j] = (result.a[i][j] + self.a[i][k] * other.a[k][j]) % MOD;
                 }
             }
-            std::mem::swap(&mut char_count, &mut alt_count);
+        }
+        result
+    }
+}
+
+/* identity matrix */
+fn I() -> Mat {
+    let mut m = Mat::new();
+    for i in 0..L {
+        m.a[i][i] = 1;
+    }
+    m
+}
+
+/* matrix exponentiation by squaring */
+fn quickmul(x: &Mat, mut y: i32) -> Mat {
+    let mut ans = I();
+    let mut cur = Mat::new();
+    cur.copy_from(x);
+    while y > 0 {
+        if y & 1 == 1 {
+            ans = ans.mul(&cur);
+        }
+        cur = cur.mul(&cur);
+        y >>= 1;
+    }
+    ans
+}
+
+impl Solution {
+    /// Copied from editorial, going to pass on the matrix multiplication optimization....
+    pub fn length_after_transformations(s: String, t: i32, nums: Vec<i32>) -> i32 {
+        let mut T = Mat::new();
+        for i in 0..L {
+            for j in 1..=nums[i] as usize {
+                T.a[(i + j) % L][i] = 1;
+            }
         }
 
-        char_count
-            .into_iter()
-            .fold(0, |acc, count| (acc + count) % MOD)
+        let res = quickmul(&T, t);
+        let mut f = [0; L];
+        for ch in s.chars() {
+            f[(ch as u8 - b'a') as usize] += 1;
+        }
+        let mut ans: i64 = 0;
+        for i in 0..L {
+            for j in 0..L {
+                ans = (ans + res.a[i][j] * f[j]) % MOD;
+            }
+        }
+        ans as i32
     }
 }
 
