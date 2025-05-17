e6f36012152ac8d711f525e3da5323334653ecbf
One <43485962+c-git@users.noreply.github.com>
2025-05-16 20:29:48 -0400

    75. Sub 4

--- rust/src/_75_sort_colors.rs
+++ rust/src/_75_sort_colors.rs
@@ -3,17 +3,18 @@
 
 impl Solution {
     pub fn sort_colors(nums: &mut [i32]) {
-        // Use radix sort for linear runtime
-        let mut freq_counts = [0; 3];
-        for &num in nums.iter() {
-            freq_counts[num as usize] += 1;
+        let freq_count = nums.iter().fold([0, 0, 0], |mut acc, &num| {
+            acc[num as usize] += 1;
+            acc
+        });
+        for num in nums.iter_mut().take(freq_count[0]) {
+            *num = 0;
         }
-        let mut done = 0;
-        for color in 0..freq_counts.len() as i32 {
-            for num in nums.iter_mut().skip(done).take(freq_counts[color as usize]) {
-                *num = color;
-            }
-            done += freq_counts[color as usize];
+        for num in nums.iter_mut().skip(freq_count[0]).take(freq_count[1]) {
+            *num = 1;
+        }
+        for num in nums.iter_mut().skip(freq_count[0] + freq_count[1]) {
+            *num = 2;
         }
     }
 }
@@ -24,8 +25,6 @@ pub struct Solution;
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
-
     use super::*;
 
     use rstest::rstest;
@@ -34,25 +33,9 @@ mod tests {
     #[case(vec![2,0,2,1,1,0])]
     #[case(vec![2,0,1])]
     fn case(#[case] mut nums: Vec<i32>) {
+        let mut expected = nums.clone();
+        expected.sort_unstable();
         Solution::sort_colors(&mut nums);
-        validate(&nums);
-    }
-
-    /// Ensure we only see each number in contiguous blocks
-    fn validate(nums: &[i32]) {
-        let mut seen = HashSet::new();
-        let Some(mut last_seen) = nums.first() else {
-            return; // Empty is fine
-        };
-        seen.insert(last_seen);
-        for num in nums.iter().skip(1) {
-            if num != last_seen {
-                assert!(
-                    seen.insert(num),
-                    "{num} seen twice no adjacent to each other"
-                );
-                last_seen = num;
-            }
-        }
+        assert_eq!(nums, expected);
     }
 }
