f86234098511e8e43fcb8da5180be1f48f01f200
One <43485962+c-git@users.noreply.github.com>
2025-05-23 19:56:57 -0400

    3068. Sub 3

--- rust/src/_3068_find_the_maximum_sum_of_node_values.rs
+++ rust/src/_3068_find_the_maximum_sum_of_node_values.rs
@@ -2,28 +2,37 @@
 //! 3068. Find the Maximum Sum of Node Values
 
 impl Solution {
-    /// After skimming the editorial but only really taking away the concept of
-    /// of chains allowing you to have an effective operation on any pair of
-    /// nodes
+    /// Translated from editorial
     pub fn maximum_value_sum(nums: Vec<i32>, k: i32, _edges: Vec<Vec<i32>>) -> i64 {
-        let mut result = 0;
-        let mut min_delta = (nums[0] ^ k) - nums[0];
-        let mut is_even_changed = true;
-        for num in nums {
-            result += num as i64;
-            let delta = (num ^ k) - num;
-            if delta > 0 {
-                result += delta as i64;
-                is_even_changed = !is_even_changed;
-            }
-            if delta.abs() < min_delta.abs() {
-                min_delta = delta;
+        let mut sum_val = 0i64;
+        let mut count = 0;
+        let mut positive_minimum = i32::MAX;
+        let mut negative_maximum = i32::MIN;
+
+        for node_value in nums {
+            let operated_node_value = node_value ^ k;
+            sum_val += node_value as i64;
+            let net_change = operated_node_value - node_value;
+
+            if net_change > 0 {
+                positive_minimum = positive_minimum.min(net_change);
+                sum_val += net_change as i64;
+                count += 1;
+            } else {
+                negative_maximum = negative_maximum.max(net_change);
             }
         }
-        if !is_even_changed {
-            result += min_delta as i64;
+
+        // If the number of positive netChange values is even, return the sum.
+        if count % 2 == 0 {
+            sum_val
+        } else {
+            // Otherwise return the maximum of both discussed cases.
+            std::cmp::max(
+                sum_val - positive_minimum as i64,
+                sum_val + negative_maximum as i64,
+            )
         }
-        result
     }
 }
 
@@ -42,6 +51,7 @@ mod tests {
     #[case(vec![2,3], 7, vec![vec![0,1]], 9)]
     #[case(vec![7,7,7,7,7,7], 3, vec![vec![0,1],vec![0,2],vec![0,3],vec![0,4],vec![0,5]], 42)]
     #[case(vec![24,78,1,97,44], 6, vec![], 260)] //edges not copied
+    #[case(vec![3,45,1,27,87,43,62], 8, vec![], 284)] //edges not copied
     fn case(
         #[case] nums: Vec<i32>,
         #[case] k: i32,
