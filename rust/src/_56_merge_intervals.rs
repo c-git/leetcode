308ee3f4e335d9b1e09b8bc45d646176914917c3
One <43485962+c-git@users.noreply.github.com>
2025-05-31 22:43:42 -0400

    56. Sub 3

--- rust/src/_56_merge_intervals.rs
+++ rust/src/_56_merge_intervals.rs
@@ -3,26 +3,19 @@
 
 impl Solution {
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        // Sort intervals so that any interval that starts before the end of the current one being merged ends must show up after
+        let mut result = vec![];
         intervals.sort_unstable();
-
-        // Store current interval being merged at tail of result
-        let mut result = vec![intervals
-            .first()
-            .expect("constraint guarantees at least one interval")
-            .clone()];
-
-        // Step though intervals merging or creating a new interval for each seen
-        for interval in intervals.into_iter() {
-            let last = result.last_mut().expect("starts off non-empty");
-            if interval[0] <= last[1] {
-                // Overlapping merge
-                last[1] = last[1].max(interval[1]);
+        let mut it = intervals.into_iter();
+        let mut current = it.next().unwrap();
+        for interval in it {
+            if current[1] >= interval[0] {
+                current[1] = current[1].max(interval[1]);
             } else {
-                // Non-overlapping start a new interval
-                result.push(interval);
+                result.push(current);
+                current = interval;
             }
         }
+        result.push(current);
         result
     }
 }
@@ -40,7 +33,6 @@ mod tests {
     #[rstest]
     #[case(vec![vec![1,3],vec![2,6],vec![8,10],vec![15,18]], vec![vec![1,6],vec![8,10],vec![15,18]])]
     #[case(vec![vec![1,4],vec![4,5]], vec![vec![1,5]])]
-    #[case(vec![vec![1,4],vec![4,5],vec![1,3]], vec![vec![1,5]])]
     fn case(#[case] intervals: Vec<Vec<i32>>, #[case] expected: Vec<Vec<i32>>) {
         let actual = Solution::merge(intervals);
         assert_eq!(actual, expected);
