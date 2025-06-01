a63eb4c7ff30320be6382cbd2c51129b8c246381
One <43485962+c-git@users.noreply.github.com>
2025-05-31 21:42:19 -0400

    347. Sub 5

--- rust/src/_347_top_k_frequent_elements.rs
+++ rust/src/_347_top_k_frequent_elements.rs
@@ -1,70 +1,53 @@
+//! Solution for https://leetcode.com/problems/top-k-frequent-elements
+//! 347. Top K Frequent Elements
+
 use std::{
     cmp::Reverse,
     collections::{BinaryHeap, HashMap},
 };
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-struct HeapNode {
-    freq: i32,
-    num: i32,
-}
-
 impl Solution {
-    // After reading editorial
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let k = k as usize;
-        let mut frequencies = HashMap::new();
-        for num in nums {
-            *frequencies.entry(num).or_insert(0) += 1;
-        }
-
-        let mut heap = BinaryHeap::with_capacity(k + 1);
-        for (num, freq) in frequencies {
-            heap.push(Reverse(HeapNode { freq, num }));
-            if heap.len() > k {
-                heap.pop();
-            }
-        }
-
-        heap.into_iter().map(|x| x.0.num).collect()
+        let freq_count = nums
+            .iter()
+            .fold(HashMap::<i32, usize>::new(), |mut acc, num| {
+                *acc.entry(*num).or_default() += 1;
+                acc
+            });
+        let most_freq = freq_count
+            .into_iter()
+            .fold(BinaryHeap::new(), |mut heap, (num, freq)| {
+                heap.push(Reverse((freq, num)));
+                if heap.len() > k {
+                    heap.pop();
+                }
+                heap
+            });
+        most_freq
+            .into_iter()
+            .map(|Reverse((_freq, num))| num)
+            .collect()
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn validate(mut actual: Vec<i32>, mut expected: Vec<i32>) {
-        actual.sort_unstable();
-        expected.sort_unstable();
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![1,1,1,2,2,3], 2, vec![1,2])]
+    #[case(vec![1], 1, vec![1])]
+    fn case(#[case] nums: Vec<i32>, #[case] k: i32, #[case] mut expected: Vec<i32>) {
+        let mut actual = Solution::top_k_frequent(nums, k);
+        actual.sort();
+        expected.sort();
         assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case1() {
-        let nums = vec![1, 1, 1, 2, 2, 3];
-        let k = 2;
-        let expected = vec![1, 2];
-        let actual = Solution::top_k_frequent(nums, k);
-        validate(actual, expected);
-    }
-
-    #[test]
-    fn case2() {
-        let nums = vec![1];
-        let k = 1;
-        let expected = vec![1];
-        let actual = Solution::top_k_frequent(nums, k);
-        validate(actual, expected);
-    }
-
-    #[test]
-    fn case3() {
-        let nums = vec![1, 1, 1, 1];
-        let k = 1;
-        let expected = vec![1];
-        let actual = Solution::top_k_frequent(nums, k);
-        validate(actual, expected);
     }
 }
