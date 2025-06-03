e883c0c424ea68562a3d36a9afb4ba430db7de86
One <43485962+c-git@users.noreply.github.com>
2025-06-03 00:39:15 -0400

    23. Sub 2

--- rust/src/_23_merge_k_sorted_lists.rs
+++ rust/src/_23_merge_k_sorted_lists.rs
@@ -1,3 +1,6 @@
+//! Solution for https://leetcode.com/problems/merge-k-sorted-lists
+//! 23. Merge k Sorted Lists
+
 // Definition for singly-linked list.
 // #[derive(PartialEq, Eq, Clone, Debug)]
 // pub struct ListNode {
@@ -15,98 +18,74 @@
 //   }
 // }
 
-use std::collections::BinaryHeap;
+struct HeapItem(Box<ListNode>);
 
-struct HeapItem {
-    head: Box<ListNode>,
-}
-
-impl Ord for HeapItem {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.head.val.cmp(&self.head.val)
-    }
-}
-
-impl PartialOrd for HeapItem {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        other.head.val.partial_cmp(&self.head.val)
-    }
-}
-
-impl PartialEq for HeapItem {
+impl PartialEq for HeapItem{
     fn eq(&self, other: &Self) -> bool {
-        self.head.val == other.head.val
+        // Doesn't do a deep check for equality
+        self.0.val == other.0.val
     }
 }
 
-impl Eq for HeapItem {}
+impl Eq for HeapItem{}
 
+impl  PartialOrd for HeapItem {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for HeapItem{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Provide a reversed result so min will come to the top of the heap
+        other.0.val.cmp(&self.0.val)
+    }
+}
 impl Solution {
     pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
         let mut result = None;
-
-        // Collect lists into a heap
-        let mut queue = BinaryHeap::with_capacity(lists.len());
-        for head in lists.into_iter().flatten() {
-            // flatten removes any None values
-            queue.push(HeapItem { head });
+        let mut tail = &mut result;
+        let mut heap = std::collections::BinaryHeap::new();
+        for list_head in lists.into_iter().flatten(){
+                heap.push(HeapItem(list_head));
         }
-
-        // Pull from heap until empty
-        let mut node = &mut result;
-        while let Some(smallest_node) = queue.pop() {
-            *node = Some(Box::new(ListNode::new(smallest_node.head.val)));
-            node = &mut node.as_mut().unwrap().next;
-            if let Some(next) = smallest_node.head.next {
-                queue.push(HeapItem { head: next })
+        while let Some(HeapItem(mut list_head)) = heap.pop(){
+            if let Some(next) = list_head.next.take(){
+             heap.push(HeapItem(next));   
             }
+            *tail = Some(list_head);
+            tail = &mut tail.as_mut().unwrap().next;
         }
-
         result
     }
 }
 
+
+// << ---------------- Code below here is only for local use ---------------- >>
+
+
 use cargo_leet::ListNode;
-struct Solution;
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use cargo_leet::ListHead;
 
     use super::*;
 
-    #[test]
-    fn case1() {
-        let input = vec![vec![1, 4, 5], vec![1, 3, 4], vec![2, 6]];
-        let input: Vec<ListHead> = input.into_iter().map(|x| x.into()).collect();
-        let expected: ListHead = vec![1, 1, 2, 3, 4, 4, 5, 6].into();
-        let actual = Solution::merge_k_lists(input.into_iter().map(|x| x.into()).collect());
-        assert_eq!(actual, expected.into());
-    }
+    use rstest::rstest;
 
-    #[test]
-    fn case2() {
-        let input = vec![vec![]];
-        let input: Vec<ListHead> = input.into_iter().map(|x| x.into()).collect();
-        let expected: ListHead = vec![].into();
-        let actual = Solution::merge_k_lists(input.into_iter().map(|x| x.into()).collect());
-        assert_eq!(actual, expected.into());
-    }
-
-    #[test]
-    fn case3() {
-        let input: Vec<Vec<i32>> = vec![];
-        let input: Vec<ListHead> = input.into_iter().map(|x| x.into()).collect();
-        let expected: ListHead = vec![].into();
-        let actual = Solution::merge_k_lists(input.into_iter().map(|x| x.into()).collect());
-        assert_eq!(actual, expected.into());
-    }
-
-    #[test]
-    fn case4() {
-        let input = vec![vec![1, 4, 5], vec![1, 3, 4], vec![]];
-        let input: Vec<ListHead> = input.into_iter().map(|x| x.into()).collect();
-        let expected: ListHead = vec![1, 1, 3, 4, 4, 5].into();
-        let actual = Solution::merge_k_lists(input.into_iter().map(|x| x.into()).collect());
-        assert_eq!(actual, expected.into());
+    #[rstest]
+    #[case(
+        vec![
+                ListHead::from(vec![1,4,5]).into(),
+                ListHead::from(vec![1,3,4]).into(),
+                ListHead::from(vec![2,6]).into()
+            ], 
+        ListHead::from(vec![1,1,2,3,4,4,5,6]).into())]
+    #[case(vec![], ListHead::from(vec![]).into())]
+    #[case(vec![ListHead::from(vec![]).into()], ListHead::from(vec![]).into())]
+    fn case(#[case] lists: Vec<Option<Box<ListNode>>>, #[case] expected: Option<Box<ListNode>>) {
+        let actual = Solution::merge_k_lists(lists);
+        assert_eq!(actual, expected);
     }
 }
