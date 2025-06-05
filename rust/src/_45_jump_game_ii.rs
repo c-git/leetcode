39bc9b6407acd1be79dde88203845108bd958147
One <43485962+c-git@users.noreply.github.com>
2025-06-04 23:13:55 -0400

    45. Sub 3

--- rust/src/_45_jump_game_ii.rs
+++ rust/src/_45_jump_game_ii.rs
@@ -13,8 +13,8 @@ impl Solution {
                 .take(range as usize)
                 .min()
                 .copied()
-                .unwrap_or(i32::MAX - 1)
-                + 1;
+                .unwrap_or(i32::MAX)
+                .saturating_add(1);
         }
         dp[0]
     }
@@ -35,6 +35,7 @@ mod tests {
     #[case(vec![2,3,0,1,4], 2)]
     #[case(vec![1], 0)]
     #[case(vec![1,2,3], 2)]
+    #[case(vec![5,9,3,2,1,0,2,3,3,1,0,0], 3)]
     fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::jump(nums);
         assert_eq!(actual, expected);
