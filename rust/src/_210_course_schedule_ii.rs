655a109c8b55e2e4b9c27bbe98d95aec27a4e776
One <43485962+c-git@users.noreply.github.com>
2025-06-12 13:13:36 -0400

    210. Sub 2

--- rust/src/_210_course_schedule_ii.rs
+++ rust/src/_210_course_schedule_ii.rs
@@ -1,73 +1,65 @@
-#[derive(Clone, PartialEq, Eq)]
-enum Status {
-    NotVisited,
-    VisitInProgress,
-    VisitCompleted,
-}
+//! Solution for https://leetcode.com/problems/course-schedule-ii
+//! 210. Course Schedule II
+
+use std::collections::HashMap;
 
 impl Solution {
     pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
-        // Use topological sort (adapted solution from 207). Source: https://www.geeksforgeeks.org/topological-sorting/
-        let mut graph = vec![Vec::new(); num_courses as usize];
-        let mut visited = vec![Status::NotVisited; num_courses as usize];
-        let mut result = Vec::with_capacity(num_courses as usize);
+        let mut result = vec![];
+        let mut block_count = vec![0u16; num_courses as usize];
+        let mut prerequisites_left = prerequisites.into_iter().fold(
+            HashMap::<i32, Vec<i32>>::new(),
+            |mut map, requirement| {
+                let blocked = requirement[0];
+                let pre_req = requirement[1];
+                map.entry(pre_req).or_default().push(blocked);
+                block_count[blocked as usize] += 1;
+                map
+            },
+        );
 
-        for item in prerequisites {
-            graph[item[0] as usize].push(item[1]);
-        }
+        let mut unblocked: Vec<i32> = block_count
+            .iter()
+            .enumerate()
+            .filter_map(|(course, &count)| {
+                if count == 0 {
+                    Some(course as i32)
+                } else {
+                    None
+                }
+            })
+            .collect();
 
-        for i in 0..(num_courses as usize) {
-            if visited[i] == Status::VisitCompleted {
-                continue;
-            }
-
-            Solution::topological_sort(&graph, &mut visited, i, &mut result);
-
-            // If it returns and empty list then no solution is possible because it must include at least 1 node otherwise.
-            if result.is_empty() {
-                break;
-            }
-        }
-
-        //result.reverse(); // Not reversed because we want the prerequisites to come first
-        result
-    }
-
-    /// Produces a reverse topologically sorted set of nodes appended to the result argument or an empty list if there is a cycle
-    fn topological_sort(
-        graph: &Vec<Vec<i32>>,
-        visited: &mut Vec<Status>,
-        course: usize,
-        result: &mut Vec<i32>,
-    ) {
-        if visited[course] == Status::VisitInProgress {
-            // Cycle exists abort sort
-            result.clear();
-            return;
-        } else if visited[course] == Status::VisitCompleted {
-            return; // Already visited do nothing
-        }
-
-        visited[course] = Status::VisitInProgress;
-        for &prerequisite in &graph[course] {
-            Solution::topological_sort(graph, visited, prerequisite as usize, result);
-            // If it returns and empty list then no solution is possible because it must include at least 1 node otherwise.
-            if result.is_empty() {
-                return;
+        while let Some(course) = unblocked.pop() {
+            result.push(course);
+            if let Some(downstream_blocked) = prerequisites_left.remove(&course) {
+                for blocked_course in downstream_blocked {
+                    let count = &mut block_count[blocked_course as usize];
+                    *count -= 1;
+                    if *count == 0 {
+                        unblocked.push(blocked_course);
+                    }
+                }
             }
         }
 
-        result.push(course as i32);
-        visited[course] = Status::VisitCompleted;
+        if prerequisites_left.is_empty() {
+            result
+        } else {
+            Vec::new()
+        }
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
     use super::*;
+    use rstest::rstest;
+    use std::collections::HashMap;
 
     /// Checks if an actual solution is valid and panics if it is not. If expected is empty then actual must also be empty.
     /// Expected is not used for anything else as the order can be different and still correct
@@ -94,30 +86,15 @@ mod tests {
                 "Expected {prerequisite_course} to come before {dependent_course} but found them in positions: {prerequisite_pos} and {dependent_pos} in {actual:?}")
         }
     }
-
-    #[test]
-    fn case1() {
-        let num_courses = 2;
-        let prerequisites = vec![vec![1, 0]];
-        let expected = vec![0, 1];
-        let actual = Solution::find_order(num_courses, prerequisites.clone());
-        is_valid_order(actual, expected, prerequisites);
-    }
-
-    #[test]
-    fn case2() {
-        let num_courses = 4;
-        let prerequisites = vec![vec![1, 0], vec![2, 0], vec![3, 1], vec![3, 2]];
-        let expected = vec![0, 2, 1, 3];
-        let actual = Solution::find_order(num_courses, prerequisites.clone());
-        is_valid_order(actual, expected, prerequisites);
-    }
-
-    #[test]
-    fn case3() {
-        let num_courses = 1;
-        let prerequisites = vec![];
-        let expected = vec![0];
+    #[rstest]
+    #[case(2, vec![vec![1,0]], vec![0,1])]
+    #[case(4, vec![vec![1,0],vec![2,0],vec![3,1],vec![3,2]], vec![0,2,1,3])]
+    #[case(1, vec![], vec![0])]
+    fn case(
+        #[case] num_courses: i32,
+        #[case] prerequisites: Vec<Vec<i32>>,
+        #[case] expected: Vec<i32>,
+    ) {
         let actual = Solution::find_order(num_courses, prerequisites.clone());
         is_valid_order(actual, expected, prerequisites);
     }
