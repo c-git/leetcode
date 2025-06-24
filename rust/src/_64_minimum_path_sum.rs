04024b10d283c2c0797f0f037bad6fc796b21c93
One <43485962+c-git@users.noreply.github.com>
2025-06-23 22:32:30 -0400

    64. Sub 3

--- rust/src/_64_minimum_path_sum.rs
+++ rust/src/_64_minimum_path_sum.rs
@@ -1,20 +1,23 @@
 //! Solution for https://leetcode.com/problems/minimum-path-sum
 //! 64. Minimum Path Sum
 
-use std::cmp::min;
-
 impl Solution {
+    /// Replace each cell with the min value to get to that cell
+    /// For each cell we can only get there from the left of the top so just
+    /// update to the cell value plus the min of those two
     pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
+        // For top row we can only get there from the left
         for col in 1..grid[0].len() {
             grid[0][col] += grid[0][col - 1];
         }
 
         for row in 1..grid.len() {
-            grid[row][0] += grid[row - 1][0];
+            grid[row][0] += grid[row - 1][0]; // We can only get to the left most cell from up
             for col in 1..grid[row].len() {
-                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1]);
+                grid[row][col] += grid[row - 1][col].min(grid[row][col - 1]);
             }
         }
+
         *grid.last().unwrap().last().unwrap()
     }
 }
