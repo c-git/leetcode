c93b1b5db501284bf3b5388529f24c4c2d3d49bb
One <43485962+c-git@users.noreply.github.com>
2025-06-26 21:59:53 -0400

    139. Sub 3

--- rust/src/_139_word_break.rs
+++ rust/src/_139_word_break.rs
@@ -1,66 +1,29 @@
 //! Solution for https://leetcode.com/problems/word-break
 //! 139. Word Break
 
-use std::collections::HashMap;
-
-struct Trie {
-    root: TrieNode,
-}
-impl Trie {
-    fn new(word_dict: Vec<String>) -> Self {
-        let mut result = Self {
-            root: TrieNode::default(),
-        };
-        for word in word_dict {
-            result.add_word(word);
-        }
-        result
-    }
-
-    fn add_word(&mut self, word: String) {
-        let mut node = &mut self.root;
-        for c in word.chars() {
-            node = node.next.entry(c).or_default();
-        }
-        node.is_end_of_word = true;
-    }
-}
-
-#[derive(Default)]
-struct TrieNode {
-    next: HashMap<char, TrieNode>,
-    is_end_of_word: bool,
-}
+use std::collections::HashSet;
 
 impl Solution {
+    /// Based on https://www.youtube.com/watch?v=TK9pptFzH-A
     pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
-        let chars: Vec<char> = s.chars().collect();
-        let mut resume_points = vec![0];
-        let trie = Trie::new(word_dict);
+        let word_dict: HashSet<&str> = word_dict.iter().map(|x| x.as_str()).collect();
 
-        while let Some(start_index) = resume_points.pop() {
-            let mut node = &trie.root;
-            let mut is_fail = false;
-            for (i, c) in chars.iter().enumerate().skip(start_index) {
-                if node.is_end_of_word {
-                    resume_points.push(i);
-                }
-                if let Some(next_node) = node.next.get(c) {
-                    node = next_node;
-                } else {
-                    // Not able to continue check for next resume point
-                    is_fail = true;
+        // Stores a set containing the possible start points
+        let mut dp = HashSet::new();
+        dp.insert(0);
+        for end_index in 0..s.len() {
+            for start_index in dp.iter().copied() {
+                if word_dict.contains(&s[start_index..=end_index]) {
+                    if end_index == s.len() - 1 {
+                        return true; // We made it to the end
+                    }
+                    dp.insert(end_index + 1);
                     break;
-                };
-            }
-            if !is_fail && node.is_end_of_word {
-                // Reached to end without failing all characters are part of a word
-                return true;
+                }
             }
         }
 
-        // No more resume points, unable to complete chars
-        false
+        false // Unable to get to end
     }
 }
 
