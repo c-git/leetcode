23fba53619d76d1e78cc30a8b95e76db31c4ac83
One <43485962+c-git@users.noreply.github.com>
2025-07-23 22:20:47 -0400

    875. Sub 2

--- rust/src/_875_koko_eating_bananas.rs
+++ rust/src/_875_koko_eating_bananas.rs
@@ -27,11 +27,17 @@ impl Solution {
 
 /// Returns true IFF all piles can be eaten in h hours
 fn test_eating(piles: &[i32], k: i32, h: i32) -> bool {
-    piles
+    let mut sum = 0;
+    for hours_needed in piles
         .iter()
         .map(|x| (x / k) + if x % k == 0 { 0 } else { 1 })
-        .sum::<i32>()
-        <= h
+    {
+        sum += hours_needed;
+        if sum > h {
+            return false;
+        }
+    }
+    sum <= h
 }
 
 // << ---------------- Code below here is only for local use ---------------- >>
@@ -48,6 +54,7 @@ mod tests {
     #[case(vec![3,6,7,11], 8, 4)]
     #[case(vec![30,11,23,4,20], 5, 30)]
     #[case(vec![30,11,23,4,20], 6, 23)]
+    #[case(vec![332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184], 823855818, 14)]
     fn case(#[case] piles: Vec<i32>, #[case] h: i32, #[case] expected: i32) {
         let actual = Solution::min_eating_speed(piles, h);
         assert_eq!(actual, expected);
