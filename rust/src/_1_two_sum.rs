f57476dc85905db65aa98bdda232b5b3821fd480
One <43485962+c-git@users.noreply.github.com>
2025-09-04 21:24:06 -0400

    1. Sub 3

--- rust/src/_1_two_sum.rs
+++ rust/src/_1_two_sum.rs
@@ -1,20 +1,19 @@
 //! Solution for https://leetcode.com/problems/two-sum
 //! 1. Two Sum
 
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut seen = BTreeMap::new();
-        for (i, &num) in nums.iter().enumerate() {
-            let diff = target - num;
-            if let Some(&other_index) = seen.get(&diff) {
-                return vec![i as _, other_index as _];
+        let mut diffs: HashMap<i32, usize> = HashMap::new();
+        for (i, num) in nums.iter().enumerate() {
+            if let Some(other_idx) = diffs.get(num) {
+                return vec![*other_idx as i32, i as i32];
             } else {
-                seen.insert(num, i);
+                diffs.insert(target - num, i);
             }
         }
-        unreachable!("question says, 'each input would have exactly one solution'")
+        unreachable!("Constraints say that a valid solution exists")
     }
 }
 
@@ -32,10 +31,8 @@ mod tests {
     #[case(vec![2,7,11,15], 9, vec![0,1])]
     #[case(vec![3,2,4], 6, vec![1,2])]
     #[case(vec![3,3], 6, vec![0,1])]
-    fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] mut expected: Vec<i32>) {
-        let mut actual = Solution::two_sum(nums, target);
-        actual.sort();
-        expected.sort();
+    fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] expected: Vec<i32>) {
+        let actual = Solution::two_sum(nums, target);
         assert_eq!(actual, expected);
     }
 }
