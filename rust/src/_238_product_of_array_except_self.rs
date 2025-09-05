8df27d8d038edb455281cfb780ab0538b790b202
One <43485962+c-git@users.noreply.github.com>
2025-09-04 21:39:16 -0400

    238. Sub 4

--- rust/src/_238_product_of_array_except_self.rs
+++ rust/src/_238_product_of_array_except_self.rs
@@ -3,21 +3,30 @@
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        // Populate result with suffix products
-        let mut result = vec![0; nums.len()];
-        let mut suffix_product = 1;
+        let mut result = Vec::with_capacity(nums.len());
+        let mut prefix_prod = Vec::with_capacity(nums.len());
+        let mut temp_prod = 1;
+        for num in nums.iter() {
+            temp_prod *= num;
+            prefix_prod.push(temp_prod);
+        }
+        let mut suffix_prod = vec![1; nums.len()];
+        temp_prod = 1;
         for (i, num) in nums.iter().enumerate().rev() {
-            suffix_product *= num;
-            result[i] = suffix_product;
+            temp_prod *= num;
+            suffix_prod[i] = temp_prod;
         }
-        result[0] = result[1];
-        let mut prefix_product = nums[0];
-        for (i, num) in nums.iter().enumerate().take(nums.len() - 1).skip(1) {
-            result[i] = prefix_product * result[i + 1];
-            prefix_product *= num;
+        // For test case 1
+        // nums:   [1,  2,   3,  4]
+        // prefix: [1,  2,   6, 24]
+        // suffix: [24, 24, 12,  4]
+        // result: [24, 12,  8,  6]
+
+        result.push(suffix_prod[1]);
+        for i in 1..nums.len() - 1 {
+            result.push(prefix_prod[i - 1] * suffix_prod[i + 1]);
         }
-        let n = result.len();
-        result[n - 1] = prefix_product;
+        result.push(prefix_prod[nums.len() - 2]);
         result
     }
 }
