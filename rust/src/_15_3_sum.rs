6e686e4d575ee2ba88a3ec6d3129a13765a14c6b
One <43485962+c-git@users.noreply.github.com>
2025-09-08 00:00:03 -0400

    15. Sub 7

--- rust/src/_15_3_sum.rs
+++ rust/src/_15_3_sum.rs
@@ -2,35 +2,36 @@
 //! 15. 3Sum
 
 impl Solution {
+    // Based on https://www.youtube.com/watch?v=wCe-MeqXgMc and previous solution
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut result = vec![];
         nums.sort_unstable();
-        for (first_idx, first_val) in nums.iter().copied().enumerate() {
-            if first_idx > 0 && first_val == nums[first_idx - 1] {
-                // Skip duplicated values
+        for (left_idx, left) in nums.iter().enumerate() {
+            if left_idx > 0 && left == &nums[left_idx - 1] {
+                // Skip duplicates
                 continue;
             }
-            let mut left = first_idx + 1;
-            let mut right = nums.len() - 1;
-            while left < right {
-                if left > first_idx + 1 && nums[left] == nums[left - 1] {
-                    // Skip duplicates
-                    left += 1;
-                    continue;
-                }
-                if right < nums.len() - 1 && nums[right] == nums[right + 1] {
-                    // Skip duplicates
-                    right -= 1;
-                    continue;
-                }
-                let curr = first_val + nums[left] + nums[right];
-                if curr == 0 {
-                    result.push(vec![first_val, nums[left], nums[right]]);
-                }
-                if curr > 0 {
-                    right -= 1;
-                } else {
-                    left += 1;
+            let mut mid_idx = left_idx + 1;
+            let mut right_idx = nums.len() - 1;
+            while mid_idx < right_idx {
+                let total = left + nums[mid_idx] + nums[right_idx];
+                match total.cmp(&0) {
+                    std::cmp::Ordering::Less => {
+                        // Increase size as total is too small
+                        mid_idx += 1;
+                    }
+                    std::cmp::Ordering::Equal => {
+                        result.push(vec![*left, nums[mid_idx], nums[right_idx]]);
+                        mid_idx += 1;
+                        while mid_idx < right_idx && nums[mid_idx - 1] == nums[mid_idx] {
+                            // Skip duplicates
+                            mid_idx += 1;
+                        }
+                    }
+                    std::cmp::Ordering::Greater => {
+                        // Decrease size too big
+                        right_idx -= 1;
+                    }
                 }
             }
         }
