b7891bccba54aed7c3cfc255703fef08d529dc46
One <43485962+c-git@users.noreply.github.com>
2025-10-02 22:14:13 -0400

    33. Sub 7

--- rust/src/_33_search_in_rotated_sorted_array.rs
+++ rust/src/_33_search_in_rotated_sorted_array.rs
@@ -3,47 +3,20 @@
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let first = nums[0];
-        let partition_point = partition_point(&nums, |&x| x >= first);
-        if partition_point >= nums.len() {
-            // not rotated (or rotated by 0)
-            return match nums.binary_search(&target) {
-                Ok(idx) => idx as _,
-                Err(_) => -1, // Not found
-            };
-        }
-        if target >= first {
-            match nums[..partition_point].binary_search(&target) {
-                Ok(idx) => idx as _,
-                Err(_) => -1, // Not in first section and cannot be in second
-            }
+        let first = nums.first().unwrap();
+        let pivot_point = nums.partition_point(|num| num >= first);
+        if &target >= first {
+            nums[..pivot_point]
+                .binary_search(&target)
+                .map(|x| x as i32)
+                .unwrap_or(-1)
         } else {
-            match nums[partition_point..].binary_search(&target) {
-                Ok(idx) => (idx + partition_point) as _,
-                Err(_) => -1, // Not in first section and cannot be in second
-            }
+            nums[pivot_point..]
+                .binary_search(&target)
+                .map(|x| x + pivot_point)
+                .map(|x| x as i32)
+                .unwrap_or(-1)
         }
-    }
-}
-
-/// Returns the index of the partition point according to the given predicate (the index of the first element of the second partition).
-/// See <https://doc.rust-lang.org/std/primitive.slice.html#method.partition_point> for more info
-fn partition_point<T>(arr: &[T], is_left_half: impl Fn(&T) -> bool) -> usize {
-    let mut size = arr.len();
-    if size == 0 {
-        return 0;
-    }
-    let mut base = 0usize;
-    while size > 1 {
-        let half = size / 2;
-        let mid = base + half;
-        base = if is_left_half(&arr[mid]) { mid } else { base };
-        size -= half;
-    }
-    if is_left_half(&arr[base]) {
-        base + 1
-    } else {
-        base
     }
 }
 
@@ -58,9 +31,8 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(vec![4,5,6,7,8,0,1,2], 0, 5)]
+    #[case(vec![4,5,6,7,0,1,2], 0, 4)]
     #[case(vec![4,5,6,7,0,1,2], 3, -1)]
-    #[case(vec![1], 0, -1)]
     #[case(vec![1], 0, -1)]
     fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] expected: i32) {
         let actual = Solution::search(nums, target);
