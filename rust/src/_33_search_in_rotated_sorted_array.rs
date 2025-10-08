5cebc77bc23a35713c3175222ea7132dfd5b0959
One <43485962+c-git@users.noreply.github.com>
2025-10-08 19:42:14 -0400

    33. Sub 8

--- rust/src/_33_search_in_rotated_sorted_array.rs
+++ rust/src/_33_search_in_rotated_sorted_array.rs
@@ -4,17 +4,16 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         let first = nums.first().unwrap();
-        let pivot_point = nums.partition_point(|num| num >= first);
+        let pivot = nums.partition_point(|x| x >= first);
         if &target >= first {
-            nums[..pivot_point]
+            nums[..pivot]
                 .binary_search(&target)
                 .map(|x| x as i32)
                 .unwrap_or(-1)
         } else {
-            nums[pivot_point..]
+            nums[pivot..]
                 .binary_search(&target)
-                .map(|x| x + pivot_point)
-                .map(|x| x as i32)
+                .map(|x| (x + pivot) as i32)
                 .unwrap_or(-1)
         }
     }
@@ -34,6 +33,7 @@ mod tests {
     #[case(vec![4,5,6,7,0,1,2], 0, 4)]
     #[case(vec![4,5,6,7,0,1,2], 3, -1)]
     #[case(vec![1], 0, -1)]
+    #[case(vec![0,1,2,3,4,5,6,7], 3, 3)]
     fn case(#[case] nums: Vec<i32>, #[case] target: i32, #[case] expected: i32) {
         let actual = Solution::search(nums, target);
         assert_eq!(actual, expected);
