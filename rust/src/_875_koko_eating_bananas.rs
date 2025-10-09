17aea89f81b9bc2e88cad83ffe5d9e6c913f9d86
One <43485962+c-git@users.noreply.github.com>
2025-10-09 13:43:46 -0400

    875. Sub 4

--- rust/src/_875_koko_eating_bananas.rs
+++ rust/src/_875_koko_eating_bananas.rs
@@ -3,43 +3,39 @@
 
 impl Solution {
     pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-        let low = 1;
-        let high = *piles.iter().max().unwrap();
-        if test_eating(&piles, low, h) {
-            return low;
-        }
-        if !test_eating(&piles, high, h) {
-            unreachable!("should never have more piles than hours")
-        }
-        let mut base = low;
-        let mut size = high - low;
+        let mut size = *piles.iter().max().unwrap();
+        debug_assert!(test_can_eat_all(size, &piles, h), "{size}");
+        let mut base = 1;
         while size > 1 {
             let half = size / 2;
             let mid = base + half;
-            base = if !test_eating(&piles, mid, h) {
+            base = if !test_can_eat_all(half + base, &piles, h) {
                 mid
             } else {
                 base
             };
             size -= half;
         }
-        base + 1
+        if !test_can_eat_all(base, &piles, h) {
+            base + 1
+        } else {
+            base
+        }
     }
 }
 
-/// Returns true IFF all piles can be eaten in h hours
-fn test_eating(piles: &[i32], k: i32, h: i32) -> bool {
-    let mut sum = 0;
-    for hours_needed in piles
-        .iter()
-        .map(|x| (x / k) + if x % k == 0 { 0 } else { 1 })
-    {
-        sum += hours_needed;
-        if sum > h {
+fn test_can_eat_all(test_value: i32, piles: &[i32], h: i32) -> bool {
+    let mut hours_sum = 0;
+    for pile in piles {
+        hours_sum += pile / test_value;
+        if pile % test_value > 0 {
+            hours_sum += 1;
+        }
+        if hours_sum > h {
             return false;
         }
     }
-    sum <= h
+    true
 }
 
 // << ---------------- Code below here is only for local use ---------------- >>
