e24d00aee843a18513dae0f72795cad50a277288
One <43485962+c-git@users.noreply.github.com>
2025-10-16 23:40:34 -0400

    39. Sub 4

--- rust/src/_39_combination_sum.rs
+++ rust/src/_39_combination_sum.rs
@@ -1,33 +1,49 @@
+//! Solution for https://leetcode.com/problems/combination-sum
+//! 39. Combination Sum
+
 impl Solution {
-    // Source: https://leetcode.com/problems/combination-sum/solutions/3280398/rust-beats-100-memory-and-100-complexety-easy-recursive-sort-solution/
+    /// After watching https://www.youtube.com/watch?v=GBKI9VSKdGg
     pub fn combination_sum(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         let mut result = vec![];
         candidates.sort_unstable();
-        Self::_combination_sum(&candidates, target, &mut result, vec![]);
+        let mut partial = vec![];
+        Self::combination_sum_(&candidates, target, &mut partial, 0, &mut result);
         result
     }
-    pub fn _combination_sum(
+
+    fn combination_sum_(
         candidates: &[i32],
         target: i32,
+        partial: &mut Vec<i32>,
+        partial_sum: i32,
         result: &mut Vec<Vec<i32>>,
-        attempt: Vec<i32>,
     ) {
-        if target == 0 {
-            return result.push(attempt);
+        if partial_sum == target {
+            result.push(partial.clone());
+        } else if partial_sum < target && !candidates.is_empty() {
+            // Normal case we can add more so take the first or leave it
+
+            // Try skipping it the first item
+            Self::combination_sum_(&candidates[1..], target, partial, partial_sum, result);
+
+            // Try including the first item
+            partial.push(candidates[0]);
+            Self::combination_sum_(
+                candidates,
+                target,
+                partial,
+                partial_sum + candidates[0],
+                result,
+            );
+            partial.pop();
         }
-        candidates
-            .iter()
-            .enumerate()
-            .take_while(|(_, &val)| val <= target)
-            .for_each(|(i, &val)| {
-                let mut new = attempt.clone();
-                new.push(val);
-                Self::_combination_sum(&candidates[i..], target - val, result, new)
-            })
     }
 }
 
-struct Solution;
+// << ---------------- Code below here is only for local use ---------------- >>
+
+pub struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
