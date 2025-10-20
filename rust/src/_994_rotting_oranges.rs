786a9a91ed0e2858ecf893be3f6b9df97da9b048
One <43485962+c-git@users.noreply.github.com>
2025-10-20 13:50:07 -0400

    994. Sub 3

--- rust/src/_994_rotting_oranges.rs
+++ rust/src/_994_rotting_oranges.rs
@@ -3,94 +3,150 @@
 
 use std::collections::VecDeque;
 
-const FRESH: i32 = 1;
-const ROTTEN: i32 = 2;
-
 impl Solution {
     pub fn oranges_rotting(mut grid: Vec<Vec<i32>>) -> i32 {
-        let mut max_seconds = 0;
+        let mut result = 0;
 
-        // Collect the starting rotten oranges
-        let mut rotten_list = VecDeque::new();
+        // Find initial rotten oranges
+        let mut rotten_queue = VecDeque::new(); // (Row, Col, Time)
         for (row_idx, row) in grid.iter().enumerate() {
             for (col_idx, cell) in row.iter().enumerate() {
-                if cell == &ROTTEN {
-                    rotten_list.push_back((row_idx, col_idx, 0));
+                let cell: CellStatus = cell.into();
+                if cell.is_rotten() {
+                    rotten_queue.push_back((row_idx, col_idx, 0));
                 }
             }
         }
 
-        // Use BFS to change adjacent oranges to rotten
-        while let Some((row, col, seconds)) = rotten_list.pop_front() {
-            if row > 0 {
-                check_cell(
-                    row - 1,
-                    col,
-                    seconds,
+        // Each step spread the rot to adjacent cells
+        while let Some((row_idx, col_idx, time)) = rotten_queue.pop_front() {
+            // Up
+            if row_idx > 0 {
+                Self::mark_rotten(
+                    row_idx - 1,
+                    col_idx,
+                    time,
                     &mut grid,
-                    &mut rotten_list,
-                    &mut max_seconds,
+                    &mut rotten_queue,
+                    &mut result,
                 );
             }
-            if col > 0 {
-                check_cell(
-                    row,
-                    col - 1,
-                    seconds,
+
+            // Down
+            if row_idx < grid.len() - 1 {
+                Self::mark_rotten(
+                    row_idx + 1,
+                    col_idx,
+                    time,
                     &mut grid,
-                    &mut rotten_list,
-                    &mut max_seconds,
+                    &mut rotten_queue,
+                    &mut result,
                 );
             }
-            if row < grid.len() - 1 {
-                check_cell(
-                    row + 1,
-                    col,
-                    seconds,
+
+            // Left
+            if col_idx > 0 {
+                Self::mark_rotten(
+                    row_idx,
+                    col_idx - 1,
+                    time,
                     &mut grid,
-                    &mut rotten_list,
-                    &mut max_seconds,
+                    &mut rotten_queue,
+                    &mut result,
                 );
             }
-            if col < grid[0].len() - 1 {
-                check_cell(
-                    row,
-                    col + 1,
-                    seconds,
+
+            // Right
+            if col_idx < grid[0].len() - 1 {
+                Self::mark_rotten(
+                    row_idx,
+                    col_idx + 1,
+                    time,
                     &mut grid,
-                    &mut rotten_list,
-                    &mut max_seconds,
+                    &mut rotten_queue,
+                    &mut result,
                 );
             }
         }
 
-        // Check for any remaining fresh oranges
-        for row in grid.iter() {
-            for cell in row {
-                if cell == &FRESH {
-                    // Found an orange this is still fresh
-                    return -1;
-                }
-            }
+        // Check if any fresh oranges remain then -1 else result
+        if grid.iter().flatten().any(|orange| {
+            let cell_status: CellStatus = orange.into();
+            cell_status.is_fresh()
+        }) {
+            -1
+        } else {
+            result
         }
-        max_seconds
+    }
+
+    fn mark_rotten(
+        row_idx: usize,
+        col_idx: usize,
+        time: i32,
+        grid: &mut [Vec<i32>],
+        rotten_queue: &mut VecDeque<(usize, usize, i32)>,
+        result: &mut i32,
+    ) {
+        let cell = &mut grid[row_idx][col_idx];
+        let cell_status: CellStatus = (&(*cell)).into();
+        if !cell_status.is_fresh() {
+            // This is not a fresh orange no action needed
+            return;
+        }
+        *cell = CellStatus::Rotten.into();
+        let new_time_step = time + 1;
+        *result = (*result).max(new_time_step);
+        rotten_queue.push_back((row_idx, col_idx, new_time_step));
     }
 }
 
-fn check_cell(
-    row: usize,
-    col: usize,
-    seconds: i32,
-    grid: &mut [Vec<i32>],
-    rotten_list: &mut VecDeque<(usize, usize, i32)>,
-    max_seconds: &mut i32,
-) {
-    if grid[row][col] != FRESH {
-        return;
+enum CellStatus {
+    /// 0 representing an empty cell,
+    Empty,
+    /// 1 representing a fresh orange, or
+    Fresh,
+    /// 2 representing a rotten orange
+    Rotten,
+}
+
+impl CellStatus {
+    /// Returns `true` if the status is [`Fresh`].
+    ///
+    /// [`Fresh`]: Status::Fresh
+    #[must_use]
+    fn is_fresh(&self) -> bool {
+        matches!(self, Self::Fresh)
     }
-    grid[row][col] = ROTTEN;
-    rotten_list.push_back((row, col, seconds + 1));
-    *max_seconds = (*max_seconds).max(seconds + 1);
+
+    /// Returns `true` if the status is [`Rotten`].
+    ///
+    /// [`Rotten`]: Status::Rotten
+    #[must_use]
+    fn is_rotten(&self) -> bool {
+        matches!(self, Self::Rotten)
+    }
+}
+
+impl From<&i32> for CellStatus {
+    fn from(value: &i32) -> Self {
+        match value {
+            0 => Self::Empty,
+            1 => Self::Fresh,
+            2 => Self::Rotten,
+            _ => unreachable!("by problem constraint"),
+        }
+    }
+}
+
+impl From<CellStatus> for i32 {
+    fn from(value: CellStatus) -> Self {
+        match value {
+            CellStatus::Empty => 0,
+            CellStatus::Fresh => 1,
+            CellStatus::Rotten => 2,
+        }
+    }
 }
 
 // << ---------------- Code below here is only for local use ---------------- >>
