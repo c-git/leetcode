b4e3d764dd9dccda8ee6407bf53f5a27ada0d048
One <43485962+c-git@users.noreply.github.com>
2025-10-22 19:54:26 -0400

    238. Sub 6

--- rust/src/_238_product_of_array_except_self.rs
+++ rust/src/_238_product_of_array_except_self.rs
@@ -3,28 +3,20 @@
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = vec![0; nums.len()];
         let mut partial_product = 1;
-        let prefix_prods: Vec<i32> = nums
+        let mut result: Vec<i32> = nums
             .iter()
-            .map(|x| {
-                partial_product *= x;
+            .map(|num| {
+                partial_product *= num;
                 partial_product
             })
             .collect();
-
-        // Set end value
-        result[nums.len() - 1] = prefix_prods[prefix_prods.len() - 2];
-
-        // Set middle values (skip both ends)
-        partial_product = nums[nums.len() - 1];
-
-        for (i, result_num) in result.iter_mut().enumerate().skip(1).rev().skip(1) {
-            *result_num = prefix_prods[i - 1] * partial_product;
-            partial_product *= nums[i];
+        partial_product = 1;
+        for (i, num) in nums.iter().enumerate().skip(1).rev() {
+            result[i] = partial_product * result[i - 1];
+            partial_product *= num;
         }
-        result[0] = partial_product; // Store product of everything but first
-
+        result[0] = partial_product;
         result
     }
 }
