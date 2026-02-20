507f20da0c2f0ff444cce379a0c4d1096fdb970f
One <43485962+c-git@users.noreply.github.com>
2026-02-19 22:32:27 -0500

    198. Sub 4

--- rust/src/_198_house_robber.rs
+++ rust/src/_198_house_robber.rs
@@ -3,17 +3,14 @@
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        if nums.len() == 1 {
-            return nums[0];
+        let mut taken = 0;
+        let mut not_taken = 0;
+        for num in nums {
+            let temp = not_taken + num;
+            not_taken = taken.max(not_taken);
+            taken = temp;
         }
-        // Each index stores the best value possible up to that point
-        let mut dp = vec![0; nums.len()];
-        dp[0] = nums[0];
-        dp[1] = nums[0].max(nums[1]);
-        for i in 2..nums.len() {
-            dp[i] = dp[i - 1].max(dp[i - 2] + nums[i]);
-        }
-        *dp.last().unwrap()
+        taken.max(not_taken)
     }
 }
 
@@ -30,7 +27,6 @@ mod tests {
     #[rstest]
     #[case(vec![1,2,3,1], 4)]
     #[case(vec![2,7,9,3,1], 12)]
-    #[case(vec![2,1,1,2], 4)]
     fn case(#[case] nums: Vec<i32>, #[case] expected: i32) {
         let actual = Solution::rob(nums);
         assert_eq!(actual, expected);
