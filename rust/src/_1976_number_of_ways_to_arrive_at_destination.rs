2df1d0d1a975ce4d22b45a53e2b3b53310a13b02
One <43485962+c-git@users.noreply.github.com>
2026-02-23 13:23:18 -0500

    1976. Sub 4

--- rust/src/_1976_number_of_ways_to_arrive_at_destination.rs
+++ rust/src/_1976_number_of_ways_to_arrive_at_destination.rs
@@ -1,99 +1,90 @@
 //! Solution for https://leetcode.com/problems/number-of-ways-to-arrive-at-destination
 //! 1976. Number of Ways to Arrive at Destination
 
-const MODULUS: usize = 1_000_000_007;
+use std::collections::BinaryHeap;
 
-#[derive(Clone, Copy)]
-struct Edge {
-    /// The other end of the road
-    dst: usize,
-    time: usize,
+const MODULO: usize = 1_000_000_007;
+type Time = usize;
+type Intersection = usize;
+
+#[derive(Default)]
+struct MinHeap {
+    max_heap: BinaryHeap<std::cmp::Reverse<(Time, Intersection)>>,
+}
+
+impl MinHeap {
+    fn push(&mut self, time: Time, intersection: Intersection) {
+        self.max_heap.push(std::cmp::Reverse((time, intersection)));
+    }
+
+    fn pop(&mut self) -> Option<(Time, Intersection)> {
+        let std::cmp::Reverse(result) = self.max_heap.pop()?;
+        Some(result)
+    }
 }
 
 struct Graph {
-    edges: Vec<Vec<Edge>>,
+    roads: Vec<Vec<(Intersection, Time)>>,
 }
 
 impl Graph {
     fn new(size: usize) -> Self {
         Self {
-            edges: vec![Vec::new(); size],
+            roads: vec![vec![]; size],
         }
     }
 
-    fn add_edge(&mut self, edge_info: Vec<i32>) {
-        let node1 = edge_info[0] as usize;
-        let node2 = edge_info[1] as usize;
-        let time = edge_info[2] as usize;
-        self.edges[node1].push(Edge { dst: node2, time });
-        self.edges[node2].push(Edge { dst: node1, time });
-    }
-}
-
-type Time = usize;
-type Intersection = usize;
-
-struct MinHeap {
-    max_heap: std::collections::BinaryHeap<std::cmp::Reverse<(Time, Intersection)>>,
-}
-
-impl MinHeap {
-    fn new() -> Self {
-        Self {
-            max_heap: Default::default(),
-        }
-    }
-
-    fn push(&mut self, value: (Time, Intersection)) {
-        self.max_heap.push(std::cmp::Reverse(value));
-    }
-
-    fn pop(&mut self) -> Option<(Time, Intersection)> {
-        self.max_heap.pop().map(|x| x.0)
+    fn add_road(&mut self, side1: Intersection, side2: Intersection, time: Time) {
+        self.roads[side1].push((side2, time));
+        self.roads[side2].push((side1, time));
     }
 }
 
 impl Solution {
-    /// After reading editorial
+    /// After reading over previous solution that was based on the editorial
     pub fn count_paths(n: i32, roads: Vec<Vec<i32>>) -> i32 {
         let n = n as usize;
-        let mut shortest_paths_times = vec![usize::MAX; n];
-        let mut path_count = vec![0; n];
-        shortest_paths_times[0] = 0; // Starting point
-        path_count[0] = 1; // Exactly 1 way to get to starting point from the starting point
 
-        // Build Graph
+        let mut path_count = vec![0; n];
+        let mut shortest_time = vec![usize::MAX; n];
+        path_count[0] = 1; // One way to start
+        shortest_time[0] = 0; // No time required to get to start
+
+        // Build graph
         let mut graph = Graph::new(n);
         for road in roads {
-            graph.add_edge(road);
+            graph.add_road(road[0] as usize, road[1] as usize, road[2] as usize);
         }
 
-        // Use Dijkstra's algorithm to get the number of shortest paths available
-        let mut min_heap = MinHeap::new();
-        min_heap.push((0, 0));
-        while let Some((time, intersection)) = min_heap.pop() {
-            if time > shortest_paths_times[intersection] {
-                // Discard larger value for known path
+        let mut heap = MinHeap::default();
+        heap.push(0, 0);
+
+        while let Some((time, src_intersection)) = heap.pop() {
+            println!("{time} {src_intersection}");
+            if time > shortest_time[src_intersection] {
+                // Skip slow routes to known intersections
                 continue;
             }
-            for edge in graph.edges[intersection].iter().copied() {
-                let new_time = time + edge.time;
-                match new_time.cmp(&shortest_paths_times[edge.dst]) {
+            for (dst, road_time) in graph.roads[src_intersection].iter().copied() {
+                let new_time = time + road_time;
+                match new_time.cmp(&shortest_time[dst]) {
                     std::cmp::Ordering::Less => {
-                        shortest_paths_times[edge.dst] = new_time;
-                        path_count[edge.dst] = path_count[intersection];
-                        min_heap.push((new_time, edge.dst));
+                        // New path forget about old ones
+                        shortest_time[dst] = new_time;
+                        path_count[dst] = path_count[src_intersection];
+                        heap.push(new_time, dst);
                     }
                     std::cmp::Ordering::Equal => {
-                        path_count[edge.dst] =
-                            (path_count[edge.dst] + path_count[intersection]) % MODULUS;
+                        // Increment
+                        path_count[dst] = (path_count[dst] + path_count[src_intersection]) % MODULO;
                     }
-                    std::cmp::Ordering::Greater => {} // Do not update this is a slower path
+                    std::cmp::Ordering::Greater => {} // Slower ignore
                 }
             }
+            println!("{shortest_time:?} {path_count:?}");
         }
 
-        path_count[n - 1] as _
+        (*path_count.last().unwrap()) as i32
     }
 }
 
